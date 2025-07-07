6ba7d081b902271a0e52c5e98dadaf1f0db97027
Hawraa <hawra8516@gmail.com>
2025-07-07 14:21:06 +0400

    TRA502: Edit Sleep-In Logic task

--- from_Hawraa/SleepInLogic.java
+++ from_Hawraa/SleepInLogic.java
@@ -1,17 +1,17 @@
 public class SleepInLogic{
 	
-	  public static void sleepIn(boolean weekday, boolean vacation){
+	  public static boolean sleepIn(boolean weekday, boolean vacation){
 		  if (!weekday || vacation){
-			  System.out.println("We can sleep in it.");
+			  return true;
 		  }
 		  else{
-			  System.out.println("We can't sleep in it.");
+			  return false;
 		  }
 	  }
 	  
 	  public static void main(String[] args) {
-		 sleepIn(false, false);
-		 sleepIn(true, false);
-		 sleepIn(false, true);
+		 System.out.println(sleepIn(false, false));
+		 System.out.println(sleepIn(true, false));
+		 System.out.println(sleepIn(false, true));
 	  }
 }
