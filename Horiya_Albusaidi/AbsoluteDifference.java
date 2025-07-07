fbf2a108d9e86de8568c8081ec58208a213aae2e
EchoCoder20 <albusaidi1915@gmail.com>
2025-07-07 14:45:04 +0400

    Solving TRA-601 task

---
+++ Horiya_Albusaidi/AbsoluteDifference.java
@@ -0,0 +1,12 @@
+public class AbsoluteDifference {
+    public static Integer diff21(int n)
+    {
+        int result;
+        if(n>21)
+        {
+            result = 2*(n-21);
+        }
+        else result = 21-n;
+        return result;
+    }
+}
