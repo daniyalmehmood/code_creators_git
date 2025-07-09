ed32abf0e5f2b9ee14a30b234ad945cf5f979a46
EchoCoder20 <albusaidi1915@gmail.com>
2025-07-09 11:51:16 +0400

    Solving TRA-760 Task

---
+++ Horiya_Albusaidi/In3050.java
@@ -0,0 +1,9 @@
+public class In3050 {
+   public static Boolean in3050(int a, int b)
+   {
+
+       if(a>= 30 && a <= 50  && b>= 30 && b <= 50)
+           return true;
+       else return false;
+   }
+}
