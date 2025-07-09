638d1f9dbc2f6801aae08b9dcb09182ee132b5ea
EchoCoder20 <albusaidi1915@gmail.com>
2025-07-09 11:15:07 +0400

    Modified the code of the task

--- Horiya_Albusaidi/StringE.java
+++ Horiya_Albusaidi/StringE.java
@@ -1,17 +1,17 @@
 public class StringE {
-     public static Boolean stringE(String str)
-     {
-         int stringLength = str.length();
-         if(stringLength>=2)
-         {
-             for (int i = 1; i <= 3; i++)
-             {
-                 if(str.charAt(i)=='e') return true;
+    public static Boolean stringE(String str)
+    {
+        int stringLength = str.length();
+        int counter=  0;
 
-             }
-         }
+        for (int i = 0; i <stringLength-1; i++)
+        {
+            if(str.charAt(i)=='e') counter++;
 
+        }
+        if(counter>3 || counter==0) {
+            return false;
+        } else return true;
 
-         return false;
-     }
+    }
 }
