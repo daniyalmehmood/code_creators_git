1f83ae56c7ae3d404e17d2ea8c77b1f6fbcb1342
EchoCoder20 <albusaidi1915@gmail.com>
2025-07-11 00:06:54 +0400

    modified code logic

--- Horiya_Albusaidi/StartOz.java
+++ Horiya_Albusaidi/StartOz.java
@@ -1,20 +1,24 @@
 public class StartOz {
     public static String startOz(String str) {
-        boolean checkIfFirstChatIsO=str.charAt(0)=='o';
-        boolean checkIfSecondChatIsZ=str.charAt(1)=='z';
-        if (checkIfFirstChatIsO && checkIfSecondChatIsZ)
-        {
-            return "oz";
+        if(!str.isEmpty()) {
+            boolean checkIfFirstChatIsO = str.charAt(0) == 'o';
+            boolean checkIfSecondChatIsZ =str.length()>1 && str.charAt(1) == 'z';
+            if (checkIfFirstChatIsO && checkIfSecondChatIsZ)
+            {
+                return "oz";
+            }
+            else if (checkIfFirstChatIsO)
+            {
+                return "o";
+            }
+            else if (checkIfSecondChatIsZ)
+            {
+                return "z";
+            }
+            else return "There is no (o,z) in the word";
         }
-        else if (checkIfFirstChatIsO)
-        {
-           return "o";
-        }
-        else if (checkIfSecondChatIsZ)
-        {
-            return "z";
-        }
-        else return "There is no (o,z) in the word";
+
+        else return "The word is empty";
 
     }
 }
