c01e9429db6f9a68ee55a064ac3cb0aefe4dad41
EchoCoder20 <albusaidi1915@gmail.com>
2025-07-10 23:46:11 +0400

    Modifed the code logic

--- Horiya_Albusaidi/MixStart.java
+++ Horiya_Albusaidi/MixStart.java
@@ -1,14 +1,17 @@
 public class MixStart {
     public static Boolean mixStart(String str)
     {
-        String beginOfString=str.substring(1,3);
-        if(beginOfString.equals("ix"))
+
+        if(str.length()>=3)
         {
-            return true;
+            String beginOfString=str.substring(1,3);
+            if(beginOfString.equals("ix"))
+            {
+                return true;
+            }
         }
-        else
-        {
-            return false;
-        }
+
+        return false;
     }
+
 }
