7c84a87857ac069f81c9875dc3df9153ea6d7b20
EchoCoder20 <albusaidi1915@gmail.com>
2025-07-10 12:35:08 +0400

    Modified code logic

--- Horiya_Albusaidi/Max1020.java
+++ Horiya_Albusaidi/Max1020.java
@@ -2,12 +2,13 @@ public class Max1020 {
     public static Integer max1020(int a, int b)
     {
         int result = 0;
-        if( (a>10 && a<20) && (b>10 && b<20) ) {
+        if( (a>10 && a<20) || (b>10 && b<20) ) {
             if(a>b)
                 result = a;
             else
                 result = b;
         }
+
         return result;
     }
 }
