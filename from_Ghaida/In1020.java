e9cb56708d7e225a071f1e840559e86aed3648a0
Ghaida02a <ggmskaj@gmail.com>
2025-07-10 14:22:20 +0400

    #TRA-701: In1020 task added

--- from_Ghaida/In1020.java
+++ from_Ghaida/In1020.java
@@ -1,8 +1,8 @@
 public class In1020{
 	public static  void main(String[] args){
 		System.out.println(in1020(12, 99)); //true
-		System.out.println(in1020(21, 12));//true
-		System.out.println(in1020(8, 99));//false
+		System.out.println(in1020(21, 12)); //true
+		System.out.println(in1020(8, 99)); //false
 	}
 	
 	public static boolean in1020(int a, int b){
