e67a2940f79a6bae20117d2a0a64f15baacdf904
NASSER1221 <alnassri1221@gmail.com>
2025-07-13 16:19:43 +0400

    added task files

---
+++ from_Nasser/LoneTeen.java
@@ -0,0 +1,16 @@
+public class LoneTeen {
+
+    public static boolean loneTeen(int a, int b) {
+
+        return (a >= 13 && a <= 19) ^ (b >= 13 && b <= 19);
+
+
+    }
+    public static void main(String[] args){
+
+        System.out.println(loneTeen(13,99));
+        System.out.println(loneTeen(21,19));
+        System.out.println(loneTeen(13,13));
+    }
+}
+
