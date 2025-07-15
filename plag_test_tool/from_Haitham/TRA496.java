d48f19a193294427f1bcebfdffb0c426a4e84277
haithemhamid <haithemalhinai2017@gmail.com>
2025-07-15 00:34:51 -0700

    change TRA496 task

--- plag_test_tool/from_Haitham/TRA496.java
+++ plag_test_tool/from_Haitham/TRA496.java
@@ -3,7 +3,7 @@ public static int diff21(int n) {
         int diff = 0;
         if (n <= 21) {
             diff = 21 - n;
-        } else if (n > 31) {
+        } else if (n > 21) {
             diff = 2 * (n - 21);
         }
         return diff;
