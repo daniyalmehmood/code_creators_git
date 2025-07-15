eb379417e1025b51da4d666bf9ce1cce52c653ba
haithemhamid <haithemalhinai2017@gmail.com>
2025-07-15 00:32:39 -0700

    change TRA695 task

--- plag_test_tool/from_Haitham/TRA695.java
+++ plag_test_tool/from_Haitham/TRA695.java
@@ -1,7 +1,7 @@
 public class TRA695{
 public static String front3(String str) {
         if (str.length() <= 2) {
-            return "the new string cannot be made";
+            return str+str+str;
         }
         return (str.substring(0, 3) + str.substring(0, 3) + str.substring(0, 3));
     }
