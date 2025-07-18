d7cca6ecf710714a98b1d2d80a38a8a0e6f50255
EchoCoder20 <albusaidi1915@gmail.com>
2025-07-18 15:38:08 +0400

    modify the code logic

--- Horiya_Albusaidi/In3050.java
+++ Horiya_Albusaidi/In3050.java
@@ -2,7 +2,9 @@ public class In3050 {
    public static Boolean in3050(int a, int b)
    {
 
-       if(a>= 30 && a <= 50  && b>= 30 && b <= 50)
+       if((a>= 30 && a <=40) && (b >= 30 && b <=40))
+           return true;
+       else if((a>= 40 && a <=50) && (b >= 40 && b <=50))
            return true;
        else return false;
    }
