d136f86970f7f8b27d8ee97ca6e0f456df422dbb
EchoCoder20 <albusaidi1915@gmail.com>
2025-07-18 15:46:06 +0400

    modify the code logic

--- Horiya_Albusaidi/IntMax.java
+++ Horiya_Albusaidi/IntMax.java
@@ -1,11 +1,11 @@
 public class IntMax {
-   public static Integer intMax(int a, int b, int c)
-   {
-       if(a>b && a>c)
-           return a;
-       else if (b>a && b>c) {
-           return b;
-       }
-       else {return c;}
-   }
+    public static Integer intMax(int a, int b, int c)
+    {
+        if(a>=b && a>=c)
+            return a;
+        else if (b>=a && b>=c) {
+            return b;
+        }
+        else {return c;}
+    }
 }
