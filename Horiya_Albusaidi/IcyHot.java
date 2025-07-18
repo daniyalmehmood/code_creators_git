3255dd72572849686e993e89a9360b4afb361023
EchoCoder20 <albusaidi1915@gmail.com>
2025-07-18 15:40:45 +0400

    modify code logic

--- Horiya_Albusaidi/IcyHot.java
+++ Horiya_Albusaidi/IcyHot.java
@@ -1,10 +1,10 @@
 public class IcyHot {
      public static Boolean icyHot(int temp1, int temp2)
      {
-         if(temp1 >0&& temp2>0 || temp1<0 && temp2<0)
+         if(temp1<0&& temp2>100 || temp1>100 && temp2<0)
          {
-             return false;
+             return true;
          }
-         else return true;
+         else return false;
      }
 }
