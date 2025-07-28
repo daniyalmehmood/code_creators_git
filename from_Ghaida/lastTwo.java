a1bc0d48e4f6dbd186fe689a7461497067930ae8
Ghaida02a <ggmskaj@gmail.com>
2025-07-28 16:11:45 +0400

    #TRA-1090: String-1. lastTwo added

---
+++ from_Ghaida/lastTwo.java
@@ -0,0 +1,13 @@
+public class lastTwo {
+    public static void main(String[] args) {
+        System.out.println(lastTwo("coding")); //codign
+        System.out.println(lastTwo("cat")); //cta
+        System.out.println(lastTwo("ab")); //ba
+    }
+    public static String lastTwo(String str){
+        if(str.length() < 2){
+            return str;
+        }
+        return str.substring(0, str.length() - 2) + str.charAt(str.length()-1) + str.charAt(str.length()-2) ;
+    }
+}
