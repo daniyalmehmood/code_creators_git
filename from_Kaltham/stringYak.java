ac31044ae3eb8e4d07032ee3f33df08cb773a288
Kaltham <kaltham.aldifai@gmail.com>
2025-07-28 15:26:34 +0400

    The solution for stringYak Task

---
+++ from_Kaltham/stringYak.java
@@ -0,0 +1,15 @@
+public class JavaFundamental {
+    public String removeYakSubstrings(String str) {
+        StringBuilder newstr=new StringBuilder(str);
+        for(int i=0;i<newstr.length()-2;) {
+                if (newstr.substring(i, i + 3).equals("yak")) {
+                    newstr.delete(i, i + 3);
+                }
+                else{
+                    i++;
+                }
+        }
+        return newstr.toString();
+    }
+
+}
