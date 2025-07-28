155be444f53d5cac05b22b30ec35541c218340f6
EchoCoder20 <albusaidi1915@gmail.com>
2025-07-28 14:48:27 +0400

    Solving StringBits Task

---
+++ Horiya_Albusaidi/Warmup2/StringBit.java
@@ -0,0 +1,14 @@
+package Warmup2;
+
+public class StringBit {
+    public String stringBits(String str) {
+        String result="";
+
+        for(int i=0;i<str.length();i+=2)
+        {
+            result+=str.charAt(i);
+        }
+        return result;
+    }
+
+}
