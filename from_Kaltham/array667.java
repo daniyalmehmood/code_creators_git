dae0890f5edee07f1cfbc246f1aabaf8cfc3294e
Kaltham <kaltham.aldifai@gmail.com>
2025-07-28 15:33:26 +0400

    The solution for array667 Task

---
+++ from_Kaltham/array667.java
@@ -0,0 +1,12 @@
+public class JavaFundamental {
+    public int count66or67Pairs(int[] nums) {
+        int count=0;
+          for(int i=0;i<nums.length-1;i++){
+              if (nums[i]==6 && nums[i+1]==6||nums[i]==6 && nums[i+1]==7) {
+                  count++;
+              }
+          }
+          return count;
+    }
+
+}
