6f9b5c2bf4b53ae01e43d3f5a9b28ae1d20ab629
Hawraa <hawra8516@gmail.com>
2025-07-28 14:55:40 +0400

    String-1: helloName added

---
+++ from_Hawraa/HelloName.java
@@ -0,0 +1,11 @@
+public class HelloName {
+    public static String helloName(String name) {
+        return "Hello " + name + "!";
+    }
+
+    public static void main(String[] args){
+        System.out.println(helloName("Bob"));
+        System.out.println(helloName("Alice"));
+        System.out.println(helloName("X"));
+    }
+}
