2302689f91838b7504694cf6fc4f07ec717d4989
Kaltham <kaltham.aldifai@gmail.com>
2025-07-28 11:20:05 +0400

    This is countXX Task

---
+++ from_Kaltham/countXX.java
@@ -0,0 +1,29 @@
+public class JavaFundamental {
+public int countCharX(String str) {
+        int count = 0;
+        if (str.contains(" ")) {
+            int space = str.indexOf(" ");
+            String firstWord = str.substring(0, space);
+            for (int i = 0; i < firstWord.length(); i++) {
+                if (firstWord.charAt(i) == ('x')) {
+                    count++;
+                }
+            }
+            return count;
+        } else if (!str.isEmpty()) {
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == ('x')) {
+                    count++;
+                }
+            }
+            if(count>0){
+                return count - 1;
+            }
+            return count;
+        }
+        else{
+            return 0;
+        }
+
+    }
+}
