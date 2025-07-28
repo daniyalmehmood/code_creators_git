9ea5941d21c325f9cfcc0f18d58a1cd687ef43c4
EchoCoder20 <albusaidi1915@gmail.com>
2025-07-28 15:49:00 +0400

    Solving NoTriples Task

---
+++ Horiya_Albusaidi/Warmup2/noTriples.java
@@ -0,0 +1,16 @@
+package Warmup2;
+
+public class NoTriples {
+    public boolean noTriples(int[] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i + 2 < nums.length && nums[i] == nums[i + 1] && nums[i] == nums[i + 2])
+                return false;
+
+        }
+        return true;
+
+    }
+
+
+}
