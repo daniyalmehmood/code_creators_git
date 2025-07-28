2f52571f68ba5d14e4115a6ddaf7fee1d90f72a0
Kaltham <kaltham.aldifai@gmail.com>
2025-07-28 10:55:26 +0400

    This is stingTimes Task

---
+++ from_Kaltham/stringTimes.java
@@ -0,0 +1,9 @@
+public class JavaFundamental {
+    public String stringDouble(String str, int n) {
+        StringBuilder str2=new StringBuilder();
+        for(int i=0; i<n; i++){
+            str2.append(str);
+        }
+        return str2.toString();
+    }
+}
