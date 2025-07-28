90a8c647960aadcf9bba432fc044d68b075e40d9
Ghaida02a <ggmskaj@gmail.com>
2025-07-28 15:58:26 +0400

    #TRA-1087: String-1: atFirst added and deleted hasBad task from #TRA-1087-atFirst branch

---
+++ from_Ghaida/atFirst.java
@@ -0,0 +1,18 @@
+public class atFirst {
+    public static void main(String[] args) {
+        System.out.println(atFirst("hello")); //"he"
+        System.out.println(atFirst("hi")); //"he"
+        System.out.println(atFirst("h")); //"he"
+    }
+    public static String atFirst(String str) {
+        if (str.length() >= 2) {
+            return str.substring(0, 2);
+        }
+        else if (str.length() == 1) {
+            return str + "@";
+        }
+        else {
+            return "@@";
+        }
+    }
+}
