443b45d4de3c614931ef5013b8339e33cdab78eb
EchoCoder20 <albusaidi1915@gmail.com>
2025-07-28 12:27:27 +0400

    Solving StringTimes Task

---
+++ Horiya_Albusaidi/Warmup2/stringTimes.java
@@ -0,0 +1,18 @@
+//package Warmup2;
+//
+//public class stringTimes {
+//    public String stringTimesResult(String str, int n) {
+//        String newString="";
+//        if(n>=0 && !str.isEmpty())
+//        {
+//            for(int i=0;i<n;i++)
+//            {
+//                newString+=str;
+//            }
+//            return newString;
+//        }
+//        else
+//            return "";
+//
+//    }
+//}
