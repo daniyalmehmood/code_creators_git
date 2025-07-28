c53c3d7f97b98cb63f0e20011becca581e8cdad4
EchoCoder20 <albusaidi1915@gmail.com>
2025-07-28 09:00:23 +0400

    Modified the return type

--- Horiya_Albusaidi/HasTeen.java
+++ Horiya_Albusaidi/HasTeen.java
@@ -1,5 +1,5 @@
 public class HasTeen {
-    public static Boolean hasTeen(int a, int b, int c)
+    public static boolean hasTeen(int a, int b, int c)
     {
         return (a >= 13 && a <= 19) || (b >= 13 && b <= 19) || (c >= 13 && c <= 19);
     }
