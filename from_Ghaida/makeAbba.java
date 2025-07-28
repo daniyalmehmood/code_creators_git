65f36f2ebbac62090bfa5f35d99f678a6fa1f32d
Ghaida02a <ggmskaj@gmail.com>
2025-07-28 14:46:25 +0400

    #TRA-1068: String-1. makeAbba added

---
+++ from_Ghaida/makeAbba.java
@@ -0,0 +1,10 @@
+public class makeAbba {
+    public static void main(String[] args) {
+        System.out.println(makeAbba("Hi", "Bye")); //"HiByeByeHi"
+        System.out.println(makeAbba("Yo", "Alice")); //"YoAliceAliceYo"
+        System.out.println(makeAbba("What", "Up")); //"WhatUpUpWhat"
+    }
+    public static String makeAbba(String a, String b) {
+        return a + b + b + a;
+    }
+}
