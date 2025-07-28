fdc3aa60ebb4f98e2602dbbf468677870742868a
Ghaida02a <ggmskaj@gmail.com>
2025-07-28 15:20:23 +0400

    #TRA-1080: String-1. withouEnd2 added

--- from_Ghaida/withouEnd2.java
+++ from_Ghaida/withouEnd2.java
@@ -1,7 +1,7 @@
-public class withoutEnd {
-    public static void main(String[] args){
-        System.out.println(withouEnd2("Hello")); // "ell"
-        System.out.println(withouEnd2("abc")); //"b"
+public class withouEnd2 {
+    public static void main(String[] args) {
+        System.out.println(withouEnd2("Hello")); //"ell"
+        System.out.println(withouEnd2("abc"));//"b"
         System.out.println(withouEnd2("ab")); //""
     }
     public static String withouEnd2(String str) {
