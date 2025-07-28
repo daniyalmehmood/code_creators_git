10e60b05a2ed737fa502041b5a361587db2243de
Ghaida02a <ggmskaj@gmail.com>
2025-07-28 15:30:22 +0400

    #TRA-1082: String-1. endsLy added

---
+++ from_Ghaida/endsLy.java
@@ -0,0 +1,13 @@
+public class endsLy {
+    public static void main(String[] args){
+        System.out.println(endsLy("oddly"));// → true
+        System.out.println(endsLy("y")); // → false
+        System.out.println(endsLy("oddy")); // → false
+    }
+    public static boolean endsLy(String str) {
+        if(str.length() >= 2){
+            return str.substring(str.length() - 2).equals("ly");
+        }
+        return false;
+    }
+}
