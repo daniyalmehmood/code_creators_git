51af989ee24e4e8fc97fe7017c799192f0e77ac3
Hawraa <hawra8516@gmail.com>
2025-07-28 10:36:16 +0400

    Warmup-2: StringBits added

---
+++ from_Hawraa/StringBits.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.net.SocketTimeoutException;
+
+public class StringBits {
+    public static String stringBits(String str) {
+        String newStr = "";
+        for (int i =0; i<str.length(); i+=2){
+            newStr = newStr + str.substring(i, i+1);
+        }
+        return newStr;
+    }
+    public static void main(String[] args){
+        System.out.println(stringBits("Hello"));
+        System.out.println(stringBits("Hi"));
+        System.out.println(stringBits("Heeololeo"));
+    }
+}
