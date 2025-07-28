a611ad123379bfb7c13db7e14bb8f1e6c789021b
EchoCoder20 <albusaidi1915@gmail.com>
2025-07-28 09:58:57 +0400

    Modifed the return type and the return logic

--- Horiya_Albusaidi/LastDigit.java
+++ Horiya_Albusaidi/LastDigit.java
@@ -1,11 +1,8 @@
 public class LastDigit {
-   public static Boolean lastDigit(int a, int b)
+   public static boolean lastDigit(int a, int b)
     {
         int lastDigitOfA = a % 10;
         int lastDigitOfB = b % 10;
-        if(lastDigitOfA==lastDigitOfB)
-            return true;
-        else
-            return false;
+        return lastDigitOfA == lastDigitOfB;
     }
 }
