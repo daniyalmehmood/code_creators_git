6683082c4a8b5b51f03e4ef6cd654f43b346524f
Kaltham <kaltham.aldifai@gmail.com>
2025-07-28 15:14:21 +0400

    The solution for stringX Task

---
+++ from_Kaltham/stringX.java
@@ -0,0 +1,19 @@
+public class JavaFundamental {
+    public  String removeInnerX(String str) {
+        if(str.length()<2 || str.isEmpty()){
+            return str;
+        }
+        String front=str.substring(0,1);
+        String back=str.substring(str.length()-1);
+        String str3=str.substring(1,str.length()-1);
+        StringBuilder str1=new StringBuilder(str3);
+        StringBuilder newstr= new StringBuilder();
+        for(int i=0;i<str1.length();i++){
+            if(str1.charAt(i)!='x'){
+                newstr.append(str1.charAt(i));
+            }
+        }
+
+        return front+newstr.toString()+back;
+    }
+}
