1422ad66e4aa65d06394ff27d97f23e87d479295
Ghaida02a <ggmskaj@gmail.com>
2025-07-28 16:10:00 +0400

    #TRA-1089: String-1. conCat added

---
+++ from_Ghaida/conCat.java
@@ -0,0 +1,16 @@
+public class conCat {
+    public static void main(String[] args) {
+        System.out.println(conCat("abc", "cat")); //"abcat"
+        System.out.println(conCat("dog", "cat")); //"dogcat"
+        System.out.println(conCat("abc", "")); //"abc"
+    }
+    public static String conCat(String a, String b) {
+        if(a.isEmpty() || b.isEmpty()){
+            return a + b;
+        }
+        else if(a.charAt(a.length() -1) == b.charAt(0)){
+            return a + b.substring(1);
+        }
+        return a + b;
+    }
+}
