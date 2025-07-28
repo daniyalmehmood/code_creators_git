f322e0b612c9ed895e917df0b9ae28532d1e771f
Kaltham <kaltham.aldifai@gmail.com>
2025-07-28 14:37:38 +0400

    The solution for last2 Task

---
+++ from_Kaltham/last2.java
@@ -0,0 +1,19 @@
+public class JavaFundamental {
+    public int countLast2Substring(String str) {
+        int count = 0;
+        if(str.length()>1) {
+            String str1 = str.substring(str.length() - 2);
+            for (int i = 0; i < str.length() - 2; i++) {
+                if (str.substring(i, i + 2).equals(str1)) {
+                    count++;
+                }
+            }
+            return count;
+        }
+        else{
+            return 0;
+        }
+    }
+
+
+}
