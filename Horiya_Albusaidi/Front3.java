de09849374bed1379436792b14448bfb7edb1ede
EchoCoder20 <albusaidi1915@gmail.com>
2025-07-28 10:13:20 +0400

    Modified the initialized line of the firstThreeChar veriable

--- Horiya_Albusaidi/Front3.java
+++ Horiya_Albusaidi/Front3.java
@@ -3,11 +3,9 @@ public class Front3 {
     public static String front3(String str)
     {
         StringBuilder builder=new StringBuilder();
-        String firstThreeChar="";
-        String newString="";
         if(str.length()>=3)
         {
-            firstThreeChar=str.substring(0,3);
+            String firstThreeChar=str.substring(0,3);
             builder.append(firstThreeChar.repeat(3));
         }
         else{
