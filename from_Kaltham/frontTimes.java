a776f60d175b05be39cf1ddde6fc7951f694df0b
Kaltham <kaltham.aldifai@gmail.com>
2025-07-28 11:03:29 +0400

    This is frontTimes Task

---
+++ from_Kaltham/frontTimes.java
@@ -0,0 +1,18 @@
+public class JavaFundamental {
+public String stringFrontDouble(String str, int n) {
+        StringBuilder str3 = new StringBuilder();
+        if (str.length() < 3) {
+            for (int i = 0; i < n; i++) {
+                str3.append(str);
+            }
+            return str3.toString();
+        }
+        else{
+            String str2 = str.substring(0, 3);
+            for (int i = 0; i < n; i++) {
+                str3.append(str2);
+            }
+            return str3.toString();
+        }
+    }
+}
