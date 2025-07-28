9fd5c9f8b0cc3860d20f38e251ae4e8cf752b594
EchoCoder20 <albusaidi1915@gmail.com>
2025-07-28 09:21:41 +0400

    Modified the code

--- Horiya_Albusaidi/Front22.java
+++ Horiya_Albusaidi/Front22.java
@@ -2,10 +2,10 @@ public class Front22 {
     public static String front22(String str)
     {
         String newString = "";
-        if(str.length()>2){
+        if(str.length()>=2){
             char firstChar = str.charAt(0);
             char SecondChar = str.charAt(1);
-            String addString=firstChar+""+SecondChar;
+            String addString = String.valueOf(firstChar) + SecondChar;
             newString = addString+str+addString;
             return newString;
         }
