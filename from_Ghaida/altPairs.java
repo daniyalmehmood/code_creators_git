6fb9305dad325274c16a9b8596358597c485dbde
Ghaida02a <ggmskaj@gmail.com>
2025-07-28 11:03:40 +0400

    #TRA-1005: warm-up 2. altPairs added

---
+++ from_Ghaida/altPairs.java
@@ -0,0 +1,17 @@
+public class altPairs {
+    public static void main(String[] args) {
+        System.out.println(altPairs("kitten")); //"kien"
+        System.out.println(altPairs("Chocolate")); //"Chole"
+        System.out.println(altPairs("CodingHorror")); //"Congrr"
+    }
+    public static String altPairs(String str) {
+        StringBuilder firstTwoIndex = new StringBuilder();
+        for(int i = 0; i < str.length(); i += 4){
+            firstTwoIndex.append(str.charAt(i));
+            if(i + 1 < str.length()){
+                firstTwoIndex.append(str.charAt(i+1));
+            }
+        }
+        return firstTwoIndex.toString();
+    }
+}
