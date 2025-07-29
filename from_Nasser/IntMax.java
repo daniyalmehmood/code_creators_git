e457ae1d63a4eaef9f29f73d91cdb72817e1d3c1
NASSER1221 <alnassri1221@gmail.com>
2025-07-29 18:01:01 +0400

    Update IntMax.java

--- from_Nasser/IntMax.java
+++ from_Nasser/IntMax.java
@@ -2,23 +2,7 @@ public class IntMax {
 
     public static int intMax(int a ,int b, int c){
 
-        if( a>b && a>c){
-
-            return a;
-
-        }
-
-        else if( b>a && b>c){
-
-            return b;
-        }
-
-        else if (c>a && c>b){
-
-            return c;
-        }
-
-        else return 0;
+        return Math.max(a, Math.max(b, c));
 
     }
 
