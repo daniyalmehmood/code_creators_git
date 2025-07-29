a16426a6b96e9eeeb4bc4a3758b97c0c5720ca69
EchoCoder20 <albusaidi1915@gmail.com>
2025-07-29 11:52:44 +0400

    Change the directory name

--- From_Horiya_Albusaidi/AbsoluteDifference.java
+++ From_Horiya_Albusaidi/AbsoluteDifference.java
@@ -1,12 +1,9 @@
 public class AbsoluteDifference {
-    public static Integer diff21(int n)
-    {
+    public static int diff21(int n) {
         int result;
-        if(n>21)
-        {
-            result = 2*(n-21);
-        }
-        else result = 21-n;
+        if (n > 21) {
+            result = 2 * (n - 21);
+        } else result = 21 - n;
         return result;
     }
 }
