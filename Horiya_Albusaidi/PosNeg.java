968239cd6ce9d68df85ede05dc7cc577a0c3bb01
EchoCoder20 <albusaidi1915@gmail.com>
2025-07-29 12:27:18 +0400

    Change file path

--- Horiya_Albusaidi/PosNeg.java
+++
@@ -1,9 +0,0 @@
-public class PosNeg {
-    public static  Boolean posNegs(int a, int b, boolean negative)
-    {
-        if(a>=0 && b<0 && !negative) return true;
-        else if(a<0 && b>=0 && !negative)return true;
-        else if(a<0 && b<0 && negative)return true;
-        else return false;
-    }
-}
