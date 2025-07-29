11166c6547804a2a6c13def8b8e86db89ecd9fc8
EchoCoder20 <albusaidi1915@gmail.com>
2025-07-29 09:07:18 +0400

    Modified the code logic

--- Horiya_Albusaidi/Warmup2/stringYak.java
+++ Horiya_Albusaidi/Warmup2/stringYak.java
@@ -6,7 +6,7 @@ public class stringYak {
         int[] index=new int[str.length()];
         for(int i=0;i<str.length();i++)
         {
-            if (i+2<str.length() && str.charAt(i) == 'y' && str.charAt(i + 1) == 'a' && str.charAt(i + 2) == 'k') {
+            if (i+2<str.length() && str.charAt(i) == 'y' &&  str.charAt(i + 2) == 'k') {
                 i=i+2;
 
             }
