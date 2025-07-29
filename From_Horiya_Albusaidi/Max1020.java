4bb275c2f68d52832c3d7c0e2544a3a21a256b80
EchoCoder20 <albusaidi1915@gmail.com>
2025-07-29 14:32:19 +0400

    Change the file path

---
+++ From_Horiya_Albusaidi/Max1020.java
@@ -0,0 +1,13 @@
+public class Max1020 {
+    public static int max1020(int a, int b) {
+        if ((a >= 10 && a <= 20) && (b >= 10 && b <= 20)) {
+            if (a > b)
+                return a;
+            else
+                return b;
+        } else if (a >= 10 && a <= 20) {
+            return a;
+        } else if (b >= 10 && b <= 20) {
+        else return 0;
+        }
+    }
