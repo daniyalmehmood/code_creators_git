53b282c7734b4871986287e1f4444a5e12bc5260
NASSER1221 <alnassri1221@gmail.com>
2025-07-29 18:28:25 +0400

    Update MissingChar.java

--- from_Nasser/MissingChar.java
+++ from_Nasser/MissingChar.java
@@ -1,18 +1,22 @@
 public class MissingChar {
 
     public static String missingChar(String str, int n){
-        char[]c=str.toCharArray();
+        if(!str.isEmpty()&& str.length()>n) {
+                char[] c = str.toCharArray();
 
-        StringBuilder letterDeleted= new StringBuilder();
-        for (int i=0;i< c.length;i++){
+                StringBuilder letterDeleted = new StringBuilder();
+                for (int i = 0; i < c.length; i++) {
 
-            if (i!=n){
-                letterDeleted.append(c[i]);
+                    if (i != n) {
+                        letterDeleted.append(c[i]);
 
+                    }
+
+
+                }
+                return letterDeleted.toString();
             }
-
-
-         }return letterDeleted.toString();
+            else return str;
     }
     public static void main(String[] args){
 
