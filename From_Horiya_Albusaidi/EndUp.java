b1260a8abdc02f46a5fddd5dab9dd52c33681f9d
EchoCoder20 <albusaidi1915@gmail.com>
2025-07-29 12:39:18 +0400

    Change the file path

--- From_Horiya_Albusaidi/EndUp.java
+++ From_Horiya_Albusaidi/EndUp.java
@@ -1,11 +1,8 @@
 public class EndUp {
-    public static String endUp(String str)
-    {
-        if(str.length()<=3)
-        {
+    public static String endUp(String str) {
+        if (str.length() <= 3) {
             return str.toUpperCase();
-        }
-        else {
+        } else {
             String lastThreeChar = str.substring(str.length() - 3);
             String firstChars = str.substring(0, str.length() - 2);
             String toUpperCase = lastThreeChar.toUpperCase();
