2b821060790755117434ecd6482a1d17e86a0906
NASSER1221 <alnassri1221@gmail.com>
2025-07-29 17:46:51 +0400

    Update EndUp.java

--- from_Nasser/EndUp.java
+++ from_Nasser/EndUp.java
@@ -3,7 +3,7 @@ import java.util.Locale;
 public class EndUp {
 
     public static String endUp( String str){
-
+        if(str.length()<3)return str.toUpperCase();
         String s1=str.substring(0,str.length()-4);
         String s2=str.substring(str.length()-4,str.length()-1).toUpperCase();
         return s1+s2;
