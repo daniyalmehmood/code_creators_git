2fb8fa94a1ff5447954426a58cb610ef3c651b96
NASSER1221 <alnassri1221@gmail.com>
2025-07-29 17:41:55 +0400

    Update EveryNth.java

--- from_Nasser/EveryNth.java
+++ from_Nasser/EveryNth.java
@@ -1,15 +1,18 @@
 public class EveryNth {
 
-    public static String everyNth(String str , int n){
-        StringBuilder String1=new StringBuilder();
-        for (int i=0;i<str.length();i+=n){
+   ublic static String everyNth(String str, int n) {
+        StringBuilder String1 = new StringBuilder();
+        if (!str.isEmpty() && n >= 1) {
+            for (int i = 0; i < str.length(); i += n) {
 
-            char [] w=str.toCharArray();
-            String1.append(w[i]);
+                char[] w = str.toCharArray();
+                String1.append(w[i]);
 
-        }
-        return String1.toString();
+            }
+            return String1.toString();
+        } else return "";
     }
+    
 
     public static void main(String[] args){
 
