3816534e59ad193313db6bea9568893b90dcbba3
NASSER1221 <alnassri1221@gmail.com>
2025-07-29 18:06:41 +0400

    Update StartOz.java

--- from_Nasser/StartOz.java
+++ from_Nasser/StartOz.java
@@ -2,18 +2,12 @@ public class StartOz {
 
     public static String startOz(String str) {
 
-        if (str.substring(0,1).equals("o") && !str.substring(1, 2).equals("z")) {
-            return "o";
+        String result = "";
 
-        } else if (str.substring(0, 2).equals("oz")) {
-            return "oz";
+    if (str.length()>=1 && str.charAt(0)=='o') result += "o";
+    if (str.length()>=2 && str.charAt(1)=='z') result += "z";
 
-        } else if (str.substring(1, 2).equals("z")) {
-
-            return "z";
-
-        }
-        else return "";
+    return result;
     }
 
     public static void main(String[] args){
