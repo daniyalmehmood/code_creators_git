57de72032494c4df82c8d4d6229f0c92566a5ad6
NASSER1221 <alnassri1221@gmail.com>
2025-07-29 18:09:57 +0400

    Update IcyHot.java

--- from_Nasser/IcyHot.java
+++ from_Nasser/IcyHot.java
@@ -2,14 +2,7 @@ public class IcyHot {
 
     public static boolean icyHot(int temp1, int temp2){
 
-        if( temp1>100||temp2<0) {
-            return true;
-
-        } else if (temp1<0||temp2>100) {
-            return true;
-
-        }
-        else return false;
+        return (temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0);
 
         }
 
