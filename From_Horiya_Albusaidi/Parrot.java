ca24cd6667c5f1779aa7ddebc0302416e6dc51b0
EchoCoder20 <albusaidi1915@gmail.com>
2025-07-29 12:07:05 +0400

    Modifiy the method return type

--- From_Horiya_Albusaidi/Parrot.java
+++ From_Horiya_Albusaidi/Parrot.java
@@ -1,9 +1,6 @@
 public class Parrot {
-    public  static Boolean parrotTrouble(boolean talking, int hour)
-    {
-        boolean result;
-        if(talking && (hour<7 || hour>20 )) result = true;
-        else result = false;
-        return result;
+    public static boolean parrotTrouble(boolean talking, int hour) {
+        result talking &&(hour < 7 || hour > 20));
+
     }
 }
