73f613f5972540bd0e7a32b851338c3c8407dcf1
Kaltham <kaltham.aldifai@gmail.com>
2025-07-30 15:31:11 +0400

    modified CountMatchingSubstrings Task

--- from_Kaltham/CountMatchingSubstrings.java
+++ from_Kaltham/CountMatchingSubstrings.java
@@ -1,8 +1,8 @@
-public class JavaFundamental {
+public class CountMatchingSubstrings {
     public int countMatchingSubstrings(String a, String b) {
-        int count=0;
-        int lenght=Math.min(a.length(),b.length());
-        for(int i=0;i<lenght-1;i++) {
+        int count = 0;
+        int lenght = Math.min(a.length(), b.length());
+        for (int i = 0; i < lenght - 1; i++) {
             if (a.substring(i, i + 2).equals(b.substring(i, i + 2))) {
                 count++;
             }
