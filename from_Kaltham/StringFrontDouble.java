baaa06cb02979ac99280d2b38a92cb39c916b276
Kaltham <kaltham.aldifai@gmail.com>
2025-07-30 14:36:37 +0400

    modified StringFrontDouble Task

--- from_Kaltham/StringFrontDouble.java
+++ from_Kaltham/StringFrontDouble.java
@@ -1,13 +1,12 @@
-public class JavaFundamental {
-public String stringFrontDouble(String str, int n) {
+public class StringFrontDouble {
+    public String stringFrontDouble(String str, int n) {
         StringBuilder str3 = new StringBuilder();
         if (str.length() < 3) {
             for (int i = 0; i < n; i++) {
                 str3.append(str);
             }
             return str3.toString();
-        }
-        else{
+        } else {
             String str2 = str.substring(0, 3);
             for (int i = 0; i < n; i++) {
                 str3.append(str2);
