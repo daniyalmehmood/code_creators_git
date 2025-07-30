012c006c9301e6508facfaf7b0e66a0cdfe96321
NASSER1221 <alnassri1221@gmail.com>
2025-07-30 14:30:20 +0400

    added the task DeFront

---
+++ from_Nasser/DeFront.java
@@ -0,0 +1,23 @@
+public class DeFront {
+
+    public String deFront(String str) {
+
+        String result="";
+
+        if (str.charAt(0)=='a'){
+            result+="a";
+        }
+
+        if (str.charAt(1)=='b'){
+            result+='b';
+        }
+
+        if(str.length()>2){
+            result+=str.substring(2);
+
+        }
+        return result;
+    }
+}
+
+
