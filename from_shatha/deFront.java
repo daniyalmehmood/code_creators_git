8393e758418628321b23f32a4311a5fdd2b0ebae
Shatha Almamari <shatha.almamari99@gmail.com>
2025-07-30 16:13:36 +0400

    deFront added

---
+++ from_shatha/deFront.java
@@ -0,0 +1,24 @@
+public class deFront {
+    public static String deFront(String str) {
+        String result = "";
+
+        if (str.length() >= 1 && str.charAt(0) == 'a') {
+            result += 'a';
+        }
+
+        if (str.length() >= 2 && str.charAt(1) == 'b') {
+            result += 'b';
+        }
+
+        if (str.length() > 2) {
+            result += str.substring(2);
+        }
+
+        return result;
+    }
+    public static void main(String[] args) {
+        System.out.println(deFront("Hello"));
+        System.out.println(deFront("java"));
+        System.out.println(deFront("away"));
+    }
+}
