3015d6c4e7db3e72c27472cfd4ed67fc2411f3b1
Ghaida02a <ggmskaj@gmail.com>
2025-07-30 16:15:35 +0400

    #TRA-973: StringTimes class name changed

--- from_Ghaida/stringTimes.java
+++ from_Ghaida/stringTimes.java
@@ -1,4 +1,4 @@
-public class stringTimes {
+public class StringTimes {
     public static void main(String[] args) {
         System.out.println(stringTimes("Hi", 2)); //"HiHi"
         System.out.println(stringTimes("Hi", 3)); //"HiHiHi"
