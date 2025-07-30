5a331e66e56135b74bfb02cef2e9cffcbde9b176
haithemhamid <haithemalhinai2017@gmail.com>
2025-07-30 12:05:15 +0400

    altPairs task is done

---
+++ from_Haitham/altPairs.java
@@ -0,0 +1,12 @@
+public class altPairs {
+public String altPairs(String str) {
+        String newString = "";
+        int index = 0;
+        while (index < str.length()) {
+            if (index + 2 > str.length()) newString += str.substring(index);
+            else newString += str.substring(index, index + 2);
+            index += 4;
+        }
+        return newString;
+    }
+	}
