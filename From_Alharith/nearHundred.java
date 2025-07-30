060dae5806d75bc2bbe66ecac0b8e2f478c2174c
hjufaili <gakhanaljufaily@gmail.com>
2025-07-30 09:01:30 +0400

    #TRA-1292: WarmUp1_nearHundred Done

---
+++ From_Alharith/nearHundred.java
@@ -0,0 +1,10 @@
+public class nearHundred {
+
+    public boolean nearHundred(int n) {
+        if (Math.abs(100 - n) <= 10 || Math.abs(200 - n) <= 10) {
+            return true;
+        }
+        return false;
+    }
+
+}
