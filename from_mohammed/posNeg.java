d453bf87729b79b9e3dcef84e614234b3c0dfcf8
mohammedanad <alhuseini.moha@gmail.com>
2025-07-30 11:56:44 +0400

    Warmup-1: #TRA-1615 done

---
+++ from_mohammed/posNeg.java
@@ -0,0 +1,8 @@
+public class posNeg {
+
+    //posNeg
+    public boolean posNeg(int a, int b, boolean negative) {
+        if(negative){
+            return (a<0 && b<0);}
+        return (a * b < 0);}
+}
