65a1eed7b234fee8e869b1a5647d8da7f2e2ba63
hjufaili <gakhanaljufaily@gmail.com>
2025-07-30 12:07:14 +0400

    #TRA-1355: WarmUp2_noTriples Done

---
+++ From_Alharith/noTriples.java
@@ -0,0 +1,11 @@
+public class noTriples {
+
+    public boolean noTriples(int[] nums) {
+        for (int i = 0; i < nums.length - 2; i++) {
+            int number = nums[i];
+
+            if (nums[i + 1] == number && nums[i + 2] == number) return false;
+        }
+        return true;
+    }
+}
