820a2f37c0d473869420643d66cb92b2a07f62a3
Kaltham <kaltham.aldifai@gmail.com>
2025-07-30 14:58:50 +0400

    modified BuildGrowingString Task

--- from_Kaltham/BuildGrowingString.java
+++ from_Kaltham/BuildGrowingString.java
@@ -1,11 +1,10 @@
-public class JavaFundamental {
+public class BuildGrowingString {
     public String buildGrowingString(String str) {
-        StringBuilder str1=new StringBuilder();
+        StringBuilder str1 = new StringBuilder();
         for (int i = 0; i < str.length(); i++) {
-            str1.append(str.substring(0,i+1));
+            str1.append(str.substring(0, i + 1));
         }
         return str1.toString();
     }
-
 
 }
