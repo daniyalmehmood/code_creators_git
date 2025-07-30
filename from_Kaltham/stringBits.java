0cf96de19193623aa2a53bace033c1d1942c2587
Kaltham <kaltham.aldifai@gmail.com>
2025-07-30 14:55:02 +0400

    modified  GetEvenIndexChars Task

--- from_Kaltham/stringBits.java
+++
@@ -1,9 +0,0 @@
-public class JavaFundamental {
-    public String getEvenIndexChars(String str) {
-        StringBuilder str1=new StringBuilder();
-      for(int i=0; i<str.length();i+=2){
-          str1.append(str.charAt(i));
-      }
-      return str1.toString();
-    }
-}
