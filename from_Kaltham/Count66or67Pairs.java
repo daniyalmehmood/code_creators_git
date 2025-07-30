1988d63efb19a84e2a88e0b5c99a04fc3263d7b3
Kaltham <kaltham.aldifai@gmail.com>
2025-07-30 15:51:56 +0400

    modified Count66or67Pairs Task

---
+++ from_Kaltham/Count66or67Pairs.java
@@ -0,0 +1,12 @@
+public class Count66or67Pairs {
+    public int count66or67Pairs(int[]nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == 6 && nums[i + 1] == 6 || nums[i] == 6 && nums[i + 1] == 7) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
