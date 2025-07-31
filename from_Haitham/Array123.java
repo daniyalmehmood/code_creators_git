42814710ffece86ab436ad7682d53b8d68a9b66a
haithemhamid <149400034+haithemhamid@users.noreply.github.com>
2025-07-31 14:31:48 +0400

    Update and rename array123.java to Array123.java

--- from_Haitham/Array123.java
+++ from_Haitham/Array123.java
@@ -1,4 +1,4 @@
-public class array123 {
+public class Array123 {
 public boolean array123(int[] nums) {
         for (int i = 0; i < nums.length - 2; i++)
             if (nums[i] == 1 && nums[i + 1] == 2 && nums[i + 2] == 3)
