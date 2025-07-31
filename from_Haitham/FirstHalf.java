207a35787979c521b45376f1156a774b16e07a12
haithemhamid <149400034+haithemhamid@users.noreply.github.com>
2025-07-31 14:53:04 +0400

    Update and rename firstHalf.java to FirstHalf.java

--- from_Haitham/FirstHalf.java
+++ from_Haitham/FirstHalf.java
@@ -1,4 +1,4 @@
-public class firstHalf {
+public class FirstHalf {
  public String firstHalf(String str) {
         int halfStringIndex = str.length() / 2;
         return str.substring(0, halfStringIndex);
