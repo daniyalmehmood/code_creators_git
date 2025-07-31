9ebc50a0791f2fea82de2c1c205e57e5d67699d9
Ghaida02a <ggmskaj@gmail.com>
2025-07-31 10:37:57 +0400

    #TRA-1011-has271: class name changed

--- from_Ghaida/Has271.java
+++ from_Ghaida/Has271.java
@@ -1,4 +1,4 @@
-public class has271 {
+public class Has271 {
     public static void main(String[] args) {
         System.out.println(has271(new int[] {1, 2, 7, 1})); // → true
         System.out.println(has271(new int[] {1, 2, 8, 1})); // → false
