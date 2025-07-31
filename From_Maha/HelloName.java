a09f923da966e0a5c68723134baa4009e466c7e0
Maha <mahathamood98@gmail.com>
2025-07-31 09:48:03 +0400

    Editing the file

--- From_Maha/HelloName.java
+++ From_Maha/HelloName.java
@@ -1,9 +1,4 @@
 public class HelloName {
-    public static void main(String[] args) {
-        System.out.println(helloName("Bob")); //"Hello Bob!"
-        System.out.println(helloName("Alice")); //"Hello Alice!"
-        System.out.println(helloName("X"));//"Hello X!"
-    }
     public static String helloName(String name) {
         return "Hello " + name + "!";
     }
