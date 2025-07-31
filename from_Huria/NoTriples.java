3ad99d9108d10a98e19fd74b062237809ce31422
Huria <alforihuria938@gmail.com>
2025-07-31 11:41:03 +0400

    code refactoring

--- from_Huria/NoTriples.java
+++ from_Huria/NoTriples.java
@@ -1,30 +1,28 @@
-public class NoTriples {
-    public static boolean noTriples(int[] nums) {
+public class HasNoTriples {
+    public static boolean hasNoTriples(int[] nums) {
 
         if (nums.length < 3) {
             return true;
         } else {
 
             for (int i = 0; i < nums.length - 1; i++) {
-                if (nums[i] == nums[i + 1] && nums[i + 1] == nums[i + 2]) return false;
+                if (nums[i] == nums[i + 1] && nums[i + 1] == nums[i + 2])
+                    return false;
             }
         }
-
         return true;
-
 
     }
 
-
     public static void main(String[] args) {
 
-        System.out.println(NoTriples.noTriples(new int[]{1, 1, 2, 2, 1}));
-        System.out.println(NoTriples.noTriples(new int[]{1, 1, 2, 2, 2, 1}));
-        System.out.println(NoTriples.noTriples(new int[]{1, 1, 1, 2, 2, 2, 1}));
-        System.out.println(NoTriples.noTriples(new int[]{1, 2, 1}));
-        System.out.println(NoTriples.noTriples(new int[]{1, 1}));
-        System.out.println(NoTriples.noTriples(new int[]{1}));
-        System.out.println(NoTriples.noTriples(new int[]{}));
+        System.out.println(hasNoTriples(new int[]{1, 1, 2, 2, 1}));
+        System.out.println(hasNoTriples(new int[]{1, 1, 2, 2, 2, 1}));
+        System.out.println(hasNoTriples(new int[]{1, 1, 1, 2, 2, 2, 1}));
+        System.out.println(hasNoTriples(new int[]{1, 2, 1}));
+        System.out.println(hasNoTriples(new int[]{1, 1}));
+        System.out.println(hasNoTriples(new int[]{1}));
+        System.out.println(hasNoTriples(new int[]{}));
 
     }
 }
