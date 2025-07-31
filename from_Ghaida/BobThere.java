d4a56ed37d55455f890a75ee27d74e49dfdda4e5
Ghaida02a <ggmskaj@gmail.com>
2025-07-31 12:27:56 +0400

    #TRA-1325-bobThere: class name changed

--- from_Ghaida/BobThere.java
+++ from_Ghaida/BobThere.java
@@ -1,4 +1,4 @@
-public class bobThere {
+public class BobThere {
     public static void main(String[] args) {
         System.out.println(bobThere("abcbob")); //true
         System.out.println(bobThere("b9b")); //true
