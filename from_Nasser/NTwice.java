2cb8af2ef6c0ff2249e9f3421a0a449911092911
NASSER1221 <alnassri1221@gmail.com>
2025-07-31 16:46:06 +0400

    added Ntwice task

---
+++ from_Nasser/NTwice.java
@@ -0,0 +1,12 @@
+public class NTwice {
+    public String nTwice(String str, int n) {
+
+        String first=str.substring(0,n);
+        String end=str.substring(str.length()-n);
+
+
+return first+end;
+    }
+
+
+}
