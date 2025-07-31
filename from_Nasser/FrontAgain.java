eb80fe3d20ed2d04d399b2119b14158595936aca
NASSER1221 <alnassri1221@gmail.com>
2025-07-31 11:47:10 +0400

    added FrontAgain task

---
+++ from_Nasser/FrontAgain.java
@@ -0,0 +1,21 @@
+public class FrontAgain {
+
+    public boolean frontAgain(String str) {
+
+        if (str.length() == 2) {
+            return true;
+        } else if (str.length() > 2) {
+
+            if (str.substring(0, 2).equals(str.substring(str.length() - 2))) {
+                return true;
+            }
+
+
+        }
+
+        return false;
+    }
+}
+
+
+
