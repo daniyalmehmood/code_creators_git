a4e1c226c123adb6439771908908426243989ed0
mohammedanad <alhuseini.moha@gmail.com>
2025-07-31 11:48:14 +0400

    #TRA-1641: Warmup-1 everyNth Done

--- from_mohammed/in1020.java
+++ from_mohammed/in1020.java
@@ -1,5 +1,5 @@
-public class in1020 {
-    public boolean In1020(int a, int b) {
+public class In1020 {
+    public boolean in1020(int a, int b) {
 
         if((a>=10&&a<=20)||(b>=10&&b<=20)){
             return true;}
