f4029db3c65a0c349c02fc34a569bf3c3ddb2e2a
NASSER1221 <alnassri1221@gmail.com>
2025-07-31 16:50:34 +0400

    added EndLsy task

---
+++ from_Nasser/ENDsly.java
@@ -0,0 +1,18 @@
+public class ENDsly {
+
+    public boolean endsLy(String str) {
+        if (str.length() >= 2) {
+
+            if (str.endsWith("ly")) {
+                return true;
+
+
+            } else return false;
+        }
+
+
+        return false;
+    }
+}
+
+
