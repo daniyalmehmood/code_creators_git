61245b57cb8522e3b09859c04c4aa62eaf8014f3
Mulk <mulkbadar774@gmail.com>
2025-07-31 12:20:39 +0400

    modified Array667 task

---
+++ From_Mulk/AltPairs.java
@@ -0,0 +1,14 @@
+ public class AltPairs{
+ public String altPairs(String str) {
+        StringBuilder str1 = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i += 4) {
+            str1.append(str.charAt(i));
+            if (i + 1 < str.length()) {
+                str1.append(str.charAt(i + 1));
+            }
+        }
+
+        return str1.toString();
+    }
+	}
