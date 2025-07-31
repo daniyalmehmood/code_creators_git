e8b2dc175dc4125ab5cc23470e69b8fa66cae9d4
mohammedanad-dev <alhuseini.moha@gmail.com>
2025-07-31 14:52:03 +0400

    Update and rename delDel.java to DelDel.java

--- from_mohammed/DelDel.java
+++ from_mohammed/DelDel.java
@@ -1,4 +1,4 @@
-public class delDel {
+public class DelDel {
     public String delDel(String str) {
         if(str.length()>=4 && str.substring(1,4).equals("del")){
             return str.substring(0,1)+str.substring(4);}
