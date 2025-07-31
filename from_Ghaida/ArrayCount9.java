c6865ec506dad1a4c76d1497bb252959d92ad95c
Ghaida02a <ggmskaj@gmail.com>
2025-07-31 10:13:51 +0400

    #TRA-997-arrayCount9: class name changed

--- from_Ghaida/ArrayCount9.java
+++ from_Ghaida/ArrayCount9.java
@@ -1,4 +1,4 @@
-public class arrayCount9 {
+public class ArrayCount9 {
     public static void main(String[] args) {
         System.out.println(arrayCount9(new int[] {1, 2, 9})); //1
         System.out.println(arrayCount9(new int[] {1, 9, 9})); //2
