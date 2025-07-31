cee0196793aab42be609eb28840850cc3a593c1f
Ghaida02a <ggmskaj@gmail.com>
2025-07-31 10:15:43 +0400

    #TRA-998-arrayFront9: class name changed

--- from_Ghaida/ArrayFront9.java
+++ from_Ghaida/ArrayFront9.java
@@ -1,4 +1,4 @@
-public class arrayFront9 {
+public class ArrayFront9 {
     public static void main(String[] args) {
         System.out.println(arrayFront9(new int[] {1, 2, 9, 3, 4})); //true
         System.out.println(arrayFront9(new int[] {1, 2, 3, 4, 9})); //false
