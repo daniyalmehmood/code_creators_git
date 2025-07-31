d00931ab97d72586661c82f1e4fe4c3fdacd145e
haithemhamid <149400034+haithemhamid@users.noreply.github.com>
2025-07-31 14:36:09 +0400

    Update and rename noTriples.java to NoTriples.java

--- from_Haitham/NoTriples.java
+++ from_Haitham/NoTriples.java
@@ -1,4 +1,4 @@
-public class noTriples {
+public class NoTriples {
     public boolean noTriples(int[] nums) {
         for(int index=0;index<nums.length-2;index++)
             if(nums[index]==nums[index+1] && nums[index+1]==nums[index+2])
