26dfb4ee4494fa83b6656b03e2e6cc13b000fe5e
Huria <alforihuria938@gmail.com>
2025-07-31 11:12:17 +0400

    code refactoring

--- From_Huria/DoubleXCounter.java
+++ From_Huria/DoubleXCounter.java
@@ -1,36 +1,23 @@
-public class CountXX {
+public class DoubleXCounter {
+    public static int doubleXCounter(String str) {
+        int countXX = 0;
+        for (int i = 0; i < str.length() - 1; i++) {
 
-        String str;
-
-        public static int countXX(String str) {
-
-            int countXX = 0;
-
-            for (int i = 0; i < str.length() -1 ; i++) {
-
-                if (str.charAt(i) == 'x' && str.charAt(i+1)=='x') {
-                    countXX++;
-                }
-
+            if (str.charAt(i) == 'x' && str.charAt(i + 1) == 'x') {
+                countXX++;
             }
-            return countXX;
         }
-
-
-
-
-
-        public static void main(String [] args){
-
-            System.out.println( CountXX.countXX("Hello"));
-            System.out.println( CountXX.countXX("Hexxo"));
-            System.out.println( CountXX.countXX("abc"));
-            System.out.println( CountXX.countXX("Kittensxxx"));
-            System.out.println( CountXX.countXX("abcxx"));
-            System.out.println( CountXX.countXX("xxx"));
-            System.out.println( CountXX.countXX("xxxx"));
-
-
-        }
+        return countXX;
     }
+
+    public static void main(String[] args) {
+        System.out.println(doubleXCounter("Hello"));
+        System.out.println(doubleXCounter("Hexxo"));
+        System.out.println(doubleXCounter("abc"));
+        System.out.println(doubleXCounter("Kittensxxx"));
+        System.out.println(doubleXCounter("abcxx"));
+        System.out.println(doubleXCounter("xxx"));
+        System.out.println(doubleXCounter("xxxx"));
+    }
+}
 
