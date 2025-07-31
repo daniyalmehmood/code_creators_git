d2f05ac06eceed9b21bee2cf36dd50b56988c76c
Ghaida02a <ggmskaj@gmail.com>
2025-07-31 11:31:51 +0400

    #TRA-1074-withoutEnd: class name changed

--- from_Ghaida/WithoutEnd.java
+++ from_Ghaida/WithoutEnd.java
@@ -1,4 +1,4 @@
-public class withoutEnd {
+public class WithoutEnd {
     public static void main(String[] args){
         System.out.println(withoutEnd("Hello")); //"ell"
         System.out.println(withoutEnd("java")); //"av"
