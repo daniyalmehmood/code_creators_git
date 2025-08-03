92ee7b03d2fd8a6f644d4587554d4786fe12d94e
Kaltham <kaltham.aldifai@gmail.com>
2025-08-03 16:02:42 +0400

    The Solution for SameFirstLast Task

---
+++ from_Kaltham/SameFirstLast.java
@@ -0,0 +1,10 @@
+public class SameFirstLast {
+    public boolean sameFirstLast(int[]nums) {
+        if (nums.length >= 1 && nums[0] == nums[nums.length - 1]) {
+            return true;
+
+        }
+        return false;
+
+    }
+}
