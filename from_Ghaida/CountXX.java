3e512c92ffbc487834be6c14114027f3daff1cc6
Ghaida02a <ggmskaj@gmail.com>
2025-08-03 11:20:24 +0400

    CountXX: Changed function name

--- from_Ghaida/CountXX.java
+++ from_Ghaida/CountXX.java
@@ -1,11 +1,11 @@
 public class CountXX {
     public static void main(String[] args) {
-        System.out.println(countXX("abcxx")); //1
-        System.out.println(countXX("xxx")); //2
-        System.out.println(countXX("xxxx")); //3
+        System.out.println(countDoubleX("abcxx")); //1
+        System.out.println(countDoubleX("xxx")); //2
+        System.out.println(countDoubleX("xxxx")); //3
     }
 
-    public static int countXX(String str) {
+    public static int countDoubleX(String str) {
         int count = 0;
         for(int i= 0; i < str.length() - 1; i++){
             if (str.substring(i, i + 2).equals("xx")) {
