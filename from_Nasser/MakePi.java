5477c3bab6732b23befb7fde2e33e278cefc5a54
NASSER1221 <alnassri1221@gmail.com>
2025-08-03 13:35:50 +0400

    added MakePi task File

---
+++ from_Nasser/MakePi.java
@@ -0,0 +1,10 @@
+
+
+public class MakePi {
+
+    public int[] makePi() {
+
+        return new int[]{0,1,2};
+    }
+
+}
--- from_Nasser/SameFirstLast.java
+++
@@ -1,8 +0,0 @@
-public class SameFirstLast {
-    public boolean sameFirstLast(int[] nums) {
-        if (nums.length >= 1) {
-            return nums[0] == nums[nums.length - 1];
-        }
-        return false;
-    }
-}
