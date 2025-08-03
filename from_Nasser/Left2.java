b1460b2ac5b19ab1be35cdedb69e2525bfec9997
NASSER1221 <alnassri1221@gmail.com>
2025-08-03 11:27:58 +0400

    added Left2 Task

---
+++ from_Nasser/Left2.java
@@ -0,0 +1,12 @@
+public class Left2 {
+
+    public String left2(String str) {
+
+        String left=str.substring(0,2);
+        String right=str.substring(2);
+
+    return right+left;
+
+    }
+
+}
