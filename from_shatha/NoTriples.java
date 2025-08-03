67b04b041135452eddce8718aaed1c8b1b4c5af2
Shatha Almamari <shatha.almamari99@gmail.com>
2025-08-03 09:52:26 +0400

    #TRA-1063-noTriples modified

--- from_shatha/NoTriples.java
+++ from_shatha/NoTriples.java
@@ -1,4 +1,4 @@
-public class noTriples {
+public class NoTriples {
     public static boolean noTriples(int[] nums) {
         for (int i = 0; i < nums.length-2; i++) {
             if (nums[i] == nums[i + 1] && nums[i] == nums[i + 2]) {
@@ -12,4 +12,5 @@ public class noTriples {
         System.out.println(noTriples(new int[] {1, 1, 2, 2, 2, 1}));
         System.out.println(noTriples(new int[] {1, 1, 1, 2, 2, 2, 1}));
     }
+
 }
