99a0d86d7cfd325851b4cf09478cfb6f01be307b
Shatha Almamari <shatha.almamari99@gmail.com>
2025-08-03 15:49:29 +0400

    #TRA-1234-withouEnd2 modified

--- from_shatha/WithouEndTwo.java
+++ from_shatha/WithouEndTwo.java
@@ -1,4 +1,4 @@
-public class withouEnd2 {
+public class WithouEndTwo {
     public static String withouEnd2(String str) {
         return str.length() <= 2 ? "" : str.substring(1, str.length() - 1);    }
 
