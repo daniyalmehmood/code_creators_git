e16664fd30b03f3da51c7907402b5d43ed9d5cc8
Maha <mahathamood98@gmail.com>
2025-08-03 09:16:48 +0400

    Modifying the file

--- From_Maha/ArrayCounter.java
+++ From_Maha/ArrayCounter.java
@@ -1,4 +1,10 @@
-  public int array667(int[] nums) {
+public class ArrayCounter {
+
+
+     // Counts the number of times the value 6 is followed by either 6 or 7 in the array.
+
+
+    public int countSixSixOrSixSeven(int[] nums) {
         int count = 0;
         for (int i = 0; i < nums.length - 1; i++) {
             if ((nums[i] == 6 && nums[i + 1] == 6) || (nums[i] == 6 && nums[i + 1] == 7)) {
@@ -6,5 +12,5 @@
             }
         }
         return count;
-
     }
+}
