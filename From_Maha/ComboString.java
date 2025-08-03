abfc88a7fc949f5e3119b2b54e10ebbc85209ca6
Maha <mahathamood98@gmail.com>
2025-08-03 11:06:28 +0400

    Modifying the file

--- From_Maha/ComboString.java
+++ From_Maha/ComboString.java
@@ -1,12 +1,13 @@
-public class ComboString {
-    public String comboString(String a, String b) {
-        if(a.length()>b.length()){
-            return b+a+b;
+public class ShortLongShortCombiner {
 
-        }
-        else {
-            return a+b+a;
-        }
 
+   // Returns a new string in the form short+long+short.
+
+    public String wrapLongWithShort(String a, String b) {
+        if (a.length() > b.length()) {
+            return b + a + b;
+        } else {
+            return a + b + a;
+        }
     }
 }
