f11b4f10ce6852d7c6f8376d5a51d5568abb3373
NASSER1221 <alnassri1221@gmail.com>
2025-08-03 16:58:09 +0400

    added FrontPiece Ending

---
+++ from_Nasser/FrontPiece.java
@@ -0,0 +1,12 @@
+public class FrontPiece {
+
+    public int[] frontPiece(int[] nums) {
+
+        if(nums.length>=2){
+            return new int[]{nums[0],nums[1]};
+        }
+        else return nums;
+
+    }
+
+}
