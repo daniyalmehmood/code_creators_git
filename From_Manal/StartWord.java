92fa6cea9f914706810634eafe9b10ab041fcd9f
Manal-Alhatmi <manalalhatmi99@outlook.com>
2025-08-03 16:10:14 +0400

    Modified StartWord.java

--- From_Manal/StartWord.java
+++ From_Manal/StartWord.java
@@ -1,14 +1,14 @@
-public class StartWord {
-    public static String startWord(String str, String word) {
+public class WordMatcher {
+    public static String getStartMatch(String str, String word) {
+        // make sure the word length no longer thn str length
         if (word.length() <= str.length() && str.substring(1, word.length()).equals(word.substring(1))) {
             return str.substring(0, word.length());
         }
         return "";
     }
     public static void main(String[] args) {
-        System.out.println(startWord("hippo", "hi") );
-        System.out.println(startWord("hippo", "xip"));
-        System.out.println(startWord("hippo", "i"));
+        System.out.println(getStartMatch("hippo", "hi") );
+        System.out.println(getStartMatch("hippo", "xip"));
+        System.out.println(getStartMatch("hippo", "i"));
     }
-
     }
