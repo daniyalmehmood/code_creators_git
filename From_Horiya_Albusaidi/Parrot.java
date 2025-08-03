a3356b1bfe928e1481fe5f502191149402c71781
EchoCoder20 <albusaidi1915@gmail.com>
2025-08-03 11:01:14 +0400

    Modify the file

--- From_Horiya_Albusaidi/Parrot.java
+++
@@ -1,9 +0,0 @@
-public class Parrot {
-    public  static Boolean parrotTrouble(boolean talking, int hour)
-    {
-        boolean result;
-        if(talking && (hour<7 || hour>20 )) result = true;
-        else result = false;
-        return result;
-    }
-}
