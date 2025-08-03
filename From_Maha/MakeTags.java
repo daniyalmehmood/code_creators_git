cbc8943ebdf7d32a09fb09e7575bc684d10da03b
Maha <mahathamood98@gmail.com>
2025-08-03 12:08:35 +0400

    Modifying the file

--- From_Maha/MakeTags.java
+++ From_Maha/MakeTags.java
@@ -1,16 +1,17 @@
+public class HtmlTagBuilder {
 
-    public class MakeTags {
 
-        public String makeTags(String tag, String word) {
-            return "<" + tag + ">" + word + "</" + tag + ">";
-        }
+    // Wraps a word with the given HTML tag.
+  
+    public String wrapWithTag(String tag, String word) {
+        return "<" + tag + ">" + word + "</" + tag + ">";
+    }
 
-        public static void main(String[] args) {
-            MakeTags obj = new MakeTags();
+    public static void main(String[] args) {
+        HtmlTagBuilder obj = new HtmlTagBuilder();
 
-            System.out.println(obj.makeTags("i", "Yay"));       // <i>Yay</i>
-            System.out.println(obj.makeTags("b", "Hello"));     // <b>Hello</b>
-            System.out.println(obj.makeTags("em", "Wow"));      // <em>Wow</em>
-        }
-       }
-
+        System.out.println(obj.wrapWithTag("i", "Yay"));       // <i>Yay</i>
+        System.out.println(obj.wrapWithTag("b", "Hello"));     // <b>Hello</b>
+        System.out.println(obj.wrapWithTag("em", "Wow"));      // <em>Wow</em>
+    }
+}
