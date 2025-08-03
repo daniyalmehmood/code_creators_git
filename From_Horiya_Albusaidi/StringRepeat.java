78bd86dae5c48222ff4a46e8b7a283b4d9119b3a
EchoCoder20 <albusaidi1915@gmail.com>
2025-08-03 09:35:07 +0400

    Rename function and class

---
+++ From_Horiya_Albusaidi/StringRepeat.java
@@ -0,0 +1,19 @@
+package Warmup2;
+
+public class StringRepeat {
+    public String repeatFrontNTimes(String str, int n) {
+        StringBuilder result = new StringBuilder();
+        if (n > 0) {
+
+            if (str.length() >= 3) {
+                for (int i = 0; i < n; i++) {
+                    result.append(str, 0, 3);
+                }
+            } else {
+                result.append(str.repeat(n));
+            }
+            return result.toString();
+        } else
+            return "";
+    }
+}
