b8565074a86db17a37288c93d3ded74499d40400
Manal-Alhatmi <manalalhatmi99@outlook.com>
2025-08-03 14:33:00 +0400

    Modified and Renamed Array667.txt to SixSixOrSixSevenCounter.java

--- From_Manal/SixSixOrSixSevenCounter.java
+++ From_Manal/SixSixOrSixSevenCounter.java
@@ -1,6 +1,6 @@
 
-public class Array667 {
-    public static int array667(int[] nums) {
+public class SixSixOrSixSevenCounter {
+    public static int countSixFollowedBySixOrSeven(int[] nums) {
         int count = 0;
         for (int i = 0; i < nums.length-1; i++) {
             if (nums[i] == 6 && nums[i + 1] == 6) {
@@ -15,8 +15,8 @@ public class Array667 {
     }
 
     public static void main(String[] arg) {
-        System.out.println(array667(new int[]{6, 6, 2}));
-        System.out.println(array667(new int[]{6, 6, 2, 6}));
-        System.out.println(array667(new int[]{6, 7, 2, 6}));
+        System.out.println(countSixFollowedBySixOrSeven(new int[]{6, 6, 2}));
+        System.out.println(countSixFollowedBySixOrSeven(new int[]{6, 6, 2, 6}));
+        System.out.println(countSixFollowedBySixOrSeven(new int[]{6, 7, 2, 6}));
     }
 }
