a877057b69fa7e887f38f9379aa46981929306d9
Ghaida02a <ggmskaj@gmail.com>
2025-08-03 12:07:09 +0400

    StringYak: removed unnecessary code comment

--- from_Ghaida/StringYak.java
+++ from_Ghaida/StringYak.java
@@ -5,15 +5,6 @@ public class StringYak {
         System.out.println(stringYak("yak123ya")); //"123ya"
     }
     public static String stringYak(String str) {
-//        StringBuilder newString = new StringBuilder();
-//        for(int i = 0; i < str.length(); i++){
-//            if (i + 2 < str.length() && str.charAt(i) == 'y' && str.charAt(i + 2) == 'k') {
-//                i += 2; // skip these 3 chars
-//            } else {
-//                newString.append(str.charAt(i));
-//            }
-//        }
-//        return newString.toString();
         return str.replaceAll("yak", "");
     }
 }
