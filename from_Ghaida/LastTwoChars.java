47fceaa1233fcc02aecbe41cfde1e9c2d152b0fb
Ghaida02a <ggmskaj@gmail.com>
2025-08-03 11:27:43 +0400

    Last2: Changed variable name

--- from_Ghaida/LastTwoChars.java
+++ from_Ghaida/LastTwoChars.java
@@ -1,4 +1,4 @@
-public class Last2 {
+public class LastTwoChars {
     public static void main(String[] args) {
         System.out.println(countMatchingLastTwoPairs("hixxhi")); //1
         System.out.println(countMatchingLastTwoPairs("xaxxaxaxx")); //1
@@ -8,10 +8,10 @@ public class Last2 {
         if (str.length() < 2) {//string can't be less 2 character
             return 0;
         }
-        String last2 = str.substring(str.length() - 2);
+        String lastTwoChars = str.substring(str.length() - 2);
         int count = 0;
         for (int i = 0; i < str.length() - 2; i++) {
-            if (str.substring(i, i + 2).equals(last2)) {
+            if (str.substring(i, i + 2).equals(lastTwoChars)) {
                 count++;
             }
         }
