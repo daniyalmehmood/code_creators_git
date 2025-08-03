f78ca001d4323eb306de25e54ea694718f720f55
NASSER1221 <alnassri1221@gmail.com>
2025-08-03 16:05:52 +0400

    added BiggerTwo file

---
+++ from_Nasser/Bigger2.java
@@ -0,0 +1,12 @@
+public class Bigger2 {
+
+    public int[] biggerTwo(int[] a, int[] b) {
+
+        if((a[0]+a[1])>b[0]+b[1]){
+            return a;
+        }
+        else if((a[0]+a[1])==b[0]+b[1]) return a;
+        else return b;
+    }
+
+}
