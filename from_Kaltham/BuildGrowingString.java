f614a9e591be40393b7dc02e99203eae8f871478
Kaltham <kaltham.aldifai@gmail.com>
2025-08-03 10:19:12 +0400

    modified stringSplosion Task

--- from_Kaltham/BuildGrowingString.java
+++ from_Kaltham/BuildGrowingString.java
@@ -1,10 +1,10 @@
 public class BuildGrowingString {
     public String buildGrowingString(String str) {
-        StringBuilder str1 = new StringBuilder();
+        StringBuilder Finalstr = new StringBuilder();
         for (int i = 0; i < str.length(); i++) {
-            str1.append(str.substring(0, i + 1));
+            Finalstr.append(str.substring(0, i + 1));
         }
-        return str1.toString();
+        return Finalstr.toString();
     }
 
 }
