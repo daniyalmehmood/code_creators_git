af49ec9cd2b77d2139a2b194404cea19dac1ff56
Maha <mahathamood98@gmail.com>
2025-08-03 15:32:38 +0400

    Editing

--- From_Maha/MiddleCharacterExtractor.java
+++ From_Maha/MiddleCharacterExtractor.java
@@ -1,9 +1,6 @@
 public class MiddleCharacterExtractor {
-
-
      // Returns the middle two characters of a string with even length.
     // If the string length is 2 or less, returns the original string.
-
     public String getMiddleTwoChars(String str) {
         if (str.length() > 2) {
             int mid = str.length() / 2;
@@ -12,6 +9,5 @@ public class MiddleCharacterExtractor {
             return str;
         }
     }
-
 }
 
