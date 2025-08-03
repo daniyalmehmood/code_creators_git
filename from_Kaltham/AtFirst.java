e545b537b5470510f29b928de8478ebfc4e8200d
Kaltham <kaltham.aldifai@gmail.com>
2025-08-03 13:49:00 +0400

    The Solution for AtFirst Task

---
+++ from_Kaltham/AtFirst.java
@@ -0,0 +1,13 @@
+public class AtFirst {
+    public String atFirst(String str) {
+
+        if (str.length() >= 2) {
+            return str.substring(0, 2);
+        }
+        if (str.length() == 1) {
+            return str.substring(0) + "@";
+        }
+        return "@" + "@";
+    }
+
+}
