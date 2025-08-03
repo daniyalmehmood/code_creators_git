9c41451237c30362c16ab043fbd96194e4a6582a
Maha <mahathamood98@gmail.com>
2025-08-03 10:58:55 +0400

    Modifying the file

--- From_Maha/Right2.java
+++ From_Maha/Right2.java
@@ -1,9 +1,13 @@
-public class Right2 {
-    public String right2(String str) {
+public class StringRotator {
+
+    /**
+     * Moves the last 2 characters of the string to the front.
+     * If the string has fewer than 2 characters, it returns the string unchanged.
+     */
+    public String rotateRight2(String str) {
         if (str.length() >= 2) {
-            return (str.substring(str.length() - 2)) + (str.substring(0, str.length() - 2));
+            return str.substring(str.length() - 2) + str.substring(0, str.length() - 2);
         }
         return str;
     }
-
 }
