4e10efeb7d32d845e3fe5bb59f9c94da2f48deba
NASSER1221 <alnassri1221@gmail.com>
2025-08-03 13:32:48 +0400

    added SameFirstLast task file

---
+++ from_Nasser/SameFirstLast.java
@@ -0,0 +1,8 @@
+public class SameFirstLast {
+    public boolean sameFirstLast(int[] nums) {
+        if (nums.length >= 1) {
+            return nums[0] == nums[nums.length - 1];
+        }
+        return false;
+    }
+}
