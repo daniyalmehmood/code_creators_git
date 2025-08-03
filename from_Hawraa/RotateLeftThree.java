678364838527d9dc3d50d37d0391a99f9f9e8a0b
Hawraa <hawra8516@gmail.com>
2025-08-03 13:00:12 +0400

    Array-1: rotateLeft3 modified

--- from_Hawraa/RotateLeftThree.java
+++ from_Hawraa/RotateLeftThree.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
-public class RotateLeft3 {
-    public static int[] rotateLeft3(int[] nums) {
+public class RotateLeftThree {
+    public static int[] rotateLeftThree(int[] nums) {
         int[] rotated = new int[3];
         for (int i = 0; i < nums.length; i++){
             rotated[i]= nums[(i + 1) % nums.length]; // When i=0, (0 + 1) % 3 = 1, so rotated[0] = nums[1]
@@ -10,8 +10,8 @@ public class RotateLeft3 {
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(rotateLeft3(new int[]{1, 2, 3})));
-        System.out.println(Arrays.toString(rotateLeft3(new int[]{5, 11, 9})));
-        System.out.println(Arrays.toString(rotateLeft3(new int[]{7, 0, 0})));
+        System.out.println(Arrays.toString(rotateLeftThree(new int[]{1, 2, 3})));
+        System.out.println(Arrays.toString(rotateLeftThree(new int[]{5, 11, 9})));
+        System.out.println(Arrays.toString(rotateLeftThree(new int[]{7, 0, 0})));
     }
 }
