c2ac4a4bbea83bffb65b1f8f54f07a9295a73243
NASSER1221 <alnassri1221@gmail.com>
2025-08-03 11:42:39 +0400

    added FirstHalf task file

---
+++ from_Nasser/FirstHalf.java
@@ -0,0 +1,18 @@
+public class FirstHalf {
+
+    public String firstHalf(String str) {
+
+
+        int length = str.length() / 2;
+
+
+        String mm = str.substring(0, length);
+
+        return mm;
+
+
+    }
+}
+
+
+
