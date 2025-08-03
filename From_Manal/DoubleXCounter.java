563c366614c35dc89b28932ed9772b14e0cd8781
Manal-Alhatmi <manalalhatmi99@outlook.com>
2025-08-03 14:12:34 +0400

    Modified and Renamed CountXx.txt and DoubleXCounter.java

---
+++ From_Manal/DoubleXCounter.java
@@ -0,0 +1,16 @@
+public class DoubleXCounter {
+    public static int countDoubleX(String str) {
+
+        int c=0;
+        for(int i=0; i< str.length()-1; i++){
+            if(str.substring(i, i+2).equals("xx")) c++;
+
+        }
+        return c;
+    }
+    public static void main(String[] args) {
+        System.out.println(countDoubleX("abcxx")); //1
+        System.out.println(countDoubleX("xxx") ); //2
+        System.out.println(countDoubleX("xxxx")); //3
+    }
+}
