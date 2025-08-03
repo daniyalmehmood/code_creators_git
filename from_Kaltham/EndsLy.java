3fbce9333b16c39c602bbc9d9e8b109967bb3ac5
Kaltham <kaltham.aldifai@gmail.com>
2025-08-03 13:19:04 +0400

    The Solution for endsLy Task

---
+++ from_Kaltham/EndsLy.java
@@ -0,0 +1,8 @@
+public class EndsLy {
+    public boolean endsLy(String str) {
+        if (str.length() >= 2 && str.substring(str.length() - 2).equals("ly")) {
+            return true;
+        }
+        return false;
+    }
+}
