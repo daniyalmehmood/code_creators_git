009166bc217cdba4b645b68260efb8a48a3e6834
Hawraa <hawra8516@gmail.com>
2025-08-03 12:02:34 +0400

    String-1: right2 modified

--- from_Hawraa/RightTwo.java
+++ from_Hawraa/RightTwo.java
@@ -1,10 +1,8 @@
 public class RightTwo {
     public static String right2(String str) {
-        String firstCh = "";
-        String lastCh = "";
         if(str.length() > 2){
-            firstCh = str.substring(str.length() - 2, str.length());
-            lastCh = str.substring(0, str.length() -2);
+            String firstCh = str.substring(str.length() - 2, str.length());
+            String lastCh = str.substring(0, str.length() -2);
             return firstCh + lastCh;
         }
         else {
