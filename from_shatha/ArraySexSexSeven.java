6e629bf014a8ea186a5a8b44d69eb3fadfaea7cd
Shatha Almamari <shatha.almamari99@gmail.com>
2025-08-03 08:49:38 +0400

    #TRA-1062-array667 modified

--- from_shatha/ArraySexSexSeven.java
+++ from_shatha/ArraySexSexSeven.java
@@ -1,5 +1,5 @@
-public class Array667 {
-    public static int ArraySexSexSeven(int[] nums) {
+public class ArraySexSexSeven {
+    public static int Array667(int[] nums) {
         int count = 0;
         for (int i=0; i < (nums.length-1); i++) {
             if (nums[i] == 6) {
