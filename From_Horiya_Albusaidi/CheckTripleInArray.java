a548acbacb220f1fb623d834e1166244269dc3f4
EchoCoder20 <albusaidi1915@gmail.com>
2025-08-03 10:57:59 +0400

    Rename the file,class and function

--- From_Horiya_Albusaidi/CheckTripleInArray.java
+++ From_Horiya_Albusaidi/CheckTripleInArray.java
@@ -1,16 +1,9 @@
-package Warmup2;
-
-public class NoTriples {
-    public boolean noTriples(int[] nums) {
-
+public class CheckTripleInArray {
+    public boolean containsTripleElement(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             if (i + 2 < nums.length && nums[i] == nums[i + 1] && nums[i] == nums[i + 2])
                 return false;
-
         }
         return true;
-
     }
-
-
-}
+}g
