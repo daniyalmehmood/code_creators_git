9d9220142f7f20f021f526b6fbb1e81eb1ce0d44
Kaltham <kaltham.aldifai@gmail.com>
2025-08-03 10:46:49 +0400

    modified StringYak Task

--- from_Kaltham/StringYak.java
+++ from_Kaltham/StringYak.java
@@ -1,12 +1,10 @@
-public class RemoveYakSubstrings {
+public class StringYak {
     public String removeYakSubstrings(String str) {
         StringBuilder newstr = new StringBuilder(str);
-        for (int i = 0; i < newstr.length() - 2; ) {
+        for (int i = 0; i < newstr.length() - 2; i++ ) {
             if (newstr.substring(i, i + 3).equals("yak")) {
                 newstr.delete(i, i + 3);
-            } else {
-                i++;
-            }
+            } 
         }
         return newstr.toString();
     }
