882b649b5eb4169d6c37b988edf70e5120b822ca
Kaltham <kaltham.aldifai@gmail.com>
2025-08-03 11:04:47 +0400

    modified StringBits Task

--- from_Kaltham/StringBits.java
+++ from_Kaltham/StringBits.java
@@ -1,4 +1,4 @@
-public class GetEvenIndexChars {
+public class StringBits {
     public String getEvenIndexChars(String str) {
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < str.length(); i += 2) {
