4b8f13cbd89682ea6b5ce8572ccb3d100137cc29
Hanin <haninzx2024@gmail.com>
2025-08-03 16:26:10 +0400

    modify the task

---
+++ from_hanin/Sleepin.java
@@ -0,0 +1,12 @@
+public class Sleepin {
+    public static boolean isItSleepIn(boolean weekday, boolean vacation) {
+        if (!weekday || vacation) {
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+}
+
+
