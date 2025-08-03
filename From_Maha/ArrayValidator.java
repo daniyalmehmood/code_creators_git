39a63e2b406cccfbad75d4a4e1171b2417b22f85
Maha <mahathamood98@gmail.com>
2025-08-03 09:09:47 +0400

    Modifying the file

--- From_Maha/ArrayValidator.java
+++ From_Maha/ArrayValidator.java
@@ -1,4 +1,8 @@
- public boolean noTriples(int[] nums) {
+public class ArrayValidator {
+
+     //return true if no triples found; false otherwise
+
+    public boolean noTriples(int[] nums) {
         for (int i = 0; i < nums.length - 2; i++) {
             if (nums[i] == nums[i + 1] && nums[i] == nums[i + 2]) {
                 return false;
@@ -6,3 +10,4 @@
         }
         return true;
     }
+}
