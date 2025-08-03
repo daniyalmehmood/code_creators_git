b3e8263840debe6868291048e651653006288936
Shatha Almamari <shatha.almamari99@gmail.com>
2025-08-03 08:31:32 +0400

    #TRA-1057-array123 modified

---
+++ from_shatha/ArrayOneTwoThree.java
@@ -0,0 +1,18 @@
+public class ArrayOneTwoThree{
+    public static boolean array123(int[] nums) {
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] == 1 && nums[i+ 1 ] == 2 && nums[i+ 2 ] == 3) {
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(array123(new int[] {1, 1, 2, 3, 1}));
+        System.out.println(array123(new int[] {1, 1, 2, 4, 1}));
+        System.out.println(array123(new int[] {1, 1, 2, 1, 2, 3}));
+    }
+}
