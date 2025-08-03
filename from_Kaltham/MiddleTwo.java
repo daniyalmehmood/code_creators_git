327b31a48bcb1bbfc4f402641620c8e9630f482f
Kaltham <kaltham.aldifai@gmail.com>
2025-08-03 13:11:35 +0400

    The Solution for MiddleTwo Task

---
+++ from_Kaltham/MiddleTwo.java
@@ -0,0 +1,9 @@
+public class MiddleTwo {
+    public String middleTwo(String str) {
+        if (str.length() % 2 == 0) {
+            return str.substring(str.length() / 2 - 1, str.length() / 2 + 1);
+
+        }
+        return str;
+    }
+}
