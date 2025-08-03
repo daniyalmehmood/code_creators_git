6f4ac36d19815aa6b71551f01b86b12c627064a9
Hawraa <hawra8516@gmail.com>
2025-08-03 10:20:45 +0400

    Warmup-2: Array123 modified

--- from_Hawraa/SequenceMatch.java
+++ from_Hawraa/SequenceMatch.java
@@ -1,5 +1,5 @@
-public class Array123 {
-    public static boolean array123(int[] nums) {
+public class SequenceMatch {
+    public static boolean sequenceMatch(int[] nums) {
         for (int i = 0; i < nums.length - 2; i++){
             if (nums[i] == 1 && nums[i +1] == 2 && nums[i+2] ==3){
                 return true;
