fc02c89ec51e90875c20a59152c52ef5331c4a41
Maha <mahathamood98@gmail.com>
2025-08-03 08:48:26 +0400

    Modifying the file

--- From_Maha/CountXX.java
+++ From_Maha/CountXX.java
@@ -1,9 +1,11 @@
-int countXX(String str) {
+public class CountXX {
+    int countConsecutiveDoubleX(String str) {
         int xCount = 0;
         for (int i = 0; i < str.length() - 1; i++) {
             if (str.charAt(i) == 'x' && str.charAt(i + 1) == 'x') {
                 xCount++;
             }
         }
-        return xCount++;
+        return xCount;
     }
+}
