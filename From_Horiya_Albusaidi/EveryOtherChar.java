62fee58c37eb1e0d0e89bb3cbc7c5e63f81b1038
EchoCoder20 <albusaidi1915@gmail.com>
2025-08-03 09:15:07 +0400

    Modify the class and function name

--- From_Horiya_Albusaidi/EveryOtherChar.java
+++ From_Horiya_Albusaidi/EveryOtherChar.java
@@ -1,9 +1,7 @@
-package Warmup2;
-
 public class StringBit {
     public String stringBits(String str) {
         String result="";
-
+        StringBuilder
         for(int i=0;i<str.length();i+=2)
         {
             result+=str.charAt(i);
