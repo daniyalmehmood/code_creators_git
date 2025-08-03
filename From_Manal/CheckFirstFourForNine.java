bc453c50ad9a2f62b628b888d4272afb819533e7
Manal-Alhatmi <manalalhatmi99@outlook.com>
2025-08-03 14:23:44 +0400

    Modified and Renamed ArrayFront9.txt to CheckFirstFourForNine.java

---
+++ From_Manal/CheckFirstFourForNine.java
@@ -0,0 +1,18 @@
+public class CheckFirstFourForNine {
+    public static boolean hasNineInFirstFour(int[] nums) {
+        //return true if one of the first 4 elements in the array is a 9
+
+        int n =nums.length;
+        if(n>4) n=4;
+        for (int i=0; i< n; i++) {
+            if (nums[i] ==9){return true;}
+
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        System.out.println(hasNineInFirstFour(new int[]{1, 2, 9, 3, 4}));
+        System.out.println(hasNineInFirstFour(new int[]{1, 2, 3, 4, 9}));
+        System.out.println(hasNineInFirstFour(new int[]{1, 2, 3, 4, 5}));
+    }
+}
