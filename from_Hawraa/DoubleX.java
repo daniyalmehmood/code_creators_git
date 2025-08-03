c40a5629bfe5a3ccc89d327063a94f81594760c0
Hawraa <hawra8516@gmail.com>
2025-08-03 10:25:54 +0400

    Warmup-2: DoubleX modified

--- from_Hawraa/DoubleX.java
+++ from_Hawraa/DoubleX.java
@@ -1,10 +1,10 @@
 public class DoubleX {
     public static boolean doubleX(String str) {
-        int i = str.indexOf("x");
-        if (i + 1 >= str.length()) {
+        int index = str.indexOf("x");
+        if (index + 1 >= str.length()) {
             return false;
         }
-        return str.substring(i + 1, i + 2).equals("x");
+        return str.substring(index + 1, index + 2).equals("x");
 }
         public static void main (String[]args){
             System.out.println(doubleX("axxbb"));
