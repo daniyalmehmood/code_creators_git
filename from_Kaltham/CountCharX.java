b6f17c8e1071176bca38969cb4e12d827ab7e8b2
Kaltham <kaltham.aldifai@gmail.com>
2025-08-03 10:10:02 +0400

    modified CountCharX Task

--- from_Kaltham/CountCharX.java
+++ from_Kaltham/CountCharX.java
@@ -1,27 +1,27 @@
 public class CountCharX {
-    public int countCharX(String str) {
+    public int countXX(String str) {
         int count = 0;
         if (str.contains(" ")) {
             int space = str.indexOf(" ");
             String firstWord = str.substring(0, space);
             for (int i = 0; i < firstWord.length(); i++) {
-                if (firstWord.charAt(i) == ('x')) {
+                if (firstWord.charAt(i) == 'x') {
                     count++;
                 }
             }
+
             return count;
-        } else if (!str.isEmpty()) {
+        } else {
             for (int i = 0; i < str.length(); i++) {
-                if (str.charAt(i) == ('x')) {
+                if (str.charAt(i) == 'x') {
                     count++;
                 }
             }
+
             if (count > 0) {
                 return count - 1;
             }
             return count;
-        } else {
-            return 0;
         }
 
     }
