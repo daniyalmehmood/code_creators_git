481bdceda1733f043153b781d6cd9aeaf7dc2c4b
Kaltham <kaltham.aldifai@gmail.com>
2025-08-04 11:17:09 +0400

    The solution for double23 Task

---
+++ from_Kaltham/HasDouble.java
@@ -0,0 +1,10 @@
+public class HasDouble {
+    public boolean DoubleTwoThree(int[]nums) {
+        if (nums.length == 2) {
+            if ((nums[0] == 2 && nums[1] == 2) || (nums[0] == 3 && nums[1] == 3)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
