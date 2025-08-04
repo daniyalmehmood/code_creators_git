8690d30fa39524dccfff1a940bbaeda43938cf61
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-04 16:25:46 +0400

    #TRA-2164: Logic1_ Love6 Done

---
+++ From_Alharith/Love6.java
@@ -0,0 +1,9 @@
+public class Love6 {
+
+    public boolean love6(int a, int b) {
+        int sum= a+b;
+        int diff= Math.abs(a-b);
+        return ((a==6 || b==6) || (sum==6) || (diff==6)) ;
+    }
+
+}
