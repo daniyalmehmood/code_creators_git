fb41c574c94939844f7887804b3e8d3cbe272723
mohammedanad <alhuseini.moha@gmail.com>
2025-08-04 10:37:45 +0400

    Warmup-2: #TRA-2055 last2 done

---
+++ from_Mohammed/Last2.java
@@ -0,0 +1,12 @@
+public int Last2(String str) {
+    if (str.length()<2)return 0;
+
+    String last=str.substring(str.length()-2);
+    int count=0;
+    for (int i=0; i<str.length()-2;i++)
+    {
+        String sub=str.substring(i, i+2);
+        if (sub.equals(last)){
+            count++;}}
+    return count;
+}
