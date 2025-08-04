aa646b5111a6f3cceb94a8ba78da9fa784a7d43f
Mulk <mulkbadar774@gmail.com>
2025-08-04 16:06:14 +0400

    modified StringYak task

--- From_Mulk/StringYak.java
+++ From_Mulk/StringYak.java
@@ -2,12 +2,11 @@ public class StringYak {
     public String removeYakSubstrings(String str) {
 
         StringBuilder newString = new StringBuilder(str);
-        for (int i = 0; i < newString.length() - 2; ) {
+        for (int i = 0; i < newString.length() - 2; i++) {
             if (newString.charAt(i) == 'y' && newString.charAt(i + 1) == 'a' && newString.charAt(i + 2) == 'k') {
                 newString.delete(i, i + 3);
-            } else {
-                i++;
             }
+
         }
         return newString.toString();
     }
