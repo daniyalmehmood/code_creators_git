005cacf9aa15acae0dca90792205e59c72271012
Huria <alforihuria938@gmail.com>
2025-08-04 12:06:18 +0400

    modifing code logic

--- from_Huria/HasTwoSevenOne.java
+++ from_Huria/HasTwoSevenOne.java
@@ -1,8 +1,8 @@
 public class HasTwoSevenOne {
 
-    public static boolean has271(int[] nums ) {
+    public static boolean hasTwoSevenOne(int[] nums) {
 
-        for (int i= 0 ; i < nums.length - 2; i++) {
+        for (int i = 0; i < nums.length - 2; i++) {
             int x = nums[i];
             if (nums[i + 1] == x + 5 && Math.abs(nums[i + 2] - (x - 1)) <= 2) {
                 return true;
@@ -12,14 +12,14 @@ public class HasTwoSevenOne {
     }
 
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
 
-        System.out.println(has271(new int[]{1, 2, 7, 1}));
-        System.out.println(has271(new int[]{1, 2, 8, 1}));
-        System.out.println(has271(new int[]{3, 8, 2}));
-        System.out.println(has271(new int[]{2, 7, 3}));
-        System.out.println(has271(new int[]{2, 7, -1}));
-        System.out.println(has271(new int[]{4, 5, 3, 8, 0}));
-        System.out.println(has271(new int[]{1, 1, 4, 9, 4, 9, 2}));
+        System.out.println(HasTwoSevenOne.hasTwoSevenOne(new int[]{1, 2, 7, 1}));
+        System.out.println(HasTwoSevenOne.hasTwoSevenOne(new int[]{1, 2, 8, 1}));
+        System.out.println(HasTwoSevenOne.hasTwoSevenOne(new int[]{3, 8, 2}));
+        System.out.println(HasTwoSevenOne.hasTwoSevenOne(new int[]{2, 7, 3}));
+        System.out.println(HasTwoSevenOne.hasTwoSevenOne(new int[]{2, 7, -1}));
+        System.out.println(HasTwoSevenOne.hasTwoSevenOne(new int[]{4, 5, 3, 8, 0}));
+        System.out.println(HasTwoSevenOne.hasTwoSevenOne(new int[]{1, 1, 4, 9, 4, 9, 2}));
     }
 }
