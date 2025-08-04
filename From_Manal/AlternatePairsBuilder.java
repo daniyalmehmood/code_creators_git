8d4128ebf09e0368e02d039c52678ce80956cef1
Manal-Alhatmi <manalalhatmi99@outlook.com>
2025-08-04 15:47:48 +0400

    Modified AlternatePairsBuilder.java

--- From_Manal/AlternatePairsBuilder.java
+++ From_Manal/AlternatePairsBuilder.java
@@ -2,14 +2,14 @@
 public class AlternatePairsBuilder {
     public static String getAlternatesPairs(String str) {
         //every 4 characters take index i and i+1
-        StringBuilder string1 = new StringBuilder();
+        StringBuilder result = new StringBuilder();
         for (int i = 0; i < str.length(); i+=4) {
-            string1.append(str.charAt(i));
+            result.append(str.charAt(i));
             //only add next characters if it excists
             if (i + 1 < str.length())
-                string1.append(str.charAt(i + 1));
+                result.append(str.charAt(i + 1));
         }
-        return string1.toString();
+        return result.toString();
     }
 
     public static void main(String[] arg) {
