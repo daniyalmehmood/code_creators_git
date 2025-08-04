ae8ef17e493b7c71111fbc43bf93659603e88668
mohammedanad <alhuseini.moha@gmail.com>
2025-08-04 10:48:19 +0400

    Warmup-2: #TRA-2060 StringX done

---
+++ from_Mohammed/StringX.java
@@ -0,0 +1,11 @@
+public String StringX(String str) {
+
+    String result = "";
+    for (int i=0; i<str.length(); i++) {
+
+        if (!(i > 0 && i < (str.length()-1) && str.substring(i, i+1).equals("x"))) {
+            result = result + str.substring(i, i+1);
+        }
+    }
+    return result;
+}
