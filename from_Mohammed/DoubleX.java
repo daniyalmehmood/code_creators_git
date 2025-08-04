bb8f36f74df2b973677f35e4e7f16c82816bd319
mohammedanad <alhuseini.moha@gmail.com>
2025-08-04 09:46:32 +0400

    Warmup2: #TRA-2050 CountXX done

--- from_Mohammed/DoubleX.java
+++
@@ -1,7 +0,0 @@
-boolean DoubleX(String str) {
-
-    int i= str.indexOf("x");
-    if (i==-1) return false;
-    if (i+1 >= str.length()) return false;
-    return str.substring(i+1, i+2).equals("x");
-}
