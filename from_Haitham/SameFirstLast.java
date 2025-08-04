5f66db4cedda2c872a1c59d5611b4911efce7cdb
haithemhamid <149400034+haithemhamid@users.noreply.github.com>
2025-08-04 15:45:05 +0400

    Update SameFirstLast.java

--- from_Haitham/SameFirstLast.java
+++ from_Haitham/SameFirstLast.java
@@ -3,8 +3,5 @@ public boolean sameFirstLast(int[] nums) {
         return (nums.length > 0 && nums[0] == nums[nums.length - 1]);
     }
 
-    public int[] makePi() {
-        int[] Pi = new int[]{3, 1, 4};
-        return Pi;
-    }
+  
 }
