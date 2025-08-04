23b589300908a611ad229b74ecf90678c60cffbf
NASSER1221 <alnassri1221@gmail.com>
2025-08-04 12:07:49 +0400

    added Old35 java file

--- from_Nasser/More20.java
+++
@@ -1,7 +0,0 @@
-public class More20 {
-    public boolean more20(int n) {
-        return (n%20==1 || n%20==2);
-
-    }
-
-}
---
+++ from_Nasser/Old35.java
@@ -0,0 +1,12 @@
+public class Old35 {
+    public boolean old35(int n) {
+
+        if (n%3==0 && n%5==0) return false;
+
+        else if (n%3==0 || n%5==0) return true;
+        else return false;
+
+    }
+
+
+}
