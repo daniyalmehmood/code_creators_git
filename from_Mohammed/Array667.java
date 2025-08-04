b5d9f392dd28578582b09384fdb6c2283fb65730
mohammedanad-dev <alhuseini.moha@gmail.com>
2025-08-04 14:50:37 +0400

    Update Array667.java

--- from_Mohammed/Array667.java
+++ from_Mohammed/Array667.java
@@ -1,4 +1,5 @@
-public int Array667(int[] nums){
+public class Array667 {
+public int array667(int[] nums){
     int count=0;
     for (int i=0; i<(nums.length-1);i++){
         if (nums[i]==6){
@@ -7,5 +8,5 @@ public int Array667(int[] nums){
             }}}
 
     return count;
-
+}
 }
