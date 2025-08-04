716360eadb42b181384632b94b6a2525e9f49f6e
Manal-Alhatmi <manalalhatmi99@outlook.com>
2025-08-04 15:55:51 +0400

    Modified  StringRepeater.java

--- From_Manal/StringRepeater.java
+++ From_Manal/StringRepeater.java
@@ -2,11 +2,11 @@ public class StringRepeater {
     //extract the first 3 characters or less if the input length less than 3
     public static String repeatFront(String str, int n) {
         String front = str.substring(0, Math.min(str.length(), 3));
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            result = result + front;
+            result.append(front);
         }
-        return result;
+        return result.toString();
     }
 
     public static void main(String[] args) {
