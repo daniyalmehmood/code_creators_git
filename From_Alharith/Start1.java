ff44c2281640c226daf577cf61309e29af17ae4b
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-04 10:42:07 +0400

    #TRA-1750: Array1_ Start1 Done

---
+++ From_Alharith/Start1.java
@@ -0,0 +1,12 @@
+public class Start1 {
+
+    public int start1(int[] a, int[] b) {
+        int result=0;
+        if (a.length>=1 &&a[0]==1  ) result+=1;
+        if ( b.length>=1&&b[0]==1) result+= 1;
+
+
+        return result;
+    }
+
+}
