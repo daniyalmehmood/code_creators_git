be9991010486cfca1334c7f60f4a147dc59a3473
Shaima <shaimaalnabhani@gmail.com>
2025-08-04 09:10:05 +0400

    Modify the StringBits task as per requested

--- From_Shaima/CharactorExtractor.java
+++ From_Shaima/CharactorExtractor.java
@@ -1,11 +1,11 @@
 public class CharactorExtractor {
     public static String extractAlternativeChars(String str) {
-        StringBuilder newStr= new StringBuilder();  // Create a StringBuilder object to build the final result 'newStr'
+        StringBuilder evenChar= new StringBuilder();  // Create a StringBuilder object to build the final result 'evenChar'
         for (int i=0 ;i<str.length(); i=i+2)  // Use a for-loop to go through each character of 'str'
         {
-            newStr.append(str.charAt(i)); // add all elements , started from index 0 +2
+            evenChar.append(str.charAt(i)); // add all elements , started from index 0 +2
         }
-        return newStr.toString(); // return the value of 'newStr' after converted to String
+        return evenChar.toString(); // return the value of 'evenChar' after converted to String
     }
     public static void main (String[]args){
         System.out.println(extractAlternativeChars("Hello"));
