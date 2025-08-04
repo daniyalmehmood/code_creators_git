6b2b0d6a3c7acad561feeea5224a97c9487df1dd
Kaltham <kaltham.aldifai@gmail.com>
2025-08-04 11:30:43 +0400

    The solution for fix23 Task

---
+++ from_Kaltham/PatternCorrector.java
@@ -0,0 +1,10 @@
+public class PatternCorrector {
+    public int[]replaceThreeAfterTwo(int[]nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 2 && nums[i + 1] == 3) {
+                nums[i + 1] = 0;
+            }
+        }
+        return nums;
+    }
+}
