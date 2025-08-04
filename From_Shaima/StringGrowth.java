ee6e3a1dc9bfd8cf436023af26700407a35f0757
Shaima <shaimaalnabhani@gmail.com>
2025-08-04 09:27:04 +0400

    Modify the String Splosion task as per requested

--- From_Shaima/StringGrowth.java
+++ From_Shaima/StringGrowth.java
@@ -1,12 +1,12 @@
 public class StringGrowth {
     public static String generateStringGrowth(String str)
     {
-        StringBuilder newStr= new StringBuilder(); // Create a StringBuilder object to build 'newStr' variable
+        StringBuilder growthBuilder= new StringBuilder(); // Create a StringBuilder object to build 'growthBuilder' variable
         for (int i = 0; i < str.length(); i++)  // Use a for-loop to go through each character in 'str'
         {
-            newStr.append(str.substring(0,i+1));  // add the 'str' substring from the first element until the element of 'i' to the 'newStr'
+            growthBuilder.append(str,0,i+1);  // add the 'str' substring from the first element until the element of 'i' to the 'growthBuilder'
         }
-        return newStr.toString(); // return the final output after converted to string
+        return growthBuilder.toString(); // return the final output after converted to string
     }
     public static void main(String[] args) {
         System.out.println(generateStringGrowth("code"));
