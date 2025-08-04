b946aa54af0d840e3273763da119e8a24c7aa740
Mulk <mulkbadar774@gmail.com>
2025-08-04 16:39:38 +0400

    modified ExtraEnd task

--- From_Mulk/ExtraEnd.java
+++ From_Mulk/ExtraEnd.java
@@ -2,8 +2,10 @@ public class ExtraEnd {
     public String replicateStringEnding(String str) {
         if (str.length() >= 2) {
             return str.substring(str.length() - 2) + str.substring(str.length() - 2) + str.substring(str.length() - 2);
-        } else {
-            return "length less than 2";
         }
-    }
+        return "length less than 2"; }
+
+
 }
+
+
