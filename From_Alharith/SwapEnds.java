ea564e3b2cf863013d5623f7f3e38f6bf1dc1dbc
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-04 10:47:43 +0400

    #TRA-1756: Array1_ SwapEnds Done

---
+++ From_Alharith/SwapEnds.java
@@ -0,0 +1,10 @@
+public class SwapEnds {
+
+    public int[] swapEnds(int[] nums) {
+        int first=nums[0];
+        nums[0]= nums[nums.length-1];
+        nums[nums.length-1] = first;
+        return nums;
+    }
+
+}
