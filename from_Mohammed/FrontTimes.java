5bd1d5cc90f35175a1b825819716b0d9f6d6662b
mohammedanad <alhuseini.moha@gmail.com>
2025-08-04 09:41:30 +0400

    Warmup2: #TRA-2049 FrontTimes done

---
+++ from_Mohammed/FrontTimes.java
@@ -0,0 +1,10 @@
+public String FrontTimes(String str, int n) {
+
+    int frontl =3;
+    if (frontl>str.length())
+        frontl=str.length();
+    String front = str.substring(0, frontl);
+    String res = "";
+    for(int i=0;i<n;i++)
+        res +=front;
+    return res ;}
