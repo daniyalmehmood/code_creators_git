ebcd341762a9429c8dadc617670426fd626394ff
Kaltham <kaltham.aldifai@gmail.com>
2025-08-04 14:43:19 +0400

    modified doubleX Task

--- from_Kaltham/DoubleX.java
+++ from_Kaltham/DoubleX.java
@@ -1,9 +1,9 @@
 public class DoubleX {
     public boolean hasDoubleX(String str) {
-        int postionOfx = str.indexOf('x');
-        if (postionOfx == -1 || postionOfx + 1 >= str.length()) {
+        int postionOfX = str.indexOf('x');
+        if (postionOfX == -1 || postionOfX + 1 >= str.length()) {
             return false;
         }
-        return str.charAt(postionOfx + 1) == 'x';
+        return str.charAt(postionOfX + 1) == 'x';
     }
 }
