e80d686bf0b5c0e28de751b28c45c7a8932b0be2
NASSER1221 <alnassri1221@gmail.com>
2025-08-04 14:43:57 +0400

    added fizzString2 task

---
+++ from_Nasser/FizzString2.java
@@ -0,0 +1,12 @@
+public class FizzString2 {
+    public String fizzString2(int n) {
+
+
+        if(n%3==0 && n%5!=0)return "Fizz!";
+        else if(n%5==0 && n%3!=0)return "Buzz!";
+        else if(n % 5 == 0)return "FizzBuzz!";
+        else return n+"!";
+
+    }
+
+}
