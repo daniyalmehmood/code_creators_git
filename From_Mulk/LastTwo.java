d6ac4cf8d9144b9a1ef539669c8f5da6b821ea65
Mulk <mulkbadar774@gmail.com>
2025-08-04 15:33:49 +0400

    modified LastTwo task

--- From_Mulk/LastTwo.java
+++ From_Mulk/LastTwo.java
@@ -1,12 +1,13 @@
-public class Last2 {
-    public int countRepeatingLast2(String str) {
-        int countX = 0;
+public class LastTwo {
+    public int lastTow(String str) {
+        int countLastChar = 0;
         for (int i = 0; i < str.length() - 2; i++) {
             if (str.substring(i, i + 2).equals(str.substring(str.length() - 2))) {
-                countX++;
+                countLastChar++;
             }
         }
-        return countX;
+
+        return countLastChar;
     }
 
 }
