b5069c5ba5174c52268c6ea6781e4a30c84b3e72
mohammedanad-dev <alhuseini.moha@gmail.com>
2025-08-04 14:30:05 +0400

    Update StringTimes.java

--- from_Mohammed/StringTimes.java
+++ from_Mohammed/StringTimes.java
@@ -1,6 +1,7 @@
-public String StringTimes(String str, int n) {
+public class StringTimes{
+public String stringTimes(String str, int n) {
     String res="";
     for(int i=0;i<n;i++)
         res+=str;
     return res;
-}
+}}
