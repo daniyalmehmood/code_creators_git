729361ac317320584f46a6d0c5634d7f802e0be9
Hawraa <hawra8516@gmail.com>
2025-08-04 09:15:13 +0400

    Array-1: CommonEnd modified

--- from_Hawraa/CommonEnd.java
+++ from_Hawraa/CommonEnd.java
@@ -1,10 +1,9 @@
 public class CommonEnd {
     public static boolean commonEnd(int[] a, int[] b) {
-        if (a.length > 1 && b.length > 1){
+        if (a.length > 0 && b.length > 0){
             if(a[0] == b[0] || a[a.length-1] == b[b.length-1]){ // compare elements of two arrays
                 return true;
             }
-            return false;
         }
         return false;
     }
