c5a06a5e338339556e9104b230a5fc91e970b358
Hawraa <hawra8516@gmail.com>
2025-08-04 10:59:41 +0400

    Array-1: HasTwoOrThree added

---
+++ from_Hawraa/HasTwoOrThree.java
@@ -0,0 +1,11 @@
+public class HasTwoOrThree {
+    public static boolean hasTwoOrThree(int[] nums) {
+        return nums[0] == 2 || nums[0] == 3 || nums[1] == 2 || nums[1] == 3;
+    }
+
+    public static void main(String[] args){
+        System.out.println(hasTwoOrThree(new int[]{2, 5}));
+        System.out.println(hasTwoOrThree(new int[]{4, 3}));
+        System.out.println(hasTwoOrThree(new int[]{4, 5}));
+    }
+}
