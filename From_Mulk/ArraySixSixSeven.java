af92e31b14c6789add816e715a496c4e3817f785
Mulk <mulkbadar774@gmail.com>
2025-08-04 16:27:21 +0400

    modified ArraySixSixSeven task

--- From_Mulk/ArraySixSixSeven.java
+++ From_Mulk/ArraySixSixSeven.java
@@ -1,11 +1,11 @@
 public class ArraySixSixSeven {
-    public int count6FollowedBy6Or7(int[] nums) {
-        int countX = 0;
+    public int countSixFollowedBySixOrSeven(int[] nums) {
+        int sequenceCount = 0;
         for (int i = 0; i < nums.length - 1; i++) {
             if (nums[i] == 6 && (nums[i + 1] == 6 || nums[i + 1] == 7)) {
-                countX++;
+                sequenceCount++;
             }
         }
-        return countX;
+        return sequenceCount;
     }
 }
