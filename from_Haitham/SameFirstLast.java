8b959ea1ff6f196c8eab706faf1600cd57bfda59
haithemhamid <haithemalhinai2017@gmail.com>
2025-08-04 15:41:15 +0400

    sameFirstLast task is done

---
+++ from_Haitham/SameFirstLast.java
@@ -0,0 +1,10 @@
+public class SameFirstLast {
+public boolean sameFirstLast(int[] nums) {
+        return (nums.length > 0 && nums[0] == nums[nums.length - 1]);
+    }
+
+    public int[] makePi() {
+        int[] Pi = new int[]{3, 1, 4};
+        return Pi;
+    }
+}
