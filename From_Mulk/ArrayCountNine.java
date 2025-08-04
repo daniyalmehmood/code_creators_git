cb382759d8bb26552e9dfde25d4e55b76cbd8a6f
Mulk <mulkbadar774@gmail.com>
2025-08-04 14:49:11 +0400

    modified ArrayCountNine task

--- From_Mulk/ArrayCountNine.java
+++ From_Mulk/ArrayCountNine.java
@@ -1,11 +1,11 @@
 public class ArrayCountNine {
     public int count9InArray(int[] nums) {
-        int countX = 0;
+        int count9 = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 9) {
-                countX++;
+                count9++;
             }
         }
-        return countX;
+        return count9;
     }
 }
