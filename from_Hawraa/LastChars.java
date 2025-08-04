7a9a816ee267347a77e660544179ddaff80be47c
Hawraa <hawra8516@gmail.com>
2025-08-04 08:58:45 +0400

    String-1: LastChars modified

--- from_Hawraa/LastChars.java
+++ from_Hawraa/LastChars.java
@@ -1,25 +1,19 @@
 public class LastChars {
-    public static String lastChars(String a, String b) {
-        String firstChar;
-        String lastChar;
+    public static String firstAndLast (String a, String b) {
+        String firstChar = "@";
+        String lastChar = "@";
         if (a.length() > 0) {
             firstChar = a.substring(0, 1);
         }
-        else {
-            firstChar = "@";
-        }
         if (b.length() > 0) {
             lastChar = b.substring(b.length() - 1);
-        }
-        else {
-            lastChar = "@";
         }
         return firstChar + lastChar;
     }
 
     public static void main(String[] args){
-        System.out.println(lastChars("last", "chars"));
-        System.out.println(lastChars("yo", "java"));
-        System.out.println(lastChars("hi", ""));
+        System.out.println(firstAndLast ("last", "chars"));
+        System.out.println(firstAndLast ("yo", "java"));
+        System.out.println(firstAndLast ("hi", ""));
     }
 }
