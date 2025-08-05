e93917887569aa2707044f3b4fefdf06bd75cadc
Mulk <mulkbadar774@gmail.com>
2025-08-05 09:51:01 +0400

    solution of TheEnd task

---
+++ From_Mulk/TheEnd.java
@@ -0,0 +1,12 @@
+public class TheEnd {
+    public String selectFirstOrLastChar(String str, boolean front) {
+        if (front) {
+            return str.substring(0, 1);
+        } else {
+            return str.substring(str.length() - 1);
+        }
+    }
+}
+
+
+
