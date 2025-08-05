0dedb3ea8bb22eaf63d9a2120c78fc7a80023307
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-05 12:18:07 +0400

    #TRA-2271: MAP1_ MapShare Done

---
+++ From_Alharith/MapShare.java
@@ -0,0 +1,11 @@
+public class MapShare {
+
+    public Map<String, String> mapShare(Map<String, String> map) {
+        map.remove("c");
+        if (map.containsKey("a")){
+            map.put("b",map.get("a"));
+        }
+        return map;
+    }
+
+}
