be20ce883cddb74356eede622ea1091a4928bd37
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-05 12:20:12 +0400

    #TRA-2274: MAP1_ Topping2 Done

---
+++ From_Alharith/Topping2.java
@@ -0,0 +1,10 @@
+public class Topping2 {
+
+    public Map<String, String> topping2(Map<String, String> map) {
+        if (map.containsKey("ice cream")) map.put("yogurt", map.get("ice cream"));
+
+        if (map.containsKey("spinach")) map.put("spinach", "nuts");
+        return map;
+    }
+
+}
