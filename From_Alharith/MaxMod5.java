1d8d00cb6fb9b7c95921d3530285929176f71ad3
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-05 10:40:02 +0400

    #TRA-2182: Logic1_ MaxMod5 Done

---
+++ From_Alharith/MaxMod5.java
@@ -0,0 +1,14 @@
+public class MaxMod5 {
+
+    public int maxMod5(int a, int b) {
+        if (a==b) return 0;
+        if (a%5 == b%5) {
+            if (a>b) return b;
+            return a;
+        }
+
+        if (a>b) return a;
+        return b;
+    }
+
+}
