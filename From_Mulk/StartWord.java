40d4936e848164db272ea2ff751606d216210d5b
Mulk <mulkbadar774@gmail.com>
2025-08-05 15:40:44 +0400

    solution of StartWord task

---
+++ From_Mulk/StartWord.java
@@ -0,0 +1,11 @@
+public class StartWord {
+    public String startsLikeWord(String str, String word) {
+        if (word.length() <= str.length()) {
+            if (str.substring(1, word.length()).equals(word.substring(1))) {
+                return str.substring(0, word.length());
+            }
+        }
+        return "";
+    }
+}
+
