d22070d80501eb5723a1a50006f1e26459955044
mohammedanad <alhuseini.moha@gmail.com>
2025-08-05 14:39:20 +0400

    Logic1: #TRA-2246 dumLimit done

---
+++ From_Mohammed/SumLimit.java
@@ -0,0 +1,11 @@
+public class SumLimit {
+    public int sumLimit(int a, int b) {
+        int sum = a+b;
+        if ((String.valueOf(a).length())==(String.valueOf(sum).length()))
+            return sum;
+        return a;
+
+
+    }
+
+}
