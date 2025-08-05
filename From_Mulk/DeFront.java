191e58831402c2807970e0db967ba40c3227d3f7
Mulk <mulkbadar774@gmail.com>
2025-08-05 15:31:17 +0400

    solution of DeFront task

---
+++ From_Mulk/DeFront.java
@@ -0,0 +1,20 @@
+public class DeFront {
+    public String cleanFirstTwoX(String str) {
+        if (str.isEmpty()) {
+            return "";
+
+        }
+        if (str.length() >= 2 && str.charAt(0) == 'x' && str.charAt(1) == 'x') {
+            return str.substring(2);
+        }
+
+        if (str.length() >= 1 && str.charAt(0) == 'x') {
+            return str.substring(1);
+        }
+        if (str.length() >= 1 && str.charAt(1) == 'x') {
+            return str.charAt(0) + str.substring(2);
+        }
+        return str;
+
+    }
+}
