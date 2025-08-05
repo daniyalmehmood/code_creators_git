5282a37c3ea7a62f086aaae046c8398811395f46
Hawraa <hawra8516@gmail.com>
2025-08-05 11:22:23 +0400

    Array-1: plusTwo added

---
+++ from_Hawraa/PlusTwo.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class PlusTwo {
+    public static int[] plusTwoArray(int[] a, int[] b) {
+        return new int[]{a[0], a[1], b[0], b[1]};
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(plusTwoArray(new int[]{1, 2}, new int[]{3, 4})));
+        System.out.println(Arrays.toString(plusTwoArray(new int[]{4, 4}, new int[]{2, 2})));
+        System.out.println(Arrays.toString(plusTwoArray(new int[]{9, 2}, new int[]{3, 4})));
+    }
+}
