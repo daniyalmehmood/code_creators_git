fca87b0dd01847e3ba0a1c67d62545bb74e5aaee
Mulk <mulkbadar774@gmail.com>
2025-08-05 12:18:30 +0400

    solution of HasBad task

---
+++ From_Mulk/HasBad.java
@@ -0,0 +1,9 @@
+public class HasBad {
+    public boolean hasBadAtStart(String str) {
+        if (str.length() >= 3 && str.substring(0, 3).equals("bad") || str.length() >= 4 && str.substring(1, 4).equals("bad")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
