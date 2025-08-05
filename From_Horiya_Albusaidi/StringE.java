7be67146474158959d62a1577ce36fc5e973f4d3
EchoCoder20 <albusaidi1915@gmail.com>
2025-08-05 16:30:25 +0400

    Modify the code format

--- From_Horiya_Albusaidi/StringE.java
+++ From_Horiya_Albusaidi/StringE.java
@@ -1,9 +1,7 @@
 public class StringE {
     public static boolean stringE(String str) {
         int counter = 0;
-        for (int i = 0; i < str.length(); ;
-        i++)
-        {
+        for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) == 'e') counter++;
 
         }
