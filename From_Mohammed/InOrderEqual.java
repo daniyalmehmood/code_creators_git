2564343e241a7939b808d06e4cee9c5203df1e0f
mohammedanad <alhuseini.moha@gmail.com>
2025-08-05 14:23:30 +0400

    Logic1: #TRA-2236 inOrderEqual done

---
+++ From_Mohammed/InOrderEqual.java
@@ -0,0 +1,9 @@
+public class InOrderEqual {
+    public boolean inOrderEqual(int a, int b, int c, boolean equalOk) {
+
+        if(a<b && b<c) return true;
+        if (equalOk && (a<=b && b<=c)) return true;
+        return false;
+    }
+
+}
