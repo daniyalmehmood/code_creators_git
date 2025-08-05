21d286a6f086b089ed7632323296cd49fa6a1c10
Ghaida02a <ggmskaj@gmail.com>
2025-08-05 09:55:48 +0400

    #TRA-2035: String-3. sameEnds modified function name

--- from_Ghaida/StartSameEnds.java
+++ from_Ghaida/StartSameEnds.java
@@ -1,10 +1,10 @@
 public class StartSameEnds {
     public static void main(String[] args) {
-        System.out.println(checkIfStartsameEnds("abXYab")); // → "ab"
-        System.out.println(checkIfStartsameEnds("xx"));// → "x"
-        System.out.println(checkIfStartsameEnds("xxx"));// → "x"
+        System.out.println(checkIfStartSameEnds("abXYab")); // → "ab"
+        System.out.println(checkIfStartSameEnds("xx"));// → "x"
+        System.out.println(checkIfStartSameEnds("xxx"));// → "x"
     }
-    public static String checkIfStartsameEnds(String string) {
+    public static String checkIfStartSameEnds(String string) {
         for (int i = string.length() / 2; i > 0; i--) { //start from middle and go down
             //the longest possible prefix that can match a suffix can only be at most length/2 chars long
             String prefixOfString = string.substring(0, i);
