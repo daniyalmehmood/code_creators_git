85c16cf66e6b730dd19a7abe6bd5db6fc80769a4
EchoCoder20 <albusaidi1915@gmail.com>
2025-08-05 15:48:51 +0400

    Simplify the code logic

--- From_Horiya_Albusaidi/DoubleX.java
+++ From_Horiya_Albusaidi/DoubleX.java
@@ -1,14 +1,10 @@
 public class StringDoubleXChecker {
-   public boolean checkDoubleX(String str) {
+    public boolean checkDoubleX(String str) {
         for (int i = 0; i < str.length() - 1; i++) {
 
             if (str.charAt(i) == 'x') {
-                if (str.charAt(i) == str.charAt(i + 1))
-                    return true;
-                else
-                    return false;
+                return str.charAt(i) == str.charAt(i + 1);
             }
-
 
         }
         return false;
