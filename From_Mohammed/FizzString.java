91c1efe94b61275d4ac9cecb8a4080f194e599f2
mohammedanad <alhuseini.moha@gmail.com>
2025-08-05 14:12:30 +0400

    Logic1: #TRA-2232 fizzString done

---
+++ From_Mohammed/FizzString.java
@@ -0,0 +1,9 @@
+public class FizzString {
+    public String fizzString(String str) {
+        if (str.startsWith("f") && str.endsWith("b")) return "FizzBuzz";
+        if (str.startsWith("f")) return "Fizz";
+        if (str.endsWith("b")) return "Buzz";
+        return str;
+    }
+
+}
