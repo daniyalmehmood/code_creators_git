c25d798b163dedf32ff42e9a4f220c9b40a95162
Mulk <mulkbadar774@gmail.com>
2025-08-05 16:22:08 +0400

    solution of SameFirstLast task

---
+++ From_Mulk/SameFirstLast.java
@@ -0,0 +1,9 @@
+public class SameFirstLast {
+    public boolean equalFirstAndLastElement(int[] nums) {
+        if (nums.length >= 1 && nums[0] == nums[nums.length - 1]) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
