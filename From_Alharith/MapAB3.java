f69e3b106cd4242c330f3fd12b02d6ba0e810197
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-05 12:21:57 +0400

    #TRA-2277: MAP1_ MapAB3 Done

---
+++ From_Alharith/MapAB3.java
@@ -0,0 +1,9 @@
+public class MapAB3 {
+
+    public Map<String, String> mapAB3(Map<String, String> map) {
+        if (!map.containsKey("a")&& map.containsKey("b")) map.put ("a", map.get("b") );
+        if (!map.containsKey("b")&& map.containsKey("a")) map.put ("b", map.get("a") );
+        return map;
+    }
+
+}
