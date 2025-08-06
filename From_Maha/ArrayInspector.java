6fb7a4f8c691c292e86f55f83ffe3663d55b6ffd
Maha <mahathamood98@gmail.com>
2025-08-06 15:44:28 +0400

    Editing

--- From_Maha/ArrayInspector.java
+++ From_Maha/ArrayInspector.java
@@ -1,5 +1,5 @@
 public class ArrayInspector {
-    public boolean unlucky1(int[] nums) {
+    public boolean hasUnluckySequence(int[] nums) {
         if (nums.length >= 2) {
             if ((nums[0] == 1 && nums[1] == 3) ||
                     (nums.length >= 3 && nums[1] == 1 && nums[2] == 3) ||
