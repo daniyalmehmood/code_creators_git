d26533ed053a27cc3e230aa0f3cb53c96dc988eb
Mulk <mulkbadar774@gmail.com>
2025-08-06 14:22:55 +0400

    solution of UnluckyOne task

---
+++ From_Mulk/UnluckyOne.java
@@ -0,0 +1,17 @@
+public class UnluckyOne {
+    public boolean unluckyOneThreeAtStartOrEnd(int[] nums) {
+        if (nums.length >= 2 && nums[0] == 1 && nums[1] == 3) {
+            return true;
+        }
+        if (nums.length >= 2 && nums[1] == 1 && nums[2] == 3) {
+            return true;
+        }
+        if (nums.length >= 2 && nums[nums.length - 2] == 1 && nums[nums.length - 1] == 3) {
+            return true;
+        }
+        return false;
+    }
+}
+
+
+
