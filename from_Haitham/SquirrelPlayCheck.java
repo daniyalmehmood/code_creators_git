d9420e52cbd77f948eeae1e791c0ee8ea232a38e
haithemhamid <haithemalhinai2017@gmail.com>
2025-08-06 10:00:37 +0400

    SquirrelPlayCheck task is done

---
+++ from_Haitham/SquirrelPlayCheck.java
@@ -0,0 +1,7 @@
+public class SquirrelPlayCheck {
+public boolean squirrelPlay(int temp, boolean isSummer) {
+        if (isSummer && temp >= 60 && temp <= 100) return true;
+        else if (temp >= 60 && temp <= 90) return true;
+        return false;
+    }
+}
