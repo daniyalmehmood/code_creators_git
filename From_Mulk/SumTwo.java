af5d7b2d2c34f210256de6c450da09ccf41de2a7
Mulk <mulkbadar774@gmail.com>
2025-08-06 09:17:52 +0400

    solution of SumTwo task

---
+++ From_Mulk/SumTwo.java
@@ -0,0 +1,18 @@
+public class SumTwo {
+    public int sumFirstTwoNumbers(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (nums.length >= 2) {
+            return nums[0] + nums[1];
+        }
+        if (nums.length < 2) {
+            return nums[0];
+        }
+        if (nums.length == 0) {
+        }
+        return 0;
+    }
+
+
+}
