056ceb0083dcd60127185f9eecbfd8b1ec0b39b8
Mulk <mulkbadar774@gmail.com>
2025-08-06 15:08:32 +0400

    Solution of DateFashion task

---
+++ From_Mulk/DateFashion.java
@@ -0,0 +1,12 @@
+public class DateFashion {
+    public int evaluateDateFashion(int you, int date) {
+        if (you <= 2 || date <= 2) {
+            return 0;
+        }
+        if (you >= 8 || date >= 8) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+}
