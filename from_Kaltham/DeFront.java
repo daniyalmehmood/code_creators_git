56193af3b5fc7afc98524db45bd2a91482cbe978
Kaltham <kaltham.aldifai@gmail.com>
2025-08-06 15:20:57 +0400

    modified DeFront Task

--- from_Kaltham/DeFront.java
+++ from_Kaltham/DeFront.java
@@ -1,12 +1,12 @@
 public class DeFront {
     public String deFront(String str) {
-        if (str.length() >= 2 && str.substring(0, 2).equals("ab")) {
+        if (str.substring(0, 2).equals("ab")) {
             return str;
         }
-        if (str.length() >= 2 && str.charAt(0) == 'a') {
+        if (str.charAt(0) == 'a') {
             return str.substring(0, 1) + str.substring(2);
         }
-        if (str.length() >= 2 && str.charAt(1) == 'b') {
+        if (str.charAt(1) == 'b') {
             return str.substring(1, 2) + str.substring(2);
         }
 
