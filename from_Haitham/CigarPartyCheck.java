00e88d9020567a54800a376769e37ac9dc7b9ba5
haithemhamid <haithemalhinai2017@gmail.com>
2025-08-06 09:52:49 +0400

    CigarPartyCheck task is done

---
+++ from_Haitham/CigarPartyCheck.java
@@ -0,0 +1,8 @@
+public class CigarPartyCheck {
+public boolean cigarParty(int cigars, boolean isWeekend) {
+        if (isWeekend && cigars >= 40)
+            return true;
+        else if (cigars >= 40 && cigars <= 60) return true;
+        return false;
+    }
+}
