df83e1e4f3bc9670e3641e6b4a41992156eb6d2c
EchoCoder20 <albusaidi1915@gmail.com>
2025-08-06 11:29:30 +0400

    Solving Logic-1 > fizzString2 Task

---
+++ From_Horiya_Albusaidi/DivisibleNumber.java
@@ -0,0 +1,9 @@
+public class DivisibleNumber {
+    public String stringBasedOnReminderValue(int n) {
+        if (n % 3 == 0 && n % 5 == 0) return "FizzBuzz!";
+        else if (n % 3 == 0) return "Fizz!";
+        else if (n % 5 == 0) return "Buzz!";
+        else return n + "!";
+
+    }
+}
