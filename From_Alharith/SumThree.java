1369c9ebc1e3cfba6cec2ac25d68d2af567eb99a
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 11:06:30 +0400

    Update and rename Sum3.java to SumThree.java

--- From_Alharith/SumThree.java
+++ From_Alharith/SumThree.java
@@ -1,6 +1,6 @@
-public class Sum3 {
+public class SumThree {
 
-    public int sum3(int[] nums) {
+    public int sumOfThree(int[] nums) {
         int sum;
         sum= nums[0] + nums[1] +nums[2];
         return sum;
