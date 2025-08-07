515da9cbea991452838f443b889e44b477da080e
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 08:23:11 +0400

    Update and rename LoneTeen.java to TeenChecker.java

--- From_Alharith/TeenChecker.java
+++ From_Alharith/TeenChecker.java
@@ -1,6 +1,6 @@
-public class LoneTeen {
+public class TeenChecker  {
 
-    public boolean loneTeen(int a, int b) {
+    public boolean isOnlyOneTeen(int a, int b) {
         if ((a>=13 && a<=19) && (b>=13 && b<=19)) return false;
         if ((a>=13 && a<=19) || (b>=13 && b<=19)) return true;
         return false;
