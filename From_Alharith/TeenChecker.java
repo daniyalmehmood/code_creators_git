ca16e1db0727a79ce493053ff8aa86be8261e7f3
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 08:22:04 +0400

    Update and rename HasTeen.java to TeenChecker.java

--- From_Alharith/TeenChecker.java
+++ From_Alharith/TeenChecker.java
@@ -1,6 +1,6 @@
-public class HasTeen {
+public class TeenChecker  {
 
-    public boolean hasTeen(int a, int b, int c) {
+    public boolean hasTeenInRange(int a, int b, int c) {
         if ((a>=13 && a<=19) || (b>=13 && b<=19) || (c>=13 && c<=19)) return true;
         return false;
     }
