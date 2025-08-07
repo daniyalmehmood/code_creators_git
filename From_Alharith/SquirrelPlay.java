71336f627fc013f64b1a64faff3a459913fcf577
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 11:58:58 +0400

    Update SquirrelPlay.java

--- From_Alharith/SquirrelPlay.java
+++ From_Alharith/SquirrelPlay.java
@@ -1,6 +1,6 @@
 public class SquirrelPlay {
 
-    public boolean squirrelPlay(int temp, boolean isSummer) {
+    public boolean isSquirrelPlayTime(int temp, boolean isSummer) {
         if (temp>100 || temp < 60) return false;
         if ((temp>=60 && temp<=90) || isSummer) return true;
         return false;
