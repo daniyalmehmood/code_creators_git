76c8c73cd402e06378a8e254b4705cac5c584e3d
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 09:52:21 +0400

    Update and rename WithouEnd2.java to StringTrimmer.java

--- From_Alharith/StringTrimmer.java
+++ From_Alharith/StringTrimmer.java
@@ -1,6 +1,6 @@
-public class WithouEnd2 {
+public class StringTrimmer {
 
-    public String withouEnd2(String str) {
+    public String removeFirstAndLastIfPossible(String str) {
         if (str.length()<2) return "";
         return str.substring(1,(str.length()-1));
     }
