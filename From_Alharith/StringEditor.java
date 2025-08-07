1295b4eaf2325dd1b2f7144fb317e9697ec4117f
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 08:14:11 +0400

    Update and rename MissingChar.java to StringEditor.java

--- From_Alharith/StringEditor.java
+++ From_Alharith/StringEditor.java
@@ -1,5 +1,5 @@
-public class MissingChar {
-    public String missingChar(String str, int n) {
+public class StringEditor  {
+    public String removeCharacterAtIndex(String str, int n) {
         String front= str.substring(0,n);
         String back= str.substring(n+1);
 
