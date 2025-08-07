90129e54ac298fafae902a7ae0f837f2b844694b
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 11:26:34 +0400

    Update and rename Sum2.java to SumTwo.java

--- From_Alharith/SumTwo.java
+++ From_Alharith/SumTwo.java
@@ -1,6 +1,6 @@
-public class Sum2 {
+public class SumTwo {
 
-    public int sum2(int[] nums) {
+    public int sumFirstTwo(int[] nums) {
         if (nums.length<1) return 0;
         if (nums.length==1) return nums[0];
         return nums[0] + nums[1];
