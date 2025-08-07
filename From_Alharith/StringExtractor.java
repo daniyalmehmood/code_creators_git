55df5034abd34219e833310a94b3c8aeae6578ba
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 09:46:35 +0400

    Update and rename FirstTwo.java to StringExtractor.java

--- From_Alharith/StringExtractor.java
+++ From_Alharith/StringExtractor.java
@@ -1,6 +1,6 @@
-public class FirstTwo {
+public class StringExtractor  {
 
-    public String firstTwo(String str) {
+    public String getFirstTwoChars(String str) {
         if (str.length()<2) return str;
         return str.substring(0,2);
     }
