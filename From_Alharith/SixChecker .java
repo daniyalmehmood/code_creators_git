32e9b2a6ffb354201a5ea1dc895dd8905adb68b8
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 12:03:31 +0400

    Update and rename Love6.java to SixChecker .java

--- From_Alharith/SixChecker .java
+++ From_Alharith/SixChecker .java
@@ -1,6 +1,6 @@
-public class Love6 {
+public class SixChecker  {
 
-    public boolean love6(int a, int b) {
+    public boolean isLoveSix(int a, int b) {
         int sum= a+b;
         int diff= Math.abs(a-b);
         return ((a==6 || b==6) || (sum==6) || (diff==6)) ;
