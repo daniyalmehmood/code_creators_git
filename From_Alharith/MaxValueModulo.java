6f7341c2d9b78cba00e01138013168f2d4789eb3
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 14:37:55 +0400

    Update and rename MaxMod5.java to MaxValueModulo.java

--- From_Alharith/MaxValueModulo.java
+++ From_Alharith/MaxValueModulo.java
@@ -1,6 +1,6 @@
-public class MaxMod5 {
+public class MaxValueModulo {
 
-    public int maxMod5(int a, int b) {
+    public int getMaxConsideringModuloFiveRule(int a, int b) {
         if (a==b) return 0;
         if (a%5 == b%5) {
             if (a>b) return b;
