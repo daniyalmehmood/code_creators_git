a0d009eb3a7e12c83de8e305f795e62ff31760c3
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 12:02:42 +0400

    Update AlarmClock.java

--- From_Alharith/AlarmClock.java
+++ From_Alharith/AlarmClock.java
@@ -1,6 +1,6 @@
 public class AlarmClock {
 
-    public String alarmClock(int day, boolean vacation) {
+    public String getAlarmTime(int day, boolean vacation) {
         if ((day==0 || day ==6) && vacation ) return "off";
         if (day>=1 && day<=5 && !vacation) return "7:00";
         return "10:00";
