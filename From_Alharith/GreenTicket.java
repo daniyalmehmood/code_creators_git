19d56e8440edaa235f629c516a224cbb79503980
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 14:25:23 +0400

    Update GreenTicket.java

--- From_Alharith/GreenTicket.java
+++ From_Alharith/GreenTicket.java
@@ -1,6 +1,6 @@
 public class GreenTicket {
 
-    public int greenTicket(int a, int b, int c) {
+    public int evaluateGreenTicketScore(int a, int b, int c) {
         if (a==b && b==c) return 20;
         if (a==b || a==c || b==c) return 10;
         return 0;
