c8f1e1733303e848530cf49d1f4540c800b7ac07
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 08:11:55 +0400

    Update NearHundred.java

--- From_Alharith/NearHundred.java
+++ From_Alharith/NearHundred.java
@@ -1,6 +1,6 @@
-public class NearHundred {
+public class NumberRangeChecker  {
 
-    public boolean nearHundred(int n) {
+    public boolean isNearHundredOrTwoHundred(int n) {
         if (Math.abs(100 - n) <= 10 || Math.abs(200 - n) <= 10) {
             return true;
         }
