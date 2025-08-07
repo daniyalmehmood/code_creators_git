1c9010d41445deb6226b03fbfcc3325686c5f351
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 11:58:07 +0400

    Update DateFashion.java

--- From_Alharith/DateFashion.java
+++ From_Alharith/DateFashion.java
@@ -1,6 +1,6 @@
 public class DateFashion {
 
-    public int dateFashion(int you, int date) {
+    public int getTableChance(int you, int date) {
         if (you <=2 || date <=2 ) return 0;
 
         if (you>=8|| date>=8) return 2;
