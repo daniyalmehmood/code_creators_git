fec93df7a6861e69ff6bfb33ae326c95aa922873
Shatha Almamari <shatha.almamari99@gmail.com>
2025-08-07 12:10:28 +0400

    #TRA-1691-deFront modified

--- from_shatha/DeFront.java
+++ from_shatha/DeFront.java
@@ -1,5 +1,5 @@
 public class DeFront {
-    public static String deFront(String str) {
+    public static String keepAorBFront(String str) {
         String result = "";
 
         if (str.length() >= 1 && str.charAt(0) == 'a') {
@@ -17,8 +17,8 @@ public class DeFront {
         return result;
     }
     public static void main(String[] args) {
-        System.out.println(deFront("Hello"));
-        System.out.println(deFront("java"));
-        System.out.println(deFront("away"));
+        System.out.println(keepAorBFront("Hello"));
+        System.out.println(keepAorBFront("java"));
+        System.out.println(keepAorBFront("away"));
     }
 }
