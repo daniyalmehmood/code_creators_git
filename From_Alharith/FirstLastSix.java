e899840d135accc86ae667d311c046cb2957ed2f
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 11:02:49 +0400

    Update and rename FirstLast6.java to FirstLastSix.java

--- From_Alharith/FirstLastSix.java
+++ From_Alharith/FirstLastSix.java
@@ -1,6 +1,6 @@
-public class FirstLast6 {
+public class FirstLastSix {
 
-    public boolean firstLast6(int[] nums) {
+    public boolean isFirstOrLastSix(int[] nums) {
         if (nums[0]==6 || nums[nums.length-1]==6) return true;
         return false;
     }
