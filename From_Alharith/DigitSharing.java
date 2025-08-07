7b808b17942de76596c85d3c6862e758df5c7bf0
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 14:24:17 +0400

    Update and rename ShareDigit.java to DigitSharing.java

--- From_Alharith/DigitSharing.java
+++ From_Alharith/DigitSharing.java
@@ -1,6 +1,6 @@
-public class ShareDigit {
+public class DigitSharing {
 
-    public boolean shareDigit(int a, int b) {
+    public boolean hasSharedDigit(int a, int b) {
         if ((a%10 == b/10) || (a/10 == b%10) || (a%10 ==b%10) || (a/10 == b/10)) return true;
         return false;
     }
