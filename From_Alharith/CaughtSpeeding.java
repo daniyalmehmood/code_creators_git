83956682e16d48a3c90c82986ea56d69027c2379
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 11:59:52 +0400

    Update CaughtSpeeding.java

--- From_Alharith/CaughtSpeeding.java
+++ From_Alharith/CaughtSpeeding.java
@@ -1,6 +1,6 @@
 public class CaughtSpeeding {
 
-    public int caughtSpeeding(int speed, boolean isBirthday) {
+    public int getTicketType(int speed, boolean isBirthday) {
         if (speed<=60 || ((speed>60 && speed<=65) && isBirthday )) return 0;
         if ((speed>60 && speed<=80) || (speed>80 && speed<=85 && isBirthday)) return 1;
         return 2;
