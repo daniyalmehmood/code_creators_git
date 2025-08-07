72d5d917171cd2d8be6148bc2ebc8aa9bb9c61aa
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 08:30:30 +0400

    Update and rename In3050.java to RangeChecker.java

--- From_Alharith/RangeChecker.java
+++ From_Alharith/RangeChecker.java
@@ -1,6 +1,6 @@
-public class In3050 {
+public class RangeChecker  {
 
-    public boolean in3050(int a, int b) {
+    public boolean areBothInSameRange30to40or40to50(int a, int b) {
         if ((a >= 30 && a <= 40 && b >= 30 && b <= 40) || (a >= 40 && a <= 50 && b >= 40 && b <= 50)) {
             return true;
         }
