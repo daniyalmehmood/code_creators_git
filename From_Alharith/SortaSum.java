e9b3560fd88a73796611581080ef0f4e035a2091
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 12:00:58 +0400

    Update SortaSum.java

--- From_Alharith/SortaSum.java
+++ From_Alharith/SortaSum.java
@@ -1,6 +1,6 @@
 public class SortaSum {
 
-    public int sortaSum(int a, int b) {
+    public int calculateSpecialSum(int a, int b) {
         int sum = a+b;
         if (sum <=19 && sum >=10) return 20;
         return sum;
