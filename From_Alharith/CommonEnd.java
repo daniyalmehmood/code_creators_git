af96a7f5d0dca5573ff5a00287bfd2f08042ddbf
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 11:05:42 +0400

    Update CommonEnd.java

--- From_Alharith/CommonEnd.java
+++ From_Alharith/CommonEnd.java
@@ -1,6 +1,6 @@
 public class CommonEnd {
 
-    public boolean commonEnd(int[] a, int[] b) {
+    public boolean hasCommonStartOrEnd(int[] a, int[] b) {
         if (a.length>=1 && b.length>=1){
             if (a[0]==b[0] || a[a.length-1]==b[b.length-1]) return true;
         }
