b62b242b71be3490793d5f555fc6aab190eea730
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 14:13:38 +0400

    Update and rename TeenSum.java to TeenRangeSumCalculator .java

--- From_Alharith/TeenRangeSumCalculator .java
+++ From_Alharith/TeenRangeSumCalculator .java
@@ -1,6 +1,6 @@
-public class TeenSum {
+public class TeenRangeSumCalculator  {
 
-    public int teenSum(int a, int b) {
+    public int calculateSumConsideringTeenRange(int a, int b) {
         int sum=a+b;
         if ((a>=13 && a<=19) || (b>=13 && b<=19)) return 19 ;
         return sum;
