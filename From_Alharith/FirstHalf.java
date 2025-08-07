a8bb2035b34a8705fa5eab4c6b30a56a8ab62146
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 09:47:13 +0400

    Update and rename FirstHalf.java to StringSplitter.java

--- From_Alharith/FirstHalf.java
+++
@@ -1,7 +0,0 @@
-public class FirstHalf {
-
-    public String firstHalf(String str) {
-        return str.substring(0, str.length()/2);
-    }
-
-}
---
+++ From_Alharith/StringSplitter.java
@@ -0,0 +1,7 @@
+public class StringSplitter  {
+
+    public String getFirstHalf(String str) {
+        return str.substring(0, str.length()/2);
+    }
+
+}
