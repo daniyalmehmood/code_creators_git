58846408f70b28d6a8e3cb61211385c8a8c13032
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 11:41:00 +0400

    Update and rename Start1.java to StartOne.java

--- From_Alharith/StartOne.java
+++ From_Alharith/StartOne.java
@@ -1,6 +1,6 @@
-public class Start1 {
+public class StartOne {
 
-    public int start1(int[] a, int[] b) {
+    public int countStartingOnes(int[] a, int[] b) {
         int result=0;
         if (a.length>=1 &&a[0]==1  ) result+=1;
         if ( b.length>=1&&b[0]==1) result+= 1;
