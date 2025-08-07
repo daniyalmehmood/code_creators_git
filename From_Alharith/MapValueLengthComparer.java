13f7eb214feb97ecb9f86daabd1ba11fefdc5767
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 14:40:09 +0400

    Update and rename MapAB4.java to MapValueLengthComparer.java

--- From_Alharith/MapValueLengthComparer.java
+++ From_Alharith/MapValueLengthComparer.java
@@ -1,6 +1,6 @@
-public class MapAB4 {
+public class MapValueLengthComparer  {
 
-    public Map<String, String> mapAB4(Map<String, String> map) {
+    public Map<String, String> compareAndModifyMapValues(Map<String, String> map) {
         if (map.containsKey("a") && map.containsKey("b")){
             if (map.get("a").length()>map.get("b").length()) map.put("c", map.get("a") );
             else if (map.get("b").length()>map.get("a").length()) map.put("c", map.get("b") );
