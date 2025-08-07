7e6280d765d2c8ea56fd8e6d9a80a27fd2144461
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 08:41:58 +0400

    Update and rename StringTimes.java to StringRepeater.java

--- From_Alharith/StringRepeater.java
+++ From_Alharith/StringRepeater.java
@@ -1,5 +1,5 @@
-public class StringTimes {
-    public String stringTimes(String str, int n) {
+public class StringRepeater  {
+    public String repeatStringNTimes(String str, int n) {
         n = Math.abs(n);
         String result = "";
         for (int i = 0; i < n; i++) {
