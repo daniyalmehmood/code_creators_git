4a7934ce414305b484ba6c7cecd5d3de1357444c
Kaltham <kaltham.aldifai@gmail.com>
2025-08-07 09:38:49 +0400

    The solution for doubleChar Task

--- from_Kaltham/DoubleChars.java
+++ from_Kaltham/DoubleChars.java
@@ -1,12 +1,12 @@
 public class DoubleChars {
     public String doubleEachChar(String str) {
-        StringBuilder str1 = new StringBuilder();
+        StringBuilder result = new StringBuilder();
         for (int i = 0; i < str.length(); i++) {
             for (int j = 0; j < 2; j++) {
-                str1.append(str.charAt(i));
+                result.append(str.charAt(i));
             }
 
         }
-        return str1.toString();
+        return result.toString();
     }
 }
