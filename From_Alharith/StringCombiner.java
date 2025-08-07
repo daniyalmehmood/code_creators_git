d897b12054731d54bcceebf2733b525c51a189bc
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 09:48:30 +0400

    Update and rename ComboString.java to StringCombiner.java

--- From_Alharith/StringCombiner.java
+++ From_Alharith/StringCombiner.java
@@ -1,6 +1,6 @@
-public class ComboString {
+public class StringCombiner  {
 
-    public String comboString(String a, String b) {
+    public String wrapShorterAroundLonger(String a, String b) {
         if (a.length()>b.length()) return b+a+b;
         if (a.length()<b.length()) return a+b+a;
         return "";
