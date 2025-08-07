781d20a829f4aadd6cf90493f1690cb4f5916d80
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 11:56:58 +0400

    Update and rename CigarParty.java to PartyCigarChecker.java

--- From_Alharith/PartyCigarChecker.java
+++ From_Alharith/PartyCigarChecker.java
@@ -1,6 +1,6 @@
-public class CigarParty {
+public class PartyCigarChecker  {
 
-    public boolean cigarParty(int cigars, boolean isWeekend) {
+    public boolean isSuccessfulParty(int cigars, boolean isWeekend) {
         if (cigars<40) return false;
         if (isWeekend ||(cigars<=60 && cigars>=40)) return true;
         return false;
