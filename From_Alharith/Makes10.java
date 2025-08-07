26f93d923ff4230ed728e2ada80168af7b2798ad
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 08:10:59 +0400

    Update Makes10.java

--- From_Alharith/Makes10.java
+++ From_Alharith/Makes10.java
@@ -1,6 +1,6 @@
-public class Makes10 {
+public class NumberCheck  {
 
-    public boolean makes10(int a, int b) {
+    public boolean isTenOrSumTen(int a, int b) {
         if (a+b==10 || a==10 || b==10){
             return true;
 
