fa79cdeb00874d8ea7454e1c3b584a0ad6512384
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 08:20:31 +0400

    Update IcyHot.java

--- From_Alharith/IcyHot.java
+++ From_Alharith/IcyHot.java
@@ -1,6 +1,6 @@
-public class IcyHot {
+public class TemperatureChecker  {
 
-    public boolean icyHot(int temp1, int temp2) {
+    public boolean isIcyHot(int temp1, int temp2) {
         if ((temp1 <0 && temp2 > 100) || (temp2 <0 && temp1 > 100) ) return true;
         return false;
     }
