a37606482ebb1d7e9f902247752510d5b05c8aaf
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 11:55:53 +0400

    Update and rename Front11.java to FirstElementsFromBoth .java

--- From_Alharith/FirstElementsFromBoth .java
+++ From_Alharith/FirstElementsFromBoth .java
@@ -1,6 +1,6 @@
-public class Front11 {
+public class FirstElementsFromBoth  {
 
-    public int[] front11(int[] a, int[] b) {
+    public int[] getFirstElements(int[] a, int[] b) {
         if (a.length>=1 && b.length>=1) return new int[] {a[0],b[0]};
         if (a.length<1 && b.length>1) return new int[] {b[0]};
         if (a.length>1 && b.length<1) return new int[] {a[0]};
