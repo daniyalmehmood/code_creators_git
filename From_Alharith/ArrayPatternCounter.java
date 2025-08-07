131918c625dcce498afb6b4129a3fdd799a92c1a
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 09:27:55 +0400

    Update and rename Array667.java to ArrayPatternCounter.java

--- From_Alharith/ArrayPatternCounter.java
+++ From_Alharith/ArrayPatternCounter.java
@@ -1,6 +1,6 @@
-public class Array667 {
+public class ArrayPatternCounter  {
 
-    public int array667(int[] nums) {
+    public int countSixFollowedBySixOrSeven(int[] nums) {
         int count = 0;
         for (int i = 0; i < nums.length - 1; i++) {
             if (nums[i] == 6 && (nums[i + 1] == 6 || nums[i + 1] == 7)) {
