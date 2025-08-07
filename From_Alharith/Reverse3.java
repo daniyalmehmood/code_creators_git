6b85abd425b05ce1b09568959ae2c03fcc266aa5
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 11:08:23 +0400

    Update and rename Reverse3.java to ReverseThree.java

--- From_Alharith/Reverse3.java
+++
@@ -1,7 +0,0 @@
-public class Reverse3 {
-
-    public int[] reverse3(int[] nums) {
-        return new int[] {nums[2],nums[1],nums[0]};
-    }
-
-}
---
+++ From_Alharith/ReverseThree.java
@@ -0,0 +1,7 @@
+public class ReverseThree {
+
+    public int[] reverseThreeElements(int[] nums) {
+        return new int[] {nums[2],nums[1],nums[0]};
+    }
+
+}
