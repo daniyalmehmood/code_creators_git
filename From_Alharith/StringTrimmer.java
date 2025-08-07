277c3dd2cac181f738a7b67a4362ede337c25b47
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 09:47:51 +0400

    Update and rename WithoutEnd.java to StringTrimmer.java

---
+++ From_Alharith/StringTrimmer.java
@@ -0,0 +1,9 @@
+public class StringTrimmer  {
+
+    public String removeFirstAndLastChar(String str) {
+
+        return str.substring (1, str.length()-1);
+
+    }
+
+}
--- From_Alharith/WithoutEnd.java
+++
@@ -1,9 +0,0 @@
-public class WithoutEnd {
-
-    public String withoutEnd(String str) {
-
-        return str.substring (1, str.length()-1);
-
-    }
-
-}
