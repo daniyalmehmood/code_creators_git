f3091888dc5e45c5adefa3801c5a72cd4e551d94
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 11:40:14 +0400

    Update and rename Fix23.java to FixTwoThree.java

--- From_Alharith/FixTwoThree.java
+++ From_Alharith/FixTwoThree.java
@@ -1,6 +1,6 @@
-public class Fix23 {
+public class FixTwoThree {
 
-    public int[] fix23(int[] nums) {
+    public int[] replaceThreeAfterTwoWithZero(int[] nums) {
         if (nums[0] == 2 && nums[1]== 3) return new int[] {nums[0],0,nums[2]};
         if (nums[1]==2 && nums[2] ==3) return new int[] {nums[0],nums[1],0};
         return nums;
