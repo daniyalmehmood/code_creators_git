eef499746e1701be4c54cbafa5b184d8be38348e
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 09:33:08 +0400

    Update and rename HelloName.java to Greeter.java

---
+++ From_Alharith/Greeter.java
@@ -0,0 +1,7 @@
+public class Greeter  {
+
+    public String greetByName(String name) {
+        return "Hello " + name+"!";
+    }
+
+}
--- From_Alharith/HelloName.java
+++
@@ -1,7 +0,0 @@
-public class HelloName {
-
-    public String helloName(String name) {
-        return "Hello " + name+"!";
-    }
-
-}
