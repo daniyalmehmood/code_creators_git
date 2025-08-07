5dc1e01b6024a65d4070b0de92a3307b054fec0e
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 14:44:48 +0400

    Update and rename Topping3.java to ToppingValueDuplicator.java

--- From_Alharith/ToppingValueDuplicator.java
+++ From_Alharith/ToppingValueDuplicator.java
@@ -1,6 +1,6 @@
-public class Topping3 {
+public class ToppingValueDuplicator  {
 
-    public Map<String, String> topping3(Map<String, String> map) {
+    public Map<String, String> duplicateToppingValues(Map<String, String> map) {
         if (map.containsKey("potato")) map.put("fries", map.get("potato"));
         if (map.containsKey("salad")) map.put("spinach", map.get("salad"));
         return map;
