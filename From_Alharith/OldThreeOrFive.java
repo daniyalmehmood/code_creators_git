7e73e2810bfdfe098a30bcfce8fb5a9e56008d2a
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 12:08:09 +0400

    Update and rename Old35.java to OldThreeOrFive.java

--- From_Alharith/OldThreeOrFive.java
+++ From_Alharith/OldThreeOrFive.java
@@ -1,6 +1,6 @@
-public class Old35 {
+public class OldThreeOrFive  {
 
-    public boolean old35(int n) {
+    public boolean isDivisibleByThreeOrFiveOnly(int n) {
         if (n%5==0 && n%3==0) return false;
         if (n%5==0 || n%3==0) return true;
         return false;
