1e095507d1cf94eff7a5bcb69ea6b3193d38943e
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 09:22:37 +0400

    Update and rename Array123.java to ArrayAnalyzer.java

--- From_Alharith/ArrayAnalyzer.java
+++ From_Alharith/ArrayAnalyzer.java
@@ -1,6 +1,6 @@
-public class Array123 {
+public class ArrayAnalyzer  {
 
-    public boolean array123(int[] nums) {
+    public boolean containsSequenceOneTwoThree(int[] nums) {
         for (int i = 0; i < nums.length - 2; i++) {
             if (nums[i] == 1 && nums[i + 1] == 2 && nums[i + 2] == 3) return true;
         }
