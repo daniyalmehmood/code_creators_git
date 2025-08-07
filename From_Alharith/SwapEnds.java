fb79d7e0766d99f08e3a9806fccfb864b5246293
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 11:44:50 +0400

    Update SwapEnds.java

--- From_Alharith/SwapEnds.java
+++ From_Alharith/SwapEnds.java
@@ -1,6 +1,6 @@
 public class SwapEnds {
 
-    public int[] swapEnds(int[] nums) {
+    public int[] swapFirstAndLast(int[] nums) {
         int first=nums[0];
         nums[0]= nums[nums.length-1];
         nums[nums.length-1] = first;
