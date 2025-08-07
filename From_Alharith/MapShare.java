c2d1c0e6d9f778a536a573fb0c7a34d36600aa2d
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 14:52:38 +0400

    Update MapShare.java

--- From_Alharith/MapShare.java
+++ From_Alharith/MapShare.java
@@ -1,6 +1,6 @@
 public class MapShare {
 
-    public Map<String, String> mapShare(Map<String, String> map) {
+    public Map<String, String> cleanAndCopyValues(Map<String, String> map) {
         map.remove("c");
         if (map.containsKey("a")){
             map.put("b",map.get("a"));
