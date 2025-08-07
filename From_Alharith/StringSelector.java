eac26ef6e3306834c8fe6849a5ce8ba9ef8327d2
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 09:51:06 +0400

    Update and rename TheEnd.java to StringSelector.java

--- From_Alharith/StringSelector.java
+++ From_Alharith/StringSelector.java
@@ -1,6 +1,6 @@
-public class TheEnd {
+public class StringSelector  {
 
-    public String theEnd(String str, boolean front) {
+    public String getEndChar(String str, boolean front) {
         if (front) return str.substring(0,1);
         return str.substring(str.length()-1);
     }
