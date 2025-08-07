eb6a40b034ffa83b50482277c0086731865feed0
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 14:53:15 +0400

    Update MapBully.java

--- From_Alharith/MapBully.java
+++ From_Alharith/MapBully.java
@@ -1,6 +1,6 @@
 public class MapBully {
 
-    public Map<String, String> mapBully(Map<String, String> map) {
+    public Map<String, String> moveAValueToB(Map<String, String> map) {
         if (map.containsKey ("a")){
             map.put("b",map.get("a"));
             map.put("a","");
