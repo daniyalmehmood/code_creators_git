33ac94b4d689c62b66052085f23115fceeae9365
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 11:36:34 +0400

    Update and rename Double23.java to DoubleTwoOrThree.java

--- From_Alharith/DoubleTwoOrThree.java
+++ From_Alharith/DoubleTwoOrThree.java
@@ -1,6 +1,6 @@
-public class Double23 {
+public class DoubleTwoOrThree {
 
-    public boolean double23(int[] nums) {
+    public boolean hasDoubleTwoOrThree(int[] nums) {
         return (nums.length == 2) &&
                 ((nums[0] == 2 && nums[1] == 2) || (nums[0] == 3 && nums[1] == 3));
     }
