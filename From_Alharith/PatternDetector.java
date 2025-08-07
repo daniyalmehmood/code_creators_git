94db30effa2399508e1940a52b30ae5c5ef8fc67
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 09:32:11 +0400

    Update and rename Has271.java to PatternDetector.java

--- From_Alharith/PatternDetector.java
+++ From_Alharith/PatternDetector.java
@@ -1,6 +1,6 @@
-public class Has271 {
+public class PatternDetector  {
 
-    public boolean has271(int[] nums) {
+    public boolean contains271Pattern(int[] nums) {
         for (int i = 0; i < nums.length - 2; i++) {
             int val = nums[i];
             if (nums[i + 1] == val + 5 && Math.abs(nums[i + 2] - (val - 1)) <= 2) {
