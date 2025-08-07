ffc0167ed8e32a4248747b0b648eba36baabdbca
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 08:21:22 +0400

    Update and rename In1020.java to RangeChecker.java

--- From_Alharith/RangeChecker.java
+++ From_Alharith/RangeChecker.java
@@ -1,6 +1,6 @@
-public class In1020 {
+public class RangeChecker  {
 
-    public boolean in1020(int a, int b) {
+    public boolean isInRange10To20(int a, int b) {
         if ((a>=10 && a<=20) || (b>=10 && b<=20)) return true;
         return false;
     }
