217abda90419a57b5f9a782dcf9422d1d02d3e40
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 11:35:55 +0400

    Update MakeLast.java

--- From_Alharith/MakeLast.java
+++ From_Alharith/MakeLast.java
@@ -1,6 +1,6 @@
 public class MakeLast {
 
-    public int[] makeLast(int[] nums) {
+    public int[] makeLastWithLastValue(int[] nums) {
         int[] DoubleLength= new int[nums.length*2];
         DoubleLength[DoubleLength.length-1]=nums[nums.length-1];
         return DoubleLength;
