3f9d982a4243b6b0edf2b01b61aed3910931f80d
Huria <alforihuria938@gmail.com>
2025-08-07 08:55:36 +0400

    modifing code logic

--- from_Huria/Mid2Picker.java
+++ from_Huria/Mid2Picker.java
@@ -1,22 +1,21 @@
-public class Mid2Picker {
-    public static String mid2Picker(String str) {
+public class MidTwoPicker {
+    public static String midTwoPicker(String str) {
         if (str.length() <= 1) {
             return "";
         }
-        int len = str.length() / 2;
-        String middTwo = str.substring(len - 1, len + 1);
+        int midLengthValue = str.length() / 2;
 
-        return middTwo;
+        return str.substring(midLengthValue - 1, midLengthValue + 1);
     }
 
     public static void main(String[] args) {
-        System.out.println(mid2Picker("string"));
-        System.out.println(mid2Picker("code"));
-        System.out.println(mid2Picker("Practice"));
-        System.out.println(mid2Picker("ab"));
-        System.out.println(mid2Picker("0123456789"));
-        System.out.println(mid2Picker("a"));
-        System.out.println(mid2Picker(""));
+        System.out.println(midTwoPicker("string"));
+        System.out.println(midTwoPicker("code"));
+        System.out.println(midTwoPicker("Practice"));
+        System.out.println(midTwoPicker("ab"));
+        System.out.println(midTwoPicker("0123456789"));
+        System.out.println(midTwoPicker("a"));
+        System.out.println(midTwoPicker(""));
     }
 }
 
