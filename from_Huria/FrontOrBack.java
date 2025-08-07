5e3f1e1dac2b3a8eb73199b85c83a489716553a8
Huria <alforihuria938@gmail.com>
2025-08-07 09:01:31 +0400

    modifing code logic

--- from_Huria/FrontOrBack.java
+++ from_Huria/FrontOrBack.java
@@ -1,12 +1,10 @@
 public class FrontOrBack {
     public static String frontOrBack(String str, boolean front) {
-
-        String newStr = "";
-        if (front == true) {
-            return newStr = str.substring(0, 1);
+        if (front) {
+            return str.substring(0, 1);
         }
 
-        return newStr = str.substring(str.length() - 1, str.length());
+        return str.substring(str.length() - 1);
     }
 
     public static void main(String[] args) {
@@ -16,7 +14,6 @@ public class FrontOrBack {
         System.out.println(frontOrBack("oh", true));
         System.out.println(frontOrBack("oh", false));
         System.out.println(frontOrBack("java", true));
-        System.out.println(frontOrBack("code", false));
         System.out.println(frontOrBack("code", false));
     }
 }
