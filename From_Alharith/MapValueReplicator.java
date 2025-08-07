6b2e9679a0b4998bf53c137afe8388c2de02ba3c
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-07 14:43:14 +0400

    Update and rename MapAB3.java to MapValueReplicator.java

--- From_Alharith/MapValueReplicator.java
+++ From_Alharith/MapValueReplicator.java
@@ -1,6 +1,6 @@
-public class MapAB3 {
+public class MapValueReplicator  {
 
-    public Map<String, String> mapAB3(Map<String, String> map) {
+    public Map<String, String> replicateMissingABKeys(Map<String, String> map) {
         if (!map.containsKey("a")&& map.containsKey("b")) map.put ("a", map.get("b") );
         if (!map.containsKey("b")&& map.containsKey("a")) map.put ("b", map.get("a") );
         return map;
