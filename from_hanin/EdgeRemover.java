a5c15ed86c2a621b89809e115783efdfaea74189
Hanin <haninzx2024@gmail.com>
2025-08-10 09:40:00 +0400

    modify the task

--- from_hanin/EdgeRemover.java
+++ from_hanin/EdgeRemover.java
@@ -1,8 +1,12 @@
-public class WithoutEndTwo {
+public class EdgeRemover {
     public static String withoutEnd(String str) {
+        // Check if the string has at least 2 characters
         if (str.length() >= 2) {
+            // Return without first and last characters
             return str.substring(1, str.length() - 1);
         }
+        // return the string
         return str;
     }
 }
+
