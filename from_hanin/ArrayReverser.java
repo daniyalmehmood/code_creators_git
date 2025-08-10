1231256b5abbd32cba8a473d65509ded0e23a03b
Hanin <haninzx2024@gmail.com>
2025-08-10 15:58:46 +0400

    reverse3.java

---
+++ from_hanin/ArrayReverser.java
@@ -0,0 +1,7 @@
+// This class provides a method to reverse a 3-element array
+public class ArrayReverser {
+    public int[] reverseThree(int[] nums) {
+        return new int[]{nums[2], nums[1], nums[0]};
+    }
+
+}
