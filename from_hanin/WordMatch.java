8b2aaf1a4f1382a3a4a6ed2dd046095873778bc8
Hanin <haninzx2024@gmail.com>
2025-08-10 10:50:52 +0400

    modify the task

--- from_hanin/WordMatch.java
+++ from_hanin/WordMatch.java
@@ -1,9 +1,9 @@
-public class StringMatch {
+public class WordMatch {
     public static int countMatchingSubstrings(String a, String b) {
         // Find the smaller length between the two strings
         int str = Math.min(a.length(), b.length());
         int count = 0;
-        // Loop through each string 
+        // Loop through each string
         for (int i = 0; i < str - 1; i++) {
             String firstSub = a.substring(i, i + 2);
             String secondSub = b.substring(i, i + 2);
