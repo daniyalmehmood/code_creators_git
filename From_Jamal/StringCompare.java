53939720f3ff8a3c850179ba2997ed082e36bae4
Jamal <Jamalzeidi@gmail.com>
2025-08-10 09:10:37 +0400

    Added StringCompare.java

---
+++ From_Jamal/StringCompare.java
@@ -0,0 +1,32 @@
+public class StringCompare {
+
+
+        public static String notString(String str) {
+
+
+
+            if (str.length() >= 3 && str.substring(0,3).equals("not")){
+
+                return str;
+
+
+
+            }
+
+            return "not " + str;
+
+
+
+
+        }
+
+    public static void main(String[] args) {
+        System.out.println(notString("bad"));
+    }
+
+
+
+
+    }
+
+
