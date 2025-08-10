749b19b949b1deb00cb3e3af5a49039bf312d48b
Mulk <mulkbadar774@gmail.com>
2025-08-10 08:24:15 +0400

    Solution of TeenSum task

---
+++ From_Mulk/TeenSum.java
@@ -0,0 +1,8 @@
+public class TeenSum {
+    public int sumWithTeenCheck(int a, int b) {
+        if (a >= 13 && a <= 19 || b >= 13 && b <= 19) {
+            return 19;
+        }
+        return a + b;
+    }
+}
