76d9fb2b0eac204b102a17f3adb5111d844ea2a4
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-10 08:28:25 +0400

    Update RoundSum.java

--- From_Alharith/RoundSum.java
+++ From_Alharith/RoundSum.java
@@ -1,17 +1,17 @@
 public class RoundSum {
 
-    public int roundSum(int a, int b, int c) {
+    public int calculateRoundedSum(int a, int b, int c) {
 
-        a=round10(a);
-        b=round10(b);
-        c=round10(c);
+        a=roundToNearest10(a);
+        b=roundToNearest10(b);
+        c=roundToNearest10(c);
 
         return a+b+c;
 
 
     }
 
-    public int round10(int num){
+    public int roundToNearest10(int num){
 
         int round= num%10;
         if (round>=5)  return num+(10-round);
