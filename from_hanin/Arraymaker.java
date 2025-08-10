5c65cee0a98cef557e88be657913049b77ab1b34
Hanin <haninzx2024@gmail.com>
2025-08-10 15:27:50 +0400

    make pi.java

---
+++ from_hanin/Arraymaker.java
@@ -0,0 +1,8 @@
+public class Arraymaker {
+    public int[] newArray() {
+// This class provides a method that returns the first three digits of Pi in an integer array
+        return new int[] {3, 1, 4};
+    }
+
+}
+
