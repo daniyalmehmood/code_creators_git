7874f7183ee8c571802f35d4b79b76c595699cd5
Hanin <haninzx2024@gmail.com>
2025-08-10 09:59:53 +0400

    modify the task

--- from_hanin/XXCounter.java
+++ from_hanin/XXCounter.java
@@ -1,7 +1,6 @@
 public class XXCounter {
     public static int countDoubleX(String str) {
         int count = 0;
-
         // Loop through the string, stopping at second-to-last character
         for (int i = 0; i < str.length() - 1; i++) {
             // Check if the current and next characters form "xx"
@@ -9,7 +8,6 @@ public class XXCounter {
                 count++;
             }
         }
-
         // Return the total count of "xx" found
         return count;
     }
