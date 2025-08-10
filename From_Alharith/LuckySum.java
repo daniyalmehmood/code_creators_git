03ebfd80f8abc354c5377c0524cd864b7ab409ac
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-10 08:18:54 +0400

    Update LuckySum.java

--- From_Alharith/LuckySum.java
+++ From_Alharith/LuckySum.java
@@ -1,6 +1,6 @@
 public class LuckySum {
 
-    public int luckySum(int a, int b, int c) {
+    public int calculateLuckySum(int a, int b, int c) {
 
         if (a==13) return 0;
         if (b==13) return a;
