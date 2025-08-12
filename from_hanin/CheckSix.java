0fdc6017fd2bd340254870bb065b243866f2dd9d
Hanin <haninzx2024@gmail.com>
2025-08-12 09:25:22 +0400

    Add love6.java

---
+++ from_hanin/CheckSix.java
@@ -0,0 +1,9 @@
+public class CheckSix {
+    public boolean ifItSix(int a, int b) {
+        if (a == 6 || b == 6 || a + b == 6 || Math.abs(a - b) == 6) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
