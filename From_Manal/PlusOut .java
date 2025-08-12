843b82139778df89bf121ba6beb26ee3db48ad85
Manal-Alhatmi <manalalhatmi99@outlook.com>
2025-08-12 14:33:01 +0400

    Added PlusOut .java

---
+++ From_Manal/PlusOut .java
@@ -0,0 +1,23 @@
+public class PlusOut {
+    public static String plusOut(String str, String word) {
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+        while (i < str.length()) {
+            // Check if the word appears at the current position
+            if (i <= str.length() - word.length() && str.substring(i, i + word.length()).equals(word)) {
+                result.append(word);
+                i += word.length();  
+            } else {
+                result.append("+");
+                i++;
+            }
+        }
+        return result.toString();
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(plusOut("12xy34", "xy"));         
+        System.out.println(plusOut("12xy34", "1"));          
+        System.out.println(plusOut("12xy34xyabcxy", "xy"));  
+    }
+}
