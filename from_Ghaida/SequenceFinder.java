ebde003e259801207f9c36c9a4dbc9c558e5652b
Ghaida02a <ggmskaj@gmail.com>
2025-08-13 12:19:57 +0400

    has271 changed class name & function name

--- from_Ghaida/SequenceFinder.java
+++ from_Ghaida/SequenceFinder.java
@@ -1,10 +1,10 @@
-public class Has271 {
+public class SequenceFinder {
     public static void main(String[] args) {
-        System.out.println(has271(new int[] {1, 2, 7, 1})); // → true
-        System.out.println(has271(new int[] {1, 2, 8, 1})); // → false
-        System.out.println(has271(new int[] {2, 7, 1})); // → true
+        System.out.println(checkSequence(new int[] {1, 2, 7, 1})); // → true
+        System.out.println(checkSequence(new int[] {1, 2, 8, 1})); // → false
+        System.out.println(checkSequence(new int[] {2, 7, 1})); // → true
     }
-    public static boolean has271(int[] nums) {
+    public static boolean checkSequence(int[] nums) {
         for (int i = 0; i < nums.length - 2; i++) {
             if (nums[i + 1] == nums[i] + 5) {
                 int num1 = nums[i] - 1;
