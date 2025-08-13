ded0ce17470fa7074033fa996a3de3325855d057
Ghaida02a <ggmskaj@gmail.com>
2025-08-13 12:08:30 +0400

    #TRA-2857: Array-1. firstLast6 added

--- from_Ghaida/FirstLastElementsChecker.java
+++ from_Ghaida/FirstLastElementsChecker.java
@@ -1,4 +1,4 @@
-public class firstLastElementsChecker {
+public class FirstLastElementsChecker {
     public static void main(String[] args) {
         System.out.println(isFirstLastEqualSix(new int[]{1, 2, 6}));// → true
         System.out.println(isFirstLastEqualSix(new int[]{6, 1, 2, 3}));// → true
