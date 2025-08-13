d8347eb06f9e42760bdbab54321624434a63b72d
Hawraa <hawra8516@gmail.com>
2025-08-13 15:47:35 +0400

    Logic-1: nearTen added

---
+++ from_Hawraa/NearTen.java
@@ -0,0 +1,11 @@
+public class NearTen {
+    public static boolean isNearMultipleOfTen(int num) {
+        return num % 10 <= 2 || num % 10 >= 8;
+    }
+
+    public static void main(String[] args){
+        System.out.println(isNearMultipleOfTen(12));
+        System.out.println(isNearMultipleOfTen(17));
+        System.out.println(isNearMultipleOfTen(19));
+    }
+}
