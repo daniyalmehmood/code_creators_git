dc8cf10f014fac7ec495dd82e3383c83bcb8bf6b
Hawraa <hawra8516@gmail.com>
2025-08-13 14:50:02 +0400

    Logic-1: specialEleven modified

--- from_Hawraa/SpecialEleven.java
+++ from_Hawraa/SpecialEleven.java
@@ -1,11 +1,11 @@
 public class SpecialEleven {
-    public static boolean isSpecialNumber (int n) {
+    public static boolean checkElevenCondition (int n) {
         return n % 11 == 0 || n % 11 == 1;
     }
 
     public static void main(String[] args){
-        System.out.println(isSpecialNumber(22));
-        System.out.println(isSpecialNumber(23));
-        System.out.println(isSpecialNumber(24));
+        System.out.println(checkElevenCondition(22));
+        System.out.println(checkElevenCondition(23));
+        System.out.println(checkElevenCondition(24));
     }
 }
