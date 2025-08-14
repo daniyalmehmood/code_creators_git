8d8517a25dbc06cab5fb93e97be504f3ce089009
Hawraa <hawra8516@gmail.com>
2025-08-14 11:09:08 +0400

    Logic-1: answerCell added

---
+++ from_Hawraa/AnswerCell.java
@@ -0,0 +1,11 @@
+public class AnswerCell {
+    public static boolean shouldAnswerCall(boolean isMorning, boolean isMom, boolean isAsleep) {
+        return !isAsleep && (!isMorning || isMom);
+    }
+
+    public static void main(String[] args){
+        System.out.println(shouldAnswerCall(false, false, false));
+        System.out.println(shouldAnswerCall(false, false, true));
+        System.out.println(shouldAnswerCall(true, false, false));
+    }
+}
