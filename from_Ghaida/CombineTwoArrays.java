da10efcf9104fe79943f8d8b311c3ddc5f037735
Ghaida02a <ggmskaj@gmail.com>
2025-08-14 14:13:44 +0400

    #TRA-2876: Array-1. plusTwo modified

--- from_Ghaida/CombineTwoArrays.java
+++ from_Ghaida/CombineTwoArrays.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
+
 public class CombineTwoArrays {
     public static void main(String[] args) {
-        System.out.println(addTwoArrays([1, 2], [3, 4]));// → [1, 2, 3, 4]
-        System.out.println(addTwoArrays([4, 4], [2, 2]));// → [4, 4, 2, 2]
-        System.out.println(addTwoArrays([9, 2], [3, 4]));// → [9, 2, 3, 4]);
+        System.out.println(Arrays.toString(addTwoArrays(new int[]{1, 2}, new int[]{3, 4})));// → [1, 2, 3, 4]
+        System.out.println(Arrays.toString(addTwoArrays(new int[]{4, 4}, new int[]{2, 2})));// → [4, 4, 2, 2]
+        System.out.println(Arrays.toString(addTwoArrays(new int[]{9, 2}, new int[]{3, 4})));// → [9, 2, 3, 4]);
     }
     public static int[] addTwoArrays(int[] a, int[] b) {
         return new int[]{a[0], a[1], b[0], b[1]};
