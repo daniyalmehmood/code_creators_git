7b1d98fbd5c456fbdd7db1907455396d39225994
Hawraa <hawra8516@gmail.com>
2025-08-14 14:19:44 +0400

    Logic-1: redTicket added

---
+++ from_Hawraa/RedTicket.java
@@ -0,0 +1,18 @@
+public class RedTicket {
+    public static int getRedTicketResult (int a, int b, int c) {
+        if (a == 2 && b == 2 && c == 2) {
+            return 10;
+        } else if (a == b && b == c) {
+            return 5;
+        } else if (a != b && a != c) {
+            return 1;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getRedTicketResult(2, 2, 2));
+        System.out.println(getRedTicketResult(2, 2, 1));
+        System.out.println(getRedTicketResult(0, 0, 0));
+    }
+}
