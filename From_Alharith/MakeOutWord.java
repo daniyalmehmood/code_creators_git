1d65bd38c85354ee497a5762f1c236f889429559
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-14 15:58:11 +0400

    Update and rename StringFormatter.java to MakeOutWord.java

--- From_Alharith/MakeOutWord.java
+++ From_Alharith/MakeOutWord.java
@@ -1,4 +1,4 @@
-public class StringFormatter {
+public class MakeOutWord {
 
     public String insertWordIntoWrapper(String out, String word) {
         return out.substring(0,2)+word+out.substring(2,4);
