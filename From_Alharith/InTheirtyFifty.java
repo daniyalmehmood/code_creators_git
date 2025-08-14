faebe834a48c09ded9f648e84f3356686b5e0d69
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-14 15:44:40 +0400

    Update and rename RangeChecker.java to InTheirtyFifty.java

--- From_Alharith/InTheirtyFifty.java
+++ From_Alharith/InTheirtyFifty.java
@@ -1,4 +1,4 @@
-public class RangeChecker  {
+public class InTheirtyFifty  {
 
     public boolean areBothInSameRange30to40or40to50(int a, int b) {
         if ((a >= 30 && a <= 40 && b >= 30 && b <= 40) || (a >= 40 && a <= 50 && b >= 40 && b <= 50)) {
