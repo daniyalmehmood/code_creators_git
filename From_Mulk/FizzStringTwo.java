c997549ed7197bf347dc0448463cb1aa5de67d59
Mulk <mulkbadar774@gmail.com>
2025-08-17 09:31:42 +0400

    solution of FizzStringTwo task

---
+++ From_Mulk/FizzStringTwo.java
@@ -0,0 +1,14 @@
+public class FizzStringTwo {
+    public String generateFizzBuzz(int n) {
+        if (n % 3 == 0 && n % 5 == 0) {
+            return "FizzBuzz" + "!";
+        }
+        if ((n % 3) == 0) {
+            return "Fizz" + "!";
+        }
+        if ((n % 5) == 0) {
+            return "Buzz" + "!";
+        }
+        return n + "!";
+    }
+}
