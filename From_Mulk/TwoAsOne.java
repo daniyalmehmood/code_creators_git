e80ff788d38dc8a7704d92c628ec43185c5efe0a
Mulk <mulkbadar774@gmail.com>
2025-08-17 10:09:28 +0400

    solution of TwoAsOne task

---
+++ From_Mulk/TwoAsOne.java
@@ -0,0 +1,14 @@
+public class TwoAsOne {
+    public boolean addTwoGetOne(int a, int b, int c) {
+        if (a + b == c) {
+            return true;
+        }
+        if (b + c == a) {
+            return true;
+        }
+        if (a + c == b) {
+            return true;
+        }
+        return false;
+    }
+}
