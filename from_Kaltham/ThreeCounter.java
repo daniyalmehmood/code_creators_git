3b23de5586b72395d01e390624def6b94ebfe98a
Kaltham <kaltham.aldifai@gmail.com>
2025-08-17 08:30:53 +0400

    This is the solution for haveThree

--- from_Kaltham/ThreeCounter.java
+++ from_Kaltham/ThreeCounter.java
@@ -4,7 +4,9 @@ public class ThreeCounter {
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 3) {
                 threeCount++;
-                i++;
+                if (i + 1 < nums.length && nums[i + 1] == 3) {
+                    return false;
+                }
             }
         }
         return threeCount == 3;
