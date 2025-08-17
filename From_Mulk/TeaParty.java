3cc99fb4d0a9ecbcb7322a7881517cd54daf89db
Mulk <mulkbadar774@gmail.com>
2025-08-17 08:58:59 +0400

    solution of TeaParty task

---
+++ From_Mulk/TeaParty.java
@@ -0,0 +1,12 @@
+public class TeaParty {
+    public int evaluateTeaParty(int tea, int candy) {
+        if (tea < 5 || candy < 5) {
+            return 0;
+        }
+        if ((tea >= 2 * candy) || (candy >= 2 * tea)) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+}
