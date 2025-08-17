affb59ef134a41ff8a9cd403511a92cd0ace60fc
Maha <mahathamood98@gmail.com>
2025-08-17 11:23:12 +0400

    Adding luckySum file

---
+++ From_Maha/SumCalculator.java
@@ -0,0 +1,15 @@
+public class SumCalculator {
+    // Returns the sum of three integers, but if any value is 13, ignore it and all values to its right
+    public int sumIgnoringThirteen(int a, int b, int c) {
+        if (a == 13) {
+            return 0;
+        }
+        if (b == 13) {
+            return a;
+        }
+        if (c == 13) {
+            return a + b;
+        }
+        return a + b + c;
+    }
+}
