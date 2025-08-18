e7a2124dc6d9cb92f1807577f4c26c1b624844f2
Shatha Almamari <shatha.almamari99@gmail.com>
2025-08-18 10:48:10 +0400

    dateFashion task added

---
+++ from_shatha/DateFation.java
@@ -0,0 +1,18 @@
+public class DateFation {
+    public static int getTableByFashion(int you, int date) {
+        if (you <= 2 || date <= 2) {
+            return 0;}
+         else if (you >= 8 || date >= 8) {
+            return 2;
+        } else{
+                return 1;
+            }
+        }
+
+        public static void main (String[]args){
+            System.out.println(getTableByFashion(5, 10));
+            System.out.println(getTableByFashion(5, 2));
+            System.out.println(getTableByFashion(5, 5));
+        }
+    }
+
