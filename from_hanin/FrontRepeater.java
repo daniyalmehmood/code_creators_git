5516eb411b7df565f4d50b673f1f1db6d2272f9f
Hanin <haninzx2024@gmail.com>
2025-08-18 10:05:26 +0400

    Add task.java

---
+++ from_hanin/FrontRepeater.java
@@ -0,0 +1,14 @@
+public class FrontRepeater {
+    public String repeatFrontSegment(String str, int n) {
+        StringBuilder results = new StringBuilder();
+
+        for (int i = 0; i <n; i++) {
+            String startWord = str.substring(0,n-i);
+            results.append(startWord);
+
+        }
+
+        return results.toString();
+    }
+
+}
