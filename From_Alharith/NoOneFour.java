9fd443fd56f65317b2c5d95f44390bf8a0022471
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-18 11:05:14 +0400

    #TRA-3105: Array2_ No14 Done

---
+++ From_Alharith/NoOneFour.java
@@ -0,0 +1,18 @@
+public class NoOneFour {
+
+    public boolean notBothOneAndFour(int[] nums) {
+
+        boolean has1= false;
+        boolean has4= false;
+
+        for (int i=0; i<nums.length; i++){
+
+            if (nums[i]==1) has1=true;
+            if (nums[i]==4) has4=true;
+
+        }
+        return !(has1&&has4);
+
+    }
+
+}
