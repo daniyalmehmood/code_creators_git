de27efc879c228e04c16b6a7d84c4daf0c2268a1
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-18 09:11:21 +0400

    #TRA-3080: Array2_ MoreOneFour Done

---
+++ From_Alharith/MoreOneFour.java
@@ -0,0 +1,15 @@
+public class MoreOneFour {
+
+    public boolean hasMoreOnesThanFours(int[] nums) {
+        int count1 = 0;
+        int count4 = 0;
+
+        for (int num : nums) {
+            if (num == 1) count1++;
+            if (num == 4) count4++;
+        }
+
+        return count1 > count4;
+    }
+
+}
