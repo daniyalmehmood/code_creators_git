dbfe0185935292470238bdc2214b6793d95f5a6a
haithemhamid <haithemalhinai2017@gmail.com>
2025-08-18 11:11:48 +0400

    add helper function for roundSum function

--- from_Haitham/RoundSumNumber.java
+++ from_Haitham/RoundSumNumber.java
@@ -5,4 +5,8 @@ public int roundSum(int a, int b, int c) {
         c=round10(c);
         return a+b+c;
     }
+	public int round10(int num){
+        if(num%10<5)return num-num%10;
+        return num+(10-num%10);
+    }
 	}
