abbae390e7d53ac7b10380a7c54ed489368b09f7
Hawraa <hawra8516@gmail.com>
2025-08-18 10:37:41 +0400

    String-3: countTriple added

---
+++ from_Hawraa/CountTriple.java
@@ -0,0 +1,17 @@
+public class CountTriple {
+    public static int countTripleChar(String str) {
+        int countTriple = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == str.charAt(i + 1) && str.charAt(i) == str.charAt(i + 2)) {
+                countTriple++;
+            }
+        }
+        return countTriple;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countTripleChar("abcXXXabc"));
+        System.out.println(countTripleChar("xxxabyyyycd"));
+        System.out.println(countTripleChar("a"));
+    }
+}
