2a7d4e7acddd0c90a0202ed9b30e32565af80c38
Hawraa <hawra8516@gmail.com>
2025-08-19 15:33:07 +0400

    Array-2: sum28 added

---
+++ from_Hawraa/TwosSumToEight.java
@@ -0,0 +1,18 @@
+// Array-2 > sum28
+public class TwosSumToEight {
+    public static boolean sumOfTwosIsEight(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 2) {
+                sum += nums[i];
+            }
+        }
+        return sum == 8;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumOfTwosIsEight(new int[]{2, 3, 2, 2, 4, 2}));
+        System.out.println(sumOfTwosIsEight(new int[]{2, 3, 2, 2, 4, 2, 2}));
+        System.out.println(sumOfTwosIsEight(new int[]{1, 2, 3, 4}));
+    }
+}
