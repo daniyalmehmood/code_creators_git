ccf9b1ab95e29f665b95f78ed8bec6738854bb45
Shatha Almamari <shatha.almamari99@gmail.com>
2025-08-20 08:49:49 +0400

    withoutDouble task added

---
+++ from_shatha/WithoutDoubles.java
@@ -0,0 +1,18 @@
+public class WithoutDoubles {
+    public static int sumWithoutDoubles(int die1, int die2, boolean noDoubles) {
+        if (noDoubles && die1 == die2) {
+            if (die1 == 6) {
+                die1 = 1;
+            } else {
+                die1 = die1 + 1;
+            }
+        }
+        return die1 + die2;
+    }
+        public static void main (String[] args){
+            System.out.println(sumWithoutDoubles(2, 3, true));
+            System.out.println(sumWithoutDoubles(3, 3, true));
+            System.out.println(sumWithoutDoubles(3, 3, false));
+        }
+
+    }
