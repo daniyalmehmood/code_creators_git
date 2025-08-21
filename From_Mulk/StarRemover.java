fda98ec88b8b3c55304681fa374abef5f69cfb61
Mulk <mulkbadar774@gmail.com>
2025-08-21 09:07:34 +0400

    Solution of StarOut task

---
+++ From_Mulk/StarRemover.java
@@ -0,0 +1,17 @@
+public class StarRemover {
+    public String removeStarsAndAdjacentChars(String str) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '*') continue;
+            if (i > 0 && str.charAt(i - 1) == '*') continue;
+            if (i < str.length() - 1 && str.charAt(i + 1) == '*') continue;
+
+            sb.append(str.charAt(i));
+        }
+
+        return sb.toString();
+    }
+}
+
+
