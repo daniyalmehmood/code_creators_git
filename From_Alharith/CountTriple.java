d26ddf2049694460336199664fa39f866fa6798b
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-21 11:50:15 +0400

    #TRA-3430: String3_ CountTriple Done

---
+++ From_Alharith/CountTriple.java
@@ -0,0 +1,13 @@
+public class CountTriple {
+
+    public int countTripleChars(String str) {
+
+        int count=0;
+
+        for (int i=0; i<str.length()-2;i++){
+
+            if (str.charAt(i)==str.charAt(i+1) && (str.charAt(i+2)==str.charAt(i))) count++;
+        }
+        return count;
+    }
+}
