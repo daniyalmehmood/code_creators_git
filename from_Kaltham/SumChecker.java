4010a0682e8ac3c717489b9d8c34775a2b59ef8d
Kaltham <kaltham.aldifai@gmail.com>
2025-08-21 08:37:00 +0400

    The solution for twoAsOne Task

---
+++ from_Kaltham/SumChecker.java
@@ -0,0 +1,16 @@
+//Logic-1 > twoAsOne
+
+public class SumChecker {
+    public boolean canTwoSumToThird(int a, int b, int c) {
+        if (a + b == c) {
+            return true;
+        }
+        if (b + c == a) {
+            return true;
+        }
+        if (a + c == b) {
+            return true;
+        }
+        return false;
+    }
+}
