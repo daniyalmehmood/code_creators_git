09ea4c83761ffba2d7fc431199d2acb9d9aeae1b
Muiayed <creeew89@gmail.com>
2025-08-22 23:38:10 +0400

    task is done'

---
+++ from_Muiayed/RoundSum.java
@@ -0,0 +1,14 @@
+public class RoundSum {
+    public int roundSum(int a, int b, int c) {
+        return round10(a) + round10(b) + round10(c);
+    }
+
+    public int round10(int num) {
+        int remainder = num % 10;
+        if (remainder >= 5) {
+            return num + (10 - remainder);
+        } else {
+            return num - remainder;
+        }
+    }
+}
