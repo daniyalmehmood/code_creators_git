5294e991a474658e22466ef607c160417391768e
Mulk <mulkbadar774@gmail.com>
2025-08-24 11:44:27 +0400

    Solution of sum28 task

---
+++ From_Mulk/SumTwo.java
@@ -0,0 +1,16 @@
+public class SumTwo {
+    public boolean checkSumOfTwosEqualsEight(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 2) {
+                sum += 2;
+
+            }
+        }
+
+        if (sum == 8) {
+            return true;
+        }
+        return false;
+    }
+}
