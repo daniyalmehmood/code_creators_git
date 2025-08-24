9fb5b50f74ba70eac74e42b47184d4e3ec5f5bc5
Hawraa <hawra8516@gmail.com>
2025-08-24 14:21:59 +0400

    Array-2: pre4 modified

---
+++ from_Hawraa/ArrayBeforeFour.java
@@ -0,0 +1,14 @@
+// Array-2 > pre4
+public class ArrayBeforeFour {
+    public int[] getPreFourElements(int[] nums) {
+        int count = 0;
+        while (count < nums.length && nums[count] != 4) {
+            count++;
+        }
+        int[] result = new int[count];
+        for (int i = 0; i < count; i++) {
+            result[i] = nums[i];
+        }
+        return result;
+    }
+}
