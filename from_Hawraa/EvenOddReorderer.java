655a31f71a85bdf91919c6fc39b01588e2ff09c4
Hawraa <hawra8516@gmail.com>
2025-08-24 15:30:47 +0400

    Array-2: evenOdd added

---
+++ from_Hawraa/EvenOddReorderer.java
@@ -0,0 +1,19 @@
+// Array-2 > evenOdd
+public class EvenOddReorderer {
+    public int[] separateEvenOdd(int[] nums) {
+        int evenIndex = 0;
+        int oddIndex = nums.length - 1;
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                result[evenIndex] = nums[i];
+                evenIndex++;
+            } else {
+                result[oddIndex] = nums[i];
+                oddIndex--;
+            }
+        }
+        return result;
+    }
+
+}
