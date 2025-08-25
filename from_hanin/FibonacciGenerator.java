7346e6c42b43bc12768f15e74845a6dd38b3a985
Hanin <haninzx2024@gmail.com>
2025-08-25 14:31:46 +0400

    Add task.java

---
+++ from_hanin/FibonacciGenerator.java
@@ -0,0 +1,11 @@
+//Recursion-1 > fibonacci
+public class FibonacciGenerator {
+    public int fibonacci(int n) {
+        if (n < 2) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
+}
