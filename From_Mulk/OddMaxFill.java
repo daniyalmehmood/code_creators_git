9450ccf026348cfdb1a9cd362616b6637135c0cb
Mulk <mulkbadar774@gmail.com>
2025-08-25 14:32:49 +0400

    Solution of zeroMax task

---
+++ From_Mulk/OddMaxFill.java
@@ -0,0 +1,18 @@
+// Array-2 > zeroMax
+public class OddMaxFill {
+    public int[] replaceZeroWithMaxOddRight(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == 0) {
+                int max = 0;
+                for (int j = i; j < nums.length; j++) {
+                    if (nums[j] % 2 != 0) {
+                        max = Math.max(nums[j], max);
+                    }
+                }
+                nums[i] = max;
+            }
+        }
+        return nums;
+    }
+
+}
