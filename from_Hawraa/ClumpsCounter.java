102aeb7dccab8989ef45f570d72a024e71c92a55
Hawraa <hawra8516@gmail.com>
2025-08-25 14:24:56 +0400

    Array-3: countClumps added

---
+++ from_Hawraa/ClumpsCounter.java
@@ -0,0 +1,18 @@
+// Array-3 > countClumps
+public class ClumpsCounter {
+    public int getClumpsCount(int[] nums) {
+        int count = 0;
+        boolean inClump = false;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                if (!inClump) {
+                    count++;
+                    inClump = true;
+                }
+            } else {
+                inClump = false;
+            }
+        }
+        return count;
+    }
+}
