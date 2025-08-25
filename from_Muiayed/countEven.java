da90d7c65393d299df52bce93cb11c9808b9a89d
Muiayed <creeew89@gmail.com>
2025-08-25 07:39:52 +0400

    Task is done

---
+++ from_Muiayed/countEven.java
@@ -0,0 +1,15 @@
+public class countEven{
+    public static int countEvens(int[] nums) {
+        int count = 0;
+        for (int n : nums) {
+            if (n % 2 == 0) count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countEvens(new int[]{2, 1, 2, 3, 4}));
+        System.out.println(countEvens(new int[]{2, 2, 0}));
+        System.out.println(countEvens(new int[]{1, 3, 5}));
+    }
+}
