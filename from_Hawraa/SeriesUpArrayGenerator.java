e5e68fba8da768b3d04fa3137d786d27c48c2392
Hawraa <hawra8516@gmail.com>
2025-08-25 12:01:01 +0400

    Array-3: seriesUp added

--- from_Hawraa/SeriesUpArrayGenerator.java
+++ from_Hawraa/SeriesUpArrayGenerator.java
@@ -4,7 +4,7 @@ public class SeriesUpArrayGenerator {
         int arrayLength = n * (n + 1) / 2;
         int[] result = new int[arrayLength];
         int index = 0;
-        for (int i = 1; i <= n; i++) { // numbers of groups
+        for (int i = 1; i <= n; i++) { // numbers of groups.
             for (int j = 1; j <= i; j++) {
                 result[index++] = j;
             }
