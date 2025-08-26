2f321d05c989dcf63f7997a3c0c58d7319818224
Muiayed <creeew89@gmail.com>
2025-08-26 10:15:38 +0400

    #TRA-4401: Map-1 > mapShare

--- from_Muiayed/mapShare.java
+++ from_Muiayed/mapShare.java
@@ -1,3 +1,17 @@
-public class mapShare {
+import java.util.*;
 
+public class MapShare {
+    public static Map<String, String> mapShare(Map<String, String> map) {
+        if (map.containsKey("a")) {
+            map.put("b", map.get("a"));
+        }
+        map.remove("c");
+        return map;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mapShare(new HashMap<>(Map.of("a", "aaa", "b", "bbb", "c", "ccc"))));
+        System.out.println(mapShare(new HashMap<>(Map.of("b", "xyz", "c", "ccc"))));
+        System.out.println(mapShare(new HashMap<>(Map.of("a", "aaa", "c", "meh", "d", "hi"))));
+    }
 }
