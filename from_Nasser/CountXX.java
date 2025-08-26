9458c367d512b00224ab33589fcfd7174558138d
NASSER1221 <alnassri1221@gmail.com>
2025-08-26 09:58:30 +0400

    Update CountXX.java

--- from_Nasser/CountXX.java
+++ from_Nasser/CountXX.java
@@ -1,6 +1,6 @@
 public class CountXX {
 
-    public static int countXX(String str) {
+    public static int CountingXs(String str) {
         int count=0;
         for (int i=0;i<str.length()-1;i++){
             if (str.charAt(i)=='x' &&  str.charAt(i+1)=='x')
@@ -10,8 +10,8 @@ public class CountXX {
    return count; }
 
     public static void main(String[] args) {
-        System.out.println(countXX("abcxx"));
-        System.out.println(countXX("xxx"));
-        System.out.println(countXX("xxxx"));
+        System.out.println(CountingXs("abcxx"));
+        System.out.println(CountingXs("xxx"));
+        System.out.println(CountingXs("xxxx"));
     }
 }
