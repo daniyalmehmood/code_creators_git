4e5280d7aeec2737f006ddefbc1538bf8876827e
Huria <alforihuria938@gmail.com>
2025-08-26 11:53:36 +0400

    Adding  TeensumCal Task

---
+++ from_Huria/TeenSumCalculator.java
@@ -0,0 +1,18 @@
+public class TeenSumCalculator {
+    public static int calculateNoTeenSum(int a, int b, int c) {
+        return fixTeen(a) + fixTeen(b) + fixTeen(c);
+    }
+
+    public static int fixTeen(int n) {
+        if ((n >= 13 && n <= 19) && n != 15 && n != 16) {
+            return 0;
+        }
+        return n;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculateNoTeenSum(1, 2, 3));
+        System.out.println(calculateNoTeenSum(2, 13, 1));
+        System.out.println(calculateNoTeenSum(2, 1, 14));
+    }
+}
