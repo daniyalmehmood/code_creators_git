dd9fd86c650a68a3fcdf522d90026cf1e849ec50
Maha <mahathamood98@gmail.com>
2025-08-26 09:21:54 +0400

    Add String3- sumDigits file

---
+++ From_Maha/DigitSummer.java
@@ -0,0 +1,12 @@
+//String-3 > sumDigits
+public class DigitSummer {
+    public int sumOfDigitsInString(String str) {
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))) {
+                sum += Character.getNumericValue(str.charAt(i));
+            }
+        }
+        return sum;
+    }
+}
