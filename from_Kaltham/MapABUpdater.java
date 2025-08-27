a9e30f56d97795bbea04b99fa4b7398cb68be735
Kaltham <kaltham.aldifai@gmail.com>
2025-08-27 14:26:41 +0400

    The solution for Map-1 >mapAB3 Task

---
+++ from_Kaltham/MapABUpdater.java
@@ -0,0 +1,19 @@
+//Map-1 > mapAB3
+
+
+import java.util.Map;
+
+public class MapABUpdater {
+    public Map < String,
+    String > fillAandB(Map < String, String > map) {
+        if (map.containsKey("a") && !map.containsKey("b")) {
+            map.put("b", map.get("a"));
+        }
+        if (map.containsKey("b") && !map.containsKey("a")) {
+            map.put("a", map.get("b"));
+        }
+
+        return map;
+    }
+
+}
