6ea136845867bb9eb8e5857bb7aa844c07a73606
Kaltham <kaltham.aldifai@gmail.com>
2025-08-27 16:18:30 +0400

    The solution for Map-2 >firstSwap Task

---
+++ from_Kaltham/StringSwapperMap.java
@@ -0,0 +1,33 @@
+//Map-2 > firstSwap
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class StringSwapperMap {
+    public String[]swapFirstOnly(String[]strings) {
+        HashMap < String,
+        Integer > newMap = new HashMap <  > ();
+        HashSet < String > usedSet = new HashSet <  > ();
+        for (int i = 0; i < strings.length; i++) {
+            String firstChar = String.valueOf(strings[i].charAt(0));
+            if (!newMap.containsKey(firstChar)) {
+                newMap.put(firstChar, i);
+            } else if (usedSet.contains(firstChar)) {
+                continue;
+            } else {
+                Integer indexSwap = newMap.get(firstChar);
+                String wordSwap = strings[indexSwap];
+                strings[indexSwap] = strings[i];
+                strings[i] = wordSwap;
+                newMap.remove(firstChar);
+                usedSet.add(firstChar);
+
+            }
+        }
+
+        return strings;
+    }
+
+}
