7f423d8cecef24aef246fd6a29d3a017994e7357
Abdullah <alrashdi0002001@gmail.com>
2025-08-27 11:51:31 +0400

    #TRA-4490: WarmUp1_ SumDouble is Done

---
+++ From_Abdullah/SumDouble.java
@@ -0,0 +1,10 @@
+public class SumDouble {
+    public int sumOrDoubleIfEqual(int a, int b) {
+        int sum=0;
+        if (a!=b){
+            return sum= (a+b);
+        } else;
+        return sum = (a+b)*2;
+    }
+
+}
