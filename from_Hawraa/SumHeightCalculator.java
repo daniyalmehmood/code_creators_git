8cdfcea60219459b4e4f1a529ca73a03792bfa43
Hawraa <hawra8516@gmail.com>
2025-08-27 12:17:06 +0400

    AP-1: sumHeights added

---
+++ from_Hawraa/SumHeightCalculator.java
@@ -0,0 +1,10 @@
+// AP-1 > sumHeights
+public class SumHeightCalculator {
+    public int getSumHeights(int[] heights, int start, int end) {
+        int sum = 0;
+        for (int i = start; i < end; i++) {
+            sum += Math.abs(heights[i] - heights[i + 1]);
+        }
+        return sum;
+    }
+}
