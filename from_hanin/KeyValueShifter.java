026975855c3155ccc266ecd577e1119d715fa460
Hanin <haninzx2024@gmail.com>
2025-08-27 12:10:05 +0400

    Add task.java

---
+++ from_hanin/KeyValueShifter.java
@@ -0,0 +1,13 @@
+import java.util.Map;
+
+//Map-1 > mapBully
+public class KeyValueShifter {
+    public static Map<String, String> mapBully(Map<String, String> map) {
+        if (map.containsKey("a")) {
+            map.put("b", map.get("a"));
+            map.put("a", "");
+        }
+        return map;
+    }
+
+}
