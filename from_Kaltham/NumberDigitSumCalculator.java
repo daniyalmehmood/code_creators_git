c03efafcab22ff9d7066e27bb90495642c1629a4
Kaltham <kaltham.aldifai@gmail.com>
2025-08-27 08:37:12 +0400

    The solution for sumDigits Task

--- from_Kaltham/NumberDigitSumCalculator.java
+++ from_Kaltham/NumberDigitSumCalculator.java
@@ -1,6 +1,6 @@
 //Recursion-1 > sumDigits
 
-public class DigitSumCalculator {
+public class NumberDigitSumCalculator {
     public int calculateDigitSum(int n) {
         if (n > 0) {
             return (n % 10) + calculateDigitSum(n / 10);
