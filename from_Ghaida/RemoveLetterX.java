70d499450eadc38d63bedd310ebde91d1f25286e
Ghaida02a <ggmskaj@gmail.com>
2025-08-27 12:42:39 +0400

    #TRA-3883: Functional-1. noX modified

--- from_Ghaida/RemoveLetterX.java
+++ from_Ghaida/RemoveLetterX.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RemoveLetterX {//Functional-1 > noX
 
@@ -9,7 +10,6 @@ public class RemoveLetterX {//Functional-1 > noX
         System.out.println(removeXFromStrings(Arrays.asList("x")));                        // → [""]
     }
     public static List<String> removeXFromStrings(List<String> strings) {
-        strings.replaceAll(removeX -> removeX.replace("x", ""));
-        return strings;
+        return strings.stream().map(removeX -> removeX.replace("x", "")).collect(Collectors.toList());
     }
 }
