900aabe89066488559331694de267bc415ea2164
Hjufaili <gakhanaljufaily@gmail.com>
2025-08-28 09:30:45 +0400

    #TRA-4605: Recursion1_ NoX is Done

---
+++ From_Alharith/NoX.java
@@ -0,0 +1,10 @@
+public class NoX {
+
+    public String noX(String str) {
+        if (str.length()<1) return str;
+
+        if (str.charAt(0)=='x') return noX(str.substring(1));
+        return str.charAt(0)+noX(str.substring(1));
+    }
+
+}
