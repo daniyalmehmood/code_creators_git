dfee457acf3e7b1caeabcd65bcfa7bf64528e04a
Manal-Alhatmi <manalalhatmi99@outlook.com>
2025-08-28 08:51:01 +0400

    Added GroupNoAdjSolver.java

---
+++ From_Manal/GroupNoAdjSolver.java
@@ -0,0 +1,18 @@
+public class GroupNoAdjSolver {
+    public static boolean groupNoAdj(int start, int[] nums, int target) {
+        if (start >= nums.length) return target == 0;
+
+        if (groupNoAdj(start + 2, nums, target - nums[start]))
+            return true;
+        if (groupNoAdj(start + 1, nums, target))
+            return true;
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(groupNoAdj(0, new int[]{2,5,10,4}, 12));
+        System.out.println(groupNoAdj(0, new int[]{2,5,10,4}, 14));
+        System.out.println(groupNoAdj(0, new int[]{2,5,10,4,7}, 14));
+    }
+}
