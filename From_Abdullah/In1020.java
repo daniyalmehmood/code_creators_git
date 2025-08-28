0bd3d8be81e3b3e9131286065cd0dfcb045e634d
Abdullah <alrashdi0002001@gmail.com>
2025-08-28 11:48:07 +0400

    #TRA-4506:Warmup1_in1020 is done

---
+++ From_Abdullah/In1020.java
@@ -0,0 +1,9 @@
+public class In1020 {
+    public boolean isInRange10To20(int a, int b) {
+        if (( a >= 10 && a <= 20) || (  b >=10 && b <=20 )) {
+            return true;
+        }
+        return false;
+    }
+
+}
