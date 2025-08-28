041ec646c8cd3f5a8cf47f9bf0bb94a2e9eb137d
Abdullah <alrashdi0002001@gmail.com>
2025-08-28 12:14:11 +0400

    #TRA-4512:Warmup1_intMax is done

---
+++ From_Abdullah/IntMax.java
@@ -0,0 +1,11 @@
+public class IntMax {
+    public int maxOfThree(int a, int b, int c) {
+        if (a > b & a > c ) {
+            return a;
+        }
+        else if (b > a & b > c ) {
+            return b;
+        }
+        return c;
+    }
+}
