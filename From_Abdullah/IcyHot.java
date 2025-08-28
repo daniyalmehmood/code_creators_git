ffb5e5034ff57b7502db16ff49b3a4d0b2c3c4df
Abdullah <alrashdi0002001@gmail.com>
2025-08-28 11:45:01 +0400

    #TRA-4505:Warmup1_icyHot is done

---
+++ From_Abdullah/IcyHot.java
@@ -0,0 +1,9 @@
+public class IcyHot {
+    public boolean hasIcyAndHot(int temp1, int temp2) {
+
+        if (temp1 < 0 && temp2 > 100 || temp2 < 0 && temp1 > 100 ) {
+            return true;
+        }
+        return false;
+    }
+}
