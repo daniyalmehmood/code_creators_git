99e9904ec4b1b499ec242079cec130d773e5a662
creeew89-lang <creeew89@gmail.com>
2025-08-31 09:15:53 +0400

    Revert "Functional-2 > NoTeen task"

--- From_Mulk/WithoutTeen.java
+++
@@ -1,8 +0,0 @@
-// Functional-2 > noTeen
-
-public class WithoutTeen {
-    public List<Integer> removeNumbersInTeenRange(List<Integer> nums) {
-        nums.removeIf(n -> n >= 13 && n <= 19);
-        return nums;
-    }
-}
