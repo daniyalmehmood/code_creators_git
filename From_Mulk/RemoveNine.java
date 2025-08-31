028a50e21c511ca6d22a92b0ad0b1aef26902465
creeew89-lang <creeew89@gmail.com>
2025-08-31 09:22:54 +0400

    Revert "Functional-2 > No9 task"

--- From_Mulk/RemoveNine.java
+++
@@ -1,8 +0,0 @@
-//Functional-2 > no9
-
-public class RemoveNine {
-    public List<Integer> removeLastDigitNine(List<Integer> nums) {
-        nums.removeIf(n -> n % 10 == 9);
-        return nums;
-    }
-}
