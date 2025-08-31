889c83df7fd017ba9fd193271ada667225220bc0
Shatha Almamari <shatha.almamari99@gmail.com>
2025-08-31 14:37:15 +0400

    countHi task added

---
+++ from_shatha/HiCounter.java
@@ -0,0 +1,17 @@
+public class HiCounter {
+    public static int countHiOccurrences(String str) {
+    int count = 0;
+    for (int i = 0; i < str.length() - 1; i++) {
+        if (str.substring(i, i + 2).equals("hi")) {
+            count++;
+        }
+    }
+
+    return count;
+}
+    public static void main (String[]args){
+        System.out.println(countHiOccurrences("abc hi ho"));
+        System.out.println(countHiOccurrences("ABChi hi"));
+        System.out.println(countHiOccurrences("hihi"));
+    }
+}
