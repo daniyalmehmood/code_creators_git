f4bd41d31814815a4d1488b7baacfc58e4246d6a
creeew89-lang <creeew89@gmail.com>
2025-08-31 09:21:27 +0400

    Revert "Functional-2 > noNeg task"

--- From_Mulk/NoNegative.java
+++
@@ -1,8 +0,0 @@
-//Functional-2 > noNeg
-
-public class NoNegative {
-    public List<Integer> removeNegativeNumbers(List<Integer> nums) {
-        nums.removeIf(n -> n < 0);
-        return nums;
-    }
-}
