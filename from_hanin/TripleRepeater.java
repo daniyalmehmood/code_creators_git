ab5792ade99c36e5be29b679ec3488fdd9d66c34
Hanin <haninzx2024@gmail.com>
2025-08-31 10:45:55 +0400

    Add task.java

--- from_hanin/TripleRepeater.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public class TripleRepeater {
-    public List<String> tripleStrings(List<String> strings) {
-        strings.replaceAll(s -> s + s + s);
-        return strings;
-    }
-
-}
