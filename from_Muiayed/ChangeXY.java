684f85cbf0cd9b2872ad3c2fcc767101d408bd52
AL - Muiayed AL - Ansari <creeew89@gmail.com>
2025-09-01 15:09:10 +0400

    #TRA-4951: Recursrion-1 > changeXY

---
+++ from_Muiayed/ChangeXY.java
@@ -0,0 +1,13 @@
+public class ChangeXY {
+  public static void main(String[] args) {
+    System.out.println(changeXY("codex"));
+    System.out.println(changeXY("xxhixx"));
+    System.out.println(changeXY("xhixhix"));
+  }
+
+  public static String changeXY(String str) {
+    if (str.length() == 0) return "";
+    if (str.charAt(0) == 'x') return "y" + changeXY(str.substring(1));
+    return str.charAt(0) + changeXY(str.substring(1));
+  }
+}
