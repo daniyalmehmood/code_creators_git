3c2ea220909a39ecf2392fed0d2a9622b0027c42
AL - Muiayed AL - Ansari <creeew89@gmail.com>
2025-09-01 07:39:53 +0400

    #TRA-4868: Recursion-1 > BunnyEars

---
+++ from_Muiayed/BunnyEars.java
@@ -0,0 +1,12 @@
+public class BunnyEars {
+  public static void main(String[] args) {
+    System.out.println(bunnyEars(0));
+    System.out.println(bunnyEars(1));
+    System.out.println(bunnyEars(2));
+  }
+
+  public static int bunnyEars(int bunnies) {
+    if (bunnies == 0) return 0;
+    return 2 + bunnyEars(bunnies - 1);
+  }
+}
