c0079183b97aebe4ded514c8733e62fa0d7a6e91
AL - Muiayed AL - Ansari <creeew89@gmail.com>
2025-09-01 09:53:50 +0400

    #TRA-4874: Recursion-1 > count8

---
+++ from_Muiayed/Count8.java
@@ -0,0 +1,16 @@
+public class Count8 {
+  public static void main(String[] args) {
+    System.out.println(count8(8));
+    System.out.println(count8(818));
+    System.out.println(count8(8818));
+  }
+
+  public static int count8(int n) {
+    if (n == 0) return 0;
+    if (n % 10 == 8) {
+      if ((n / 10) % 10 == 8) return 2 + count8(n / 10);
+      return 1 + count8(n / 10);
+    }
+    return count8(n / 10);
+  }
+}
