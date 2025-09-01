d9c94350f2f60ebcb4ce8768ac90c3262305893e
Huria <alforihuria938@gmail.com>
2025-09-01 09:41:44 +0400

    Adding bigDiff Task

---
+++ from_Huria/CalculateRangeDifference.java
@@ -0,0 +1,17 @@
+public class CalculateRangeDifference {
+    public static int getMaxMinDifference(int[] nums) {
+        int min = nums[0];
+        int max = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            min = Math.min(min, nums[i]);
+            max = Math.max(max, nums[i]);
+        }
+        return max - min;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMaxMinDifference(new int[]{10, 3, 5, 6}));
+        System.out.println(getMaxMinDifference(new int[]{7, 2, 10, 9}));
+        System.out.println(getMaxMinDifference(new int[]{2, 10, 7, 2}));
+    }
+}
