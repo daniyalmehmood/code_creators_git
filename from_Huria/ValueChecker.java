9966305c63ab56f2bdd7377d9e8dc20a39005683
Huria <alforihuria938@gmail.com>
2025-09-01 15:01:12 +0400

    Adding isEverywhere Task

---
+++ from_Huria/ValueChecker.java
@@ -0,0 +1,13 @@
+public class ValueChecker {
+    public static boolean isValueEverywhere(int[] nums, int val) {
+        for(int i=0 ; i< nums.length-1 ; i++){
+            if(nums[i]!=val && nums[i+1]!=val) return false;
+        }
+        return true ;
+    }
+    public static void main(String[] args) {
+        System.out.println(isValueEverywhere(new int[]{1, 2, 1, 3}, 1));
+        System.out.println(isValueEverywhere(new int[]{1, 2, 1, 3}, 2));
+        System.out.println(isValueEverywhere(new int[]{1, 2, 1, 3, 4}, 1));
+    }
+}
