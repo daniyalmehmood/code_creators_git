99e52c4dc9129b9ddfc7573739f0175ebc7e9da3
Hanin <haninzx2024@gmail.com>
2025-09-01 14:38:49 +0400

    Add task.java

---
+++ from_hanin/TrailHeightCalculator.java
@@ -0,0 +1,10 @@
+public class TrailHeightCalculator {
+    public int calculateHeightChangeSum(int[] heights, int start, int end) {
+        int sum = 0;
+        for (int i = start; i < end; i++) {
+            sum += Math.abs(heights[i] - heights[i + 1]);
+        }
+        return sum;
+    }
+
+}
