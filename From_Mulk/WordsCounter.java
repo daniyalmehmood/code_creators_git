5a4fb6709f208336b18f3e1d4c706e7f425709e7
Mulk <mulkbadar774@gmail.com>
2025-09-02 14:34:45 +0400

    Solution of wordsCount task

---
+++ From_Mulk/WordsCounter.java
@@ -0,0 +1,13 @@
+//AP-1 > wordsCount
+public class WordsCounter {
+    public int countStringsWithLength(String[] words, int len) {
+        int count = 0;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length() == len) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
