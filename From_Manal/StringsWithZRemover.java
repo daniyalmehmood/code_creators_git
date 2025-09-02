d690e3ae9674c0b338641cc75485d33c396bb2fc
Manal-Alhatmi <manalalhatmi99@outlook.com>
2025-09-02 11:44:41 +0400

    Added ThreeTimesRepeater.java

--- From_Manal/StringsWithZRemover.java
+++
@@ -1,8 +0,0 @@
-   import java.util.List;
-    public class StringsWithZRemover {
-        // Remove strings containing "z"
-        public List<String> removeStringsWithZ(List<String> words) {
-            words.removeIf(s -> s.contains("z"));
-            return words;
-        }
-    }
