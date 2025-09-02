bbedb0258f4e262388f5c538f4be6240fcbb88ce
Hawraa <hawra8516@gmail.com>
2025-09-02 11:42:51 +0400

    Recursion-1: fibonacci added

---
+++ from_Hawraa/FibonacciNumberGenerator.java
@@ -0,0 +1,11 @@
+public class FibonacciNumberGenerator {
+    public int getFibonacciNumber(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return getFibonacciNumber(n - 1) + getFibonacciNumber(n - 2);
+        }
+    }
+}
