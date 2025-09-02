36585dc425f8c1f09b903ff603d7409ce761c9e8
Kaltham <kaltham.aldifai@gmail.com>
2025-09-02 11:17:49 +0400

    The solution for scoresClump Task

---
+++ from_Kaltham/CloseTriplet.java
@@ -0,0 +1,15 @@
+//AP-1 > scoresClump
+
+
+public class CloseTriplet {
+    public boolean hasCloseTriplet(int[]scores) {
+        for (int i = 0; i < scores.length - 2; i++) {
+            if (scores[i + 2] - scores[i] <= 2) {
+                return true;
+
+            }
+        }
+
+        return false;
+    }
+}
