6008381845ed5714fc9151711899581e35291118
Mulk <mulkbadar774@gmail.com>
2025-09-02 11:59:09 +0400

    Solution of scores100 task

---
+++ From_Mulk/HundredPair.java
@@ -0,0 +1,12 @@
+//AP-1 > scores100
+public class HundredPair {
+    public boolean checkForDoubleHundreds(int[] scores) {
+        for (int i = 0; i < scores.length - 1; i++) {
+            if (scores[i] == 100 && scores[i + 1] == 100) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
